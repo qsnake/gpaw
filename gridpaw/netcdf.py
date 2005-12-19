6167a956ec02dd8bfcdda69d5fe427189a488113
larsbrunohansen <larsbrunohansen@6419b465-d902-0410-997c-be2266baf723>
2005-12-19 14:54:53 +0000

    Added parallel read Added parallel write for k-point parallel calculation
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@112 6419b465-d902-0410-997c-be2266baf723

--- gridpaw/netcdf.py
+++ gridpaw/netcdf.py
@@ -1,6 +1,8 @@
 import os
 
 import Numeric as num
+from parallel import get_parallel_info_s_k
+
 try:
     import Scientific.IO.NetCDF as NetCDF
 except ImportError:
@@ -24,47 +26,95 @@ def read_netcdf(paw, filename):
     a0 = paw.a0
     Ha = paw.Ha
 
-    if mpi.parallel:
-        raise NotImplementedError('Not implemented')
-
     nc = NetCDF.NetCDFFile(filename, 'r')
     
     # Units ???? XXXX
     realvalued = (not nc.dimensions.has_key('two'))
     vars = nc.variables
 
-    if vars.has_key('PseudoWaveFunctions'):
-        psit_unG = vars['PseudoWaveFunctions']
-        f_skn = vars['OccupationNumbers']
-        u = 0 
-        for s in range(wf.nspins):
-            for k in range(wf.nkpts): 
+    required_netcdf_variables = ['PseudoWaveFunctions',
+                                 'OccupationNumbers',
+                                 'PotentialEnergy',
+                                 'PseudoElectronDensity',
+                                 'Projections' ]
+
+    # wavefunctions: All processors keeps a reference to the netcdf variable 
+    psit_unG = vars['PseudoWaveFunctions']
+    for s in range(wf.nspins):
+        for k in range(wf.nkpts):
+            kpt_rank,u = get_parallel_info_s_k(wf,s,k)
+            if wf.kpt_comm.rank==kpt_rank:
                 kpt = wf.kpt_u[u]
                 kpt.allocate(wf.nbands)
                 kpt.psit_nG = NetCDFWaveFunction(psit_unG, s, k,
                                                  scale=a0**1.5,
                                                  cmplx=not realvalued)
+    
+    # eigenvalues and occupation
+    eps_skn = vars['Eigenvalues']
+    f_skn = vars['OccupationNumbers']
+    for s in range(wf.nspins):
+        for k in range(wf.nkpts):
+            kpt_rank,u = get_parallel_info_s_k(wf,s,k)
+            if wf.kpt_comm.rank==kpt_rank:
+                kpt = wf.kpt_u[u]
+                kpt.eps_n[:] = eps_skn[s,k]
                 kpt.f_n[:] = f_skn[s, k]
 
-                u += 1
+
+    paw.Etot = vars['PotentialEnergy'][0] / Ha
+    paw.Ekin = nc.Ekin[0] / Ha
+    paw.Epot = nc.Epot[0] / Ha
+    paw.Ebar = nc.Ebar[0] / Ha
+    paw.Exc = nc.Exc[0] / Ha
+    paw.S = nc.S[0] / Ha
+    
+    # Read pseudoelectron density on the coarse grid and
+    # distribute out to nodes
+    for s in range(paw.nspins): 
+        paw.gd.distribute(vars['PseudoElectronDensity'][:][s] * a0**3,paw.nt_sG[s])
+
+    # Transfer the density to the fine grid:
+    for s in range(paw.nspins):
+        paw.interpolate(paw.nt_sG[s], paw.nt_sg[s])
 
     # Read projections:
     for nucleus in paw.nuclei:
         nucleus.allocate(wf.nspins, wf.nmykpts, wf.nbands)
+
     P_skni = vars['Projections']
-    for ns in range(wf.nspins): 
-        for nk in range(wf.nkpts):
-            i = 0
-            for nucleus in paw.nuclei:
-                P = nucleus.P_uni
-                ni = P.shape[2]  
-                if realvalued:
-                    P[ns,:,: ] = P_skni[ns, nk,:,i:i + ni]
-                else:
-                    P.real[ns,:,:] = P_skni[ns, nk, :,i:i + ni, 0]
-                    P.imag[ns,:,:] = P_skni[ns, nk, :,i:i + ni, 1]
-                i += ni
-            assert i == nc.dimensions['nproj']
+    i = 0
+    for nucleus in paw.nuclei:
+        ni = nucleus.get_number_of_partial_waves()
+        P_uni = num.zeros((wf.nspins*wf.nmykpts,wf.nbands,ni),nucleus.typecode)
+
+        P_uni_tot = num.zeros((wf.nspins, wf.nkpts, wf.nbands, ni),
+                               nucleus.typecode)
+        if realvalued:
+           P_uni_tot[:] = P_skni[:, :, :, i:i + ni]
+        else:
+           P_uni_tot[:].real = P_skni[:, :, :, i:i + ni, 0]
+           P_uni_tot[:].imag = P_skni[:, :, :, i:i + ni, 1]
+
+        for s in range(wf.nspins):
+           for k in range(wf.nkpts):
+              kpt_rank,u = get_parallel_info_s_k(wf,s,k)
+              if paw.wf.kpt_comm.rank == kpt_rank:
+                  P_uni[u,:] = P_uni_tot[s,k,:]
+
+        if paw.domain.comm.rank==MASTER:
+            if nucleus.domain_overlap == EVERYTHING:
+                nucleus.P_uni[:] = P_uni[:]
+            else:
+                paw.domain.comm.send(P_uni, nucleus.rank)
+        else:
+            if nucleus.rank == paw.domain.comm.rank:
+                paw.domain.comm.receive(nucleus.P_uni,MASTER)
+
+        i += ni
+
+    if mpi.rank==MASTER:
+        assert i == nc.dimensions['nproj']
 
     # Read atomic density matrices:
     D_sq = vars['AtomicDensityMatrices']
@@ -76,19 +126,6 @@ def read_netcdf(paw, filename):
         q1 = q2
     assert q2 == nc.dimensions['nadm']
 
-    paw.Etot = vars['PotentialEnergy'][0] / Ha
-    paw.Ekin = nc.Ekin[0] / Ha
-    paw.Epot = nc.Epot[0] / Ha
-    paw.Ebar = nc.Ebar[0] / Ha
-    paw.Exc = nc.Exc[0] / Ha
-    paw.S = nc.S[0] / Ha
-
-    # Read pseudoelectron density on the coarse grid
-    paw.nt_sG[:] = vars['PseudoElectronDensity'][:] * a0**3
-
-    # Transfer the density to the fine grid:
-    for s in range(paw.nspins):
-        paw.interpolate(paw.nt_sG[s], paw.nt_sg[s])
 
 def write_netcdf(paw, filename):
     wf = paw.wf
@@ -161,26 +198,54 @@ def write_netcdf(paw, filename):
                                      'nbands', 'nproj'))
         else:
             var = nc.createVariable('Projections', num.Float,
-                                    ('nspins', 'nibzkpst',
+                                    ('nspins', 'nibzkpts',
                                      'nbands', 'nproj', 'two'))
 
-    # master (domain_comm 0) collects projections
-    i = 0
-    for nucleus in paw.nuclei:
-        P_uni = paw.get_nucleus_P_uni(nucleus)
-        if mpi.rank == MASTER:
-            ni = P_uni.shape[2]
-            P_uni.shape = (wf.nspins, wf.nkpts, wf.nbands, ni) # don't forget
-            # to restore the shape! XXX
-            if realvalued:
-                var[:, :, :, i:i + ni] = P_uni
-            else:
-                var[:, :, :, i:i + ni, 0] = P_uni.real
-                var[:, :, :, i:i + ni, 1] = P_uni.imag
-            P_uni.shape = (wf.nspins * wf.nkpts, wf.nbands, ni)
-            i += ni
-    assert i == nproj
 
+    # master in each domain (domain_comm 0) collects projections
+    # with domain_overlap==EVERYTHING for the nkpt local kpoints,
+    # these are then summed in P_uni_tot
+    i = 0
+    nnodes = wf.kpt_comm.size
+    nkpt = len(wf.myibzk_kc)
+    # note the layout of P_uni, it has allocated
+    # all spins 
+    nspins = wf.nspins      
+    rank = paw.wf.kpt_comm.rank 
+    for nucleus in paw.nuclei:
+        ni = nucleus.get_number_of_partial_waves()
+        P_uni = num.zeros((nspins*nkpt,wf.nbands,ni),nucleus.typecode)
+        P_uni_tot = num.zeros((wf.nspins, wf.nkpts, wf.nbands, ni),
+                              nucleus.typecode)
+        if paw.domain.comm.rank==MASTER: 
+            if nucleus.domain_overlap == EVERYTHING:
+                P_uni = nucleus.P_uni
+            else:
+                paw.domain.comm.receive(P_uni, nucleus.rank)
+        else:
+            if nucleus.rank == paw.domain.comm.rank:
+                paw.domain.comm.send(nucleus.P_uni,MASTER)
+
+        for s in range(wf.nspins): 
+           for k in range(wf.nkpts): 
+              kpt_rank,u = get_parallel_info_s_k(wf,s,k) 
+              if paw.wf.kpt_comm.rank == kpt_rank: 
+                  P_uni_tot[s,k,:] = P_uni[u,:]
+
+        paw.wf.kpt_comm.sum(P_uni_tot)
+              
+        if mpi.rank == MASTER:
+            P_uni_tot.shape = (wf.nspins, wf.nkpts, wf.nbands, ni)
+            if realvalued:
+                var[:, :, :, i:i + ni] = P_uni_tot
+            else:
+                var[:, :, :, i:i + ni, 0] = P_uni_tot.real
+                var[:, :, :, i:i + ni, 1] = P_uni_tot.imag
+            # P_uni.shape = (wf.nspins * wf.nkpts, wf.nbands, ni)
+            i += ni
+
+    if mpi.rank==MASTER:
+        assert i == nproj
 
     if mpi.rank == MASTER:
         # Write atomic density matrices:
@@ -199,42 +264,59 @@ def write_netcdf(paw, filename):
             q1 = q2
         assert q2 == nadm
 
+    if mpi.rank == MASTER:
         # Write the eigenvalues:
         var = nc.createVariable('Eigenvalues', num.Float,
                                 ('nspins', 'nibzkpts', 'nbands'))
-        u = 0
-        for s in range(wf.nspins):
-            for k in range(wf.nkpts):
-                var[s, k] = wf.kpt_u[u].eps_n * Ha
-                u += 1
 
+    eps_n = num.zeros((wf.nspins,wf.nkpts,wf.nbands),num.Float)
+    utest = 0
+    for s in range(wf.nspins):
+        for k in range(wf.nkpts):
+            kpt_rank,u = get_parallel_info_s_k(wf,s,k)
+            if wf.kpt_comm.rank == kpt_rank:
+                eps_n[s, k] = wf.kpt_u[u].eps_n * Ha
+            utest += 1
+    wf.kpt_comm.sum(eps_n)
+
+    if mpi.rank == MASTER: 
+        var[:] = eps_n[:]
+
+    if mpi.rank == MASTER: 
         # Write the occupation numbers:
         var = nc.createVariable('OccupationNumbers', num.Float,
                                 ('nspins', 'nibzkpts', 'nbands'))
-        u = 0
-        for s in range(wf.nspins):
-            for k in range(wf.nkpts):
-                var[s, k] = wf.kpt_u[u].f_n
-                u += 1
+        
+    f_n = num.zeros((wf.nspins,wf.nkpts,wf.nbands),num.Float)
+    for s in range(wf.nspins):
+        for k in range(wf.nkpts):
+            kpt_rank,u = get_parallel_info_s_k(wf,s,k)
+            if wf.kpt_comm.rank == kpt_rank:
+                f_n[s, k] = wf.kpt_u[u].f_n
+    wf.kpt_comm.sum(f_n)
 
+    if mpi.rank == MASTER: 
+        var[:] = f_n[:]
 
     # Write the pseudodensity on the coarse grid
     if mpi.rank==MASTER:
         var = nc.createVariable('PseudoElectronDensity', num.Float,
                                 ('nspins',
                                  'ngptsx', 'ngptsy', 'ngptsz'))
-    nt_sG = paw.gd.collect(paw.nt_sG)
-    if mpi.rank == MASTER:
-        var[:] = nt_sG / a0**3
+
+    for s in range(wf.nspins):
+        nt_sG = paw.gd.collect(paw.nt_sG[s])
+        if mpi.rank == MASTER:
+            var[s] = nt_sG / a0**3
 
     # Write the pseudo charge density on the fine grid (rhot_g)
-    if mpi.rank == MASTER:
-        var = nc.createVariable('PseudoChargeDensity', num.Float,
-                                ('nspins',
-                                 'nfinegptsx', 'nfinegptsy', 'nfinegptsz'))
-    rhot_g = paw.gd.collect(paw.rhot_g)
-    if mpi.rank == MASTER:
-        var[:] = rhot_g / a0**3
+    # if mpi.rank == MASTER:
+    #  var = nc.createVariable('PseudoChargeDensity', num.Float,
+    #                            ('nspins',
+    #                             'nfinegptsx', 'nfinegptsy', 'nfinegptsz'))
+    # rhot_g = paw.gd.collect(paw.rhot_g)
+    # if mpi.rank == MASTER:
+    #    var[:] = rhot_g / a0**3
 
     # Write the wave functions:
     if mpi.rank == MASTER:
@@ -246,19 +328,16 @@ def write_netcdf(paw, filename):
             var = nc.createVariable('PseudoWaveFunctions', num.Float,
                                     ('nspins', 'nibzkpts', 'nbands',
                                      'ngptsx', 'ngptsy', 'ngptsz', 'two'))
-    c = 1.0 / a0**1.5
-    u = 0
     for s in range(wf.nspins):
         for k in range(wf.nkpts):
-            for n, psit_G in enumerate(wf.kpt_u[u].psit_nG):
-                a_G = paw.gd.collect(psit_G)
-                if mpi.rank == MASTER:
+            for n in range(wf.nbands):
+                a_G = paw.get_wave_function_array(n, k, s)
+                if mpi.rank==MASTER: 
                     if realvalued:
-                        var[s, k, n] = c * a_G
+                        var[s, k, n] = a_G
                     else:
-                        var[s, k, n, :, :, :, 0] = c * a_G.real
-                        var[s, k, n, :, :, :, 1] = c * a_G.imag
-            u += 1
+                        var[s, k, n, :, :, :, 0] = a_G.real
+                        var[s, k, n, :, :, :, 1] = a_G.imag
 
     if mpi.rank == MASTER:
         # Add sync here to ensure that the last wave function is
@@ -277,6 +356,9 @@ class NetCDFWaveFunction:
             self.shape = netcdfshape[2:-1]
         else:
             self.shape = netcdfshape[2:]
+
+    def __len__(self):
+        return self.shape[0]
             
     def __getitem__(self, index):
         if type(index) is not tuple:
