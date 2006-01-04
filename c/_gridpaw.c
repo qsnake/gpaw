d7764f51b3e4a228d05427200994de02f8a062d4
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2006-01-04 12:48:48 +0000

    second derivatives of Exc interfaces added
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@129 6419b465-d902-0410-997c-be2266baf723

--- c/_gridpaw.c
+++ c/_gridpaw.c
@@ -3,6 +3,8 @@
 
 PyObject* gemm(PyObject *self, PyObject *args);
 PyObject* axpy(PyObject *self, PyObject *args);
+PyObject* d2Excdnsdnt(PyObject *self, PyObject *args);
+PyObject* d2Excdn2(PyObject *self, PyObject *args);
 PyObject* rk(PyObject *self, PyObject *args);
 PyObject* r2k(PyObject *self, PyObject *args);
 PyObject* diagonalize(PyObject *self, PyObject *args);
@@ -17,6 +19,8 @@ PyObject* NewXCFunctionalObject(PyObject *self, PyObject *args);
 static PyMethodDef functions[] = {
   {"gemm", gemm, METH_VARARGS, 0},
   {"axpy", axpy, METH_VARARGS, 0},
+  {"d2Excdnsdnt", d2Excdnsdnt, METH_VARARGS, 0},
+  {"d2Excdn2", d2Excdn2, METH_VARARGS, 0},
   {"rk",  rk,  METH_VARARGS, 0},
   {"r2k", r2k, METH_VARARGS, 0},
   {"diagonalize", diagonalize, METH_VARARGS, 0},
