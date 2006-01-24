69593fedce7017fda8314c791bdd5021f729c2d4
larsbrunohansen <larsbrunohansen@6419b465-d902-0410-997c-be2266baf723>
2006-01-24 11:21:16 +0000

    added two wannier examples
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@154 6419b465-d902-0410-997c-be2266baf723

---
+++ test/wannier-ethylene.py
@@ -0,0 +1,49 @@
+""" gridpaw wannier example for ethylene, 
+    corresponding to the ASE Wannier tutorial. 
+"""
+from ASE import Atom, ListOfAtoms
+from gridpaw import Calculator
+from gridpaw.utilities import equal
+
+if 1:
+    a = 6.0  # Size of unit cell (Angstrom)
+
+    ethylene = ListOfAtoms([
+                       Atom('H', (-1.235, 0.936 , 0 ),tag=0),
+                       Atom('H', ( 1.235,-0.936 , 0 ),tag=1),
+                       Atom('H', ( 1.235, 0.936 , 0 ),tag=1),
+                       Atom('H', (-1.235,-0.936 , 0 ),tag=1),
+                       Atom('C', ( 0.660, 0.000 , 0 ),tag=1),
+                       Atom('C', (-0.660, 0.000 , 0 ),tag=1)],
+                       cell=(a,a,a), periodic=True)
+
+
+    # display to the center of the cell
+    pos = ethylene.GetCartesianPositions() 
+    pos += a/2. 
+    ethylene.SetCartesianPositions(pos)
+
+    calc = Calculator(nbands=8, h=0.20, tolerance=0.001)
+    ethylene.SetCalculator(calc)
+    print ethylene.GetPotentialEnergy()
+    calc.Write('ethylene.nc')
+
+from ASE.Utilities.Wannier import Wannier
+
+ethylene = Calculator.ReadAtoms('ethylene.nc')
+print ethylene.GetPotentialEnergy()
+wannier = Wannier(numberofwannier=6, calculator=ethylene.GetCalculator())
+wannier.Localize()
+
+value = wannier.GetFunctionalValue() 
+equal(13.3625749,value,1e-3)
+
+for w in wannier.GetCenters():
+    print w['radius'], w['pos']
+
+ethylene.extend(wannier.GetCentersAsAtoms())
+
+for n in range(1): 
+    wannier.WriteCube(n,"ethylene%d.cube"%n)
+
+
---
+++ test/wannier-hwire.py
@@ -0,0 +1,48 @@
+from gridpaw import Calculator
+from ASE import Atom,ListOfAtoms
+from ASE.Utilities.Wannier.Wannier import Wannier
+from ASE.IO.Cube import WriteCube
+
+
+if 1:
+    hhbondlenght = 0.9
+    natoms = 1
+    L = hhbondlenght*(natoms)
+    bands = 2
+
+    posx = 0.0
+    dx = hhbondlenght
+    atomslst = []
+    for n in range(natoms):
+        atom = Atom('H',(posx,4.,4.))
+        atomslst.append(atom)
+        posx = posx + dx
+    atoms = ListOfAtoms(atomslst,cell = (L,10,10),periodic=(True,True,True))
+
+
+    # gridpaw calculator
+    calc = Calculator(h=0.18, nbands=bands, xc='PBE',out='wire.txt',kpts=(21,1,1))
+    atoms.SetCalculator(calc)
+    # Displace kpoints sligthly, so that the symmetry program 
+    # not use inversion symmetry to reduce kpoints.
+    calc.bzk_kc[:,0] += 2e-5
+    energy = atoms.GetPotentialEnergy()
+    calc.Write('wire.nc')
+    
+
+nwannier = 1
+atoms = Calculator.ReadAtoms('wire.nc')
+calc = atoms.GetCalculator()
+wannier = Wannier(numberofwannier=nwannier,calculator=calc,occupationenergy=30.0)
+
+wannier.SaveZIBlochMatrix('zibloch.pickle')
+wannier.Localize()
+wannier.TranslateAllWannierFunctionsToCell([10,0,0])
+
+centers = wannier.GetCenters()
+for center in centers: 
+   print center
+
+for n in range(1): 
+   wannier.WriteCube(n,'test'+str(n)+'.cube')
+
