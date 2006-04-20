fa3cf277fcc5f06e014454ef99c200db51e646a5
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2006-04-20 14:42:26 +0000

    The code has been changed to use gaussians as projector functions and Ono-Hirose restriction of numerical projector functions has been dropped.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@228 6419b465-d902-0410-997c-be2266baf723

--- demo/H.py
+++ demo/H.py
@@ -5,7 +5,8 @@ a = 5.0
 H = ListOfAtoms([Atom('H',(a/2, a/2, a/2), magmom=0)],
                 periodic=0,
                 cell=(a, a, a))
-calc = Calculator(nbands=1, h=0.20, onohirose=5, tolerance=0.001, softgauss=0)
+#calc = Calculator(nbands=1, h=0.2, onohirose=1, tolerance=0.001, softgauss=0)
+calc = Calculator(nbands=2, h=0.2, softgauss=0)
 H.SetCalculator(calc)
 if 0:
     import profile
