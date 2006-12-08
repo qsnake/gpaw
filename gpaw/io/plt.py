a5545c5171a81458e0c703ac0c9cbe7560a541d5
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2006-12-08 07:53:43 +0000

    read_plt big endian files enabled
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@511 6419b465-d902-0410-997c-be2266baf723

--- gpaw/io/plt.py
+++ gpaw/io/plt.py
@@ -6,14 +6,24 @@ from gpaw.utilities import check_unit_cell
 
 def read_plt(filename):
     """Read plt files
-    returns the cell(3x3 matrix) and the grid
+    returns the cell(3x3 matrix), the grid and the origin
     """
     f = open(filename)
+    fmt='ii'
+    # check endian encoding
+    byteswap = False
+    three, tos = unpack(fmt,f.read(calcsize(fmt)))
+##    print "<read_plt> three=",three
+    if three != 3: byteswap = True
 
-    fmt='iiiii'
-    d, d, nx, ny, nz = unpack(fmt,f.read(calcsize(fmt)))
+    fmt='iii'
+    if byteswap: fmt='>iii'
+    nx, ny, nz = unpack(fmt,f.read(calcsize(fmt)))
+##    print "<read_plt> nx,ny,nz=",nx,ny,nz
+    
 
     fmt='ff'
+    if byteswap: fmt='>ff'
     z0, ze = unpack(fmt,f.read(calcsize(fmt)))
     y0, ye = unpack(fmt,f.read(calcsize(fmt)))
     x0, xe = unpack(fmt,f.read(calcsize(fmt)))
@@ -26,13 +36,19 @@ def read_plt(filename):
     cell[2,2] = nz*dz 
     
     fmt='f'
+    if byteswap: fmt='>f'
     size = nx*ny*nz * calcsize(fmt)
+##    print "<read_plt> nx,ny,nz=",nx,ny,nz
+##    print "<read_plt> size=",size
     arr = num.fromstring(f.read(size),num.Float32)
+    if byteswap: arr = arr.byteswapped()
     f.close()
-    
-    return cell, num.transpose(num.resize(arr,(nz,ny,nx)))
 
-def write_plt(cell, grid, filename, type=4):
+    return cell, num.transpose(num.resize(arr,(nx,ny,nz))), (x0,y0,z0)
+
+def write_plt(cell, grid, filename,
+              origin=(0.0,0.0,0.0), # ASE uses (0,0,0) as origin
+              type=4):
     """Input:
     cell = unit cell object as given from ListOfAtoms.GetUnitCell()
     grid = the grid to write
@@ -54,8 +70,7 @@ def write_plt(cell, grid, filename, type=4):
     f.write(pack('ii', 3, type))
     f.write(pack('iii', nx, ny, nz))
 
-    # ASE uses (0,0,0) as origin ????
-    x0 = y0 = z0 = 0.
+    x0, y0, z0 = origin
     xe = x0 +(nx-1)*dx
     ye = y0 +(ny-1)*dy
     ze = z0 +(nz-1)*dz
