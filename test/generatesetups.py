62f99d1bf25f661605401d1600dc91344b092bed
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2007-04-24 11:46:11 +0000

    New test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@678 6419b465-d902-0410-997c-be2266baf723

---
+++ test/generatesetups.py
@@ -0,0 +1,27 @@
+from gpaw.atom.generator import Generator, parameters
+
+"""Test generation of setups
+
+Run this script to generate exactly those setups needed for running
+the tests.
+"""
+
+files = []
+
+def gen(symbol, xcname):
+    g = Generator(symbol, xcname, scalarrel=True, nofiles=True)
+    g.run(exx=True, **parameters[symbol])
+    files.append('%s.%s' % (symbol, xcname))
+
+for symbol in ['H', 'He', 'C', 'N', 'O', 'Cl', 'Al', 'Si', 'Na', 'Fe', 'Cu']:
+    gen(symbol, 'LDA')
+for symbol in ['H', 'He', 'Li', 'N']:
+    gen(symbol, 'PBE')
+for symbol in ['He', 'Li']:
+    gen(symbol, 'revPBE')
+
+if __name__ != '__main__':
+    # We have been imported by test.py, so we should clean up:
+    from os import remove
+    for file in files:
+        remove(file)
