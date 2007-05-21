42f4b5d81dbaccf808aab41c7ee32a801f844b48
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2007-05-21 09:32:37 +0000

    corrected and cleaned
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@726 6419b465-d902-0410-997c-be2266baf723

--- gpaw/lrtddft/spectrum.py
+++ gpaw/lrtddft/spectrum.py
@@ -1,16 +1,17 @@
 import sys
+import Numeric as num
 from math import exp, pi, sqrt
 
 from ASE.Units import Convert
-from gpaw.gauss import Gauss
 
 def spectrum(exlist=None,
              filename=None,
              emin=None,
              emax=None,
              de=None,
+             energyunit='eV',
              folding='Gauss',
-             width=0.08 # Gauss/Lorentz width in eV
+             width=0.08 # Gauss/Lorentz width
              ):
     """Print the optical spectrum of an excitation list
 
@@ -21,61 +22,73 @@ def spectrum(exlist=None,
     ``emin``        min. energy, set to cover all energies if not given
     ``emax``        max. energy, set to cover all energies if not given
     ``de``          energy spacing
+    ``energyunit``  Energy unit, default 'eV'
     ``folding``     Gauss (default) or Lorentz
-    ``width``       folding width
+    ``width``       folding width in terms of the chosen energyunit
     =============== ===================================================
     all energies in [eV]
     """
 
+    # initialise the folding function
     func=Gauss(width)
     if folding == 'Lorentz':
         func=Lorentz(width)
+    func=Lorentz(width)
 
     # output
     out = sys.stdout
     if filename != None:
         out = open( filename, 'w' )
 
-    au_eV = Convert(1., 'Hartree', 'eV')
-
- ##   print "exlist=",exlist.GetEnergies()
+    # energy unit
+    Ha = Convert(1., 'Hartree', energyunit)
 
     # minimal and maximal energies
     if emin == None:
-        emin=exlist.GetEnergies()[0]*au_eV
-        for e in exlist.GetEnergies():
-            e*=au_eV
-            if e<emin: emin=e
-        emin -= 4*width
-    if emax == None:
-        emax=exlist.GetEnergies()[0]*au_eV
-        for e in exlist.GetEnergies():
-            e*=au_eV
-            if e>emax: emax=e
-        emax += 4*width
+        emin=exlist.get_energies()[0]*Ha
+        emax=emin
+    for e in exlist.get_energies():
+        e*=Ha
+        if e<emin: emin=e
+        if e>emax: emax=e
+    emin -= 4*width
+    emax += 4*width
 
     # set de to sample 4 points in the width
     if de == None:
         de = width/4.
 
-    print >> out, '# %s folded, width=%g [eV]' % (folding,width)
-    print >> out, '# om [eV]     osz          osz x       osz y       osz z'
+    print >> out, '# %s folded, width=%g [%s]' % (folding,width,energyunit)
+    print >> out, '# om [%s]     osz          osz x       osz y       osz z'\
+          % energyunit
 
     # loop over energies
     emax=emax+.5*de
     e=emin
     while e<emax:
-        val=[0.,0.,0.,0.]
+        val=num.zeros((4),num.Float)
         for ex in exlist:
-            wght=func.Get(ex.GetEnergy()*au_eV-e)
-            osz=ex.GetOszillatorStrength()
-            for i in range(4):
-                val[i]+=wght*osz[i]
-        print >> out, "%10.5f %12.7f %12.7f %11.7f %11.7f" % \
+            wght=func.Get(ex.get_energy()*Ha-e)
+            osz=num.array(ex.GetOscillatorStrength())
+            val += wght*osz
+        print >> out, "%10.5f %12.7e %12.7e %11.7e %11.7e" % \
               (e,val[0],val[1],val[2],val[3])
         e+=de
         
     if filename != None: out.close()
+
+class Gauss:
+    """Normalised Gauss distribution"""
+    def __init__(self,width=0.08):
+        self.SetWidth(width)
+        
+    def Get(self,x):
+        return self.norm*exp(-(x*self.wm1)**2)
+    
+    def SetWidth(self,width=0.08):
+        self.norm=1./width/sqrt(pi)
+        self.wm1=sqrt(.5)/width
+    
 
 class Lorentz:
     """Normalised Lorentz distribution"""
