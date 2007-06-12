e8c15fca7541d687ef514ad841dfef9d5e68238e
rostgaard <rostgaard@6419b465-d902-0410-997c-be2266baf723>
2007-06-12 12:52:04 +0000

    Added a module for calculating the local density of states. This is actually just achieved be projecting the wavefunctions onto the projector functions for the considered atom, i.e. using nucleus.P_uni as a weight.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@822 6419b465-d902-0410-997c-be2266baf723

---
+++ gpaw/utilities/dos.py
@@ -0,0 +1,44 @@
+from math import pi, sqrt
+from ASE.Utilities.DOS import DOS
+
+import Numeric as num
+
+
+class LDOS(DOS):
+    def __init__(self, calc, width=None, window=None, npts=201):
+        """Electronic Local Density Of States object.
+
+        'calc' is an ASE calculator instance.
+        'width' is the width of the "delta-functions". Defaults to the electronic temperature of the calculation.
+        'window' is the energy window. Default is from the lowest to the highest eigenvalue.
+        'npts' is the number of energy points.
+        """
+        DOS.__init__(self, calc, width, window, npts)
+        self.P_auni = [nucleus.P_uni for nucleus in calc.paw.nuclei]
+
+    def Delta(self, energy):
+        """Return a delta-function centered at 'energy'."""
+        x = -((self.energies - energy) / self.width)**2
+        x = num.clip(x, -100.0, 100.0)
+        return num.exp(x) / (sqrt(pi) * self.width)
+
+    def GetLDOS(self, a, spin=None):
+        """Get the DOS projected onto the projector functions of atom a."""
+        
+        if spin is None:
+            if self.nspins == 2:
+                # Spin-polarized calculation, but no spin specified -
+                # return the total DOS:
+                return self.GetLDOS(a, spin=0) + self.GetLDOS(a, spin=1)
+            else:
+                spin = 0
+
+        nk = len(self.w_k)
+        P_kni = self.P_auni[a][spin * nk : (spin + 1) * nk]
+        ni = len(P_kni[0, 0])
+        dos_ie = num.zeros((ni, self.npts), num.Float)
+        for w, P_ni, e_n in zip(self.w_k, P_kni, self.e_skn[spin]):
+            for P_i, e in zip(P_ni, e_n):
+                for i, P in enumerate(P_i):
+                    dos_ie[i] += w * P**2 * self.Delta(e)
+        return dos_ie
