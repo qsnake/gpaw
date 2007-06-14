35e6add4c5c30179f7af64980e6004c803a9eeaa
lauri <lauri@6419b465-d902-0410-997c-be2266baf723>
2007-06-14 10:38:41 +0000

    Old vector dot product replaced by Numeric's vdot.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@834 6419b465-d902-0410-997c-be2266baf723

--- gpaw/tddft/BasicLinearAlgebra.py
+++ gpaw/tddft/BasicLinearAlgebra.py
@@ -7,13 +7,9 @@ class BLAS:
     # Vector dot product, a^H b
     # where ^H is conjugate transpose ^*^T
     # BLAS operation ZDOTC
-    # !!! FIX ME !!! better way to do this? BLAS!!!
     def zdotc(self, x, y):
+        return num.vdot(x,y)
         # return dotc(x,y)
-        tmp = num.conjugate(x)*y
-        while len(tmp.shape) > 1:
-            tmp = num.sum(tmp)
-        return num.sum(tmp)
     
     # Scalar multiplication and vector addition, y = a x + y
     # BLAS operation ZAXPY
