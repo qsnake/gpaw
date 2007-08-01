3580626e143e09c6c011df346fdba4d86dc3d1e1
moses <moses@6419b465-d902-0410-997c-be2266baf723>
2007-08-01 20:01:48 +0000

    added mic to vdw test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@923 6419b465-d902-0410-997c-be2266baf723

--- test/vdw.py
+++ test/vdw.py
@@ -26,3 +26,14 @@ d += d[::-1].copy()
 e2 = VanDerWaals(d, unitcell=uc,xcname='RPBE').GetEnergy(n=4)
 print 'RPBE',e1, e2, 2 * e1 - e2
 
+#pbc using mic
+for x in range(2 * n):
+    for z in range(n):
+        for y in range(n):
+            r = sqrt((x * h - c)**2 + (y * h - c)**2 + (z * h - c)**2)
+            d[x, y, z] = exp(-2 * r) / pi
+                                    
+e1 = VanDerWaals(d, unitcell=uc,xcname='revPBE',pbc='mic').GetEnergy(n=4)
+d += d[::-1].copy()
+e2 = VanDerWaals(d, unitcell=uc,xcname='revPBE',pbc='mic').GetEnergy(n=4)
+print  'revPBE mic',e1, e2, 2 * e1 - e2
