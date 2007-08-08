c889c938ae0bcdf7f2a6bed84683b301b3b3c460
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2007-08-08 07:58:55 +0000

    nc used only if available
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@946 6419b465-d902-0410-997c-be2266baf723

--- test/restart2.py
+++ test/restart2.py
@@ -6,7 +6,7 @@ from ASE import Atom, ListOfAtoms
 from gpaw.utilities import equal
 from gpaw.cluster import Cluster
 
-endings = ['nc','gpw']
+endings = ['gpw']
 try:
     import Scientific.IO.NetCDF
     endings.append('nc')
