3bdf4069f6f20d794e577c5cb580e05f8cf1cae8
mvanin <mvanin@6419b465-d902-0410-997c-be2266baf723>
2007-09-10 07:28:02 +0000

    LCAO stuff: work in progress
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1059 6419b465-d902-0410-997c-be2266baf723

---
+++ gpaw/lcao/lcao.py
@@ -0,0 +1,26 @@
+from gpaw.kpoint import KPoint
+from gpaw.utilities.blas import axpy
+
+class LCAOKPoint(KPoint):
+    """Special KPoint class for LCAO calculation"""
+    
+    def __init__(self, nuclei, gd, weight, s, k, u, k_c, typecode):
+        KPoint.__init__(self, gd, weight, s, k, u, k_c, typecode)
+        self.nuclei = nuclei
+        
+    def add_to_density(self, nt_G):
+        """Add contribution to pseudo electron-density."""
+        psit_G = self.gd.zeros()
+        for n in range(self.nbands):
+            psit_G[:] = 0.0
+            m1 = 0
+            for nucleus in self.nuclei:
+                niao = nucleus.get_number_of_atomic_orbitals()
+                m2 = m1 + niao
+                nucleus.phit_i.add(psit_G, self.C_nm[n, m1:m2])
+                m1 = m2
+            axpy(self.f_n[n], psit_G**2, nt_G)  # nt_G += f * psit_G**2
+
+
+
+        
