458e5c97ead5f4317727a9f6d5aafd8abc19e8cc
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2007-10-10 07:31:16 +0000

    expandyl test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1182 6419b465-d902-0410-997c-be2266baf723

---
+++ test/analyse/yl.py
@@ -0,0 +1,37 @@
+import Numeric as num
+
+from ASE import Atom
+
+from gpaw.utilities import equal
+from gpaw.cluster import Cluster
+from gpaw import Calculator
+from gpaw.analyse.expandyl import ExpandYl
+
+R = 1.
+H2 = Cluster([Atom('H',(0,0,0)), Atom('H',(0,0,R))])
+H2.minimal_box(3.)
+
+calc = Calculator(h=0.2, width=0.01, nbands=2)
+H2.SetCalculator(calc)
+H2.GetPotentialEnergy()
+
+yl = ExpandYl(H2.center_of_mass(), calc.gd, Rmax=2.5)
+gl = []
+for n in range(calc.nbands):
+    psit_G = calc.kpt_u[0].psit_nG[n]
+    norm = calc.gd.integrate(psit_G**2)
+    g = yl.expand(psit_G)
+    gsum = num.sum(g)
+
+    # allow for 10 % inaccuracy in the norm
+    print "norm, sum=", norm, gsum
+    equal(norm, gsum, 0.1)
+    
+    gl.append(g/gsum*100)
+
+# 1 sigma_g has s-symmetry mainly
+print gl[0]
+equal( gl[0][0], 100, 10)
+# 1 sigma_u has p-symmetry mainly
+print gl[1]
+equal( gl[1][1], 100, 10)
