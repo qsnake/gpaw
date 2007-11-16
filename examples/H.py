9454be1e6bb0ab89172012f4a8ef3176f259374c
lauri <lauri@6419b465-d902-0410-997c-be2266baf723>
2007-11-16 15:12:45 +0000

    Directory demo changed to examples
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1291 6419b465-d902-0410-997c-be2266baf723

---
+++ examples/H.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from ASE import Atom, ListOfAtoms
+from gpaw import Calculator
+
+a = 5.0
+H = ListOfAtoms([Atom('H',(a/2, a/2, a/2), magmom=1)],
+                periodic=False,
+                cell=(a, a, a))
+
+H.SetCalculator(Calculator(nbands=1, h=0.2, convergence={'eigenstates': 1e-3}))
+e = H.GetPotentialEnergy()
