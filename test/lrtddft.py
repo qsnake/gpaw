62ff28aca5594ef1928c46a40799aeef6a927d0f
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2007-11-16 12:01:00 +0000

    more tests for more coverage
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1282 6419b465-d902-0410-997c-be2266baf723

--- test/lrtddft.py
+++ test/lrtddft.py
@@ -1,3 +1,5 @@
+import os
+
 from gpaw import Calculator
 from gpaw.utilities import equal
 from ASE import ListOfAtoms, Atom, Crystal
@@ -13,16 +15,36 @@ calc = Calculator(xc='PBE',nbands=2,spinpol=False)
 H2.SetCalculator(calc)
 H2.GetPotentialEnergy()
 
+xc='LDA'
+
 # without spin
-lr = LrTDDFT(calc,xc=None)
+lr = LrTDDFT(calc,xc=xc)
 lr.diagonalize()
 t1 = lr[0]
 
 # with spin
-lr = LrTDDFT(calc,xc=None,nspins=2)
+lr = LrTDDFT(calc,xc=xc,nspins=2)
 lr.diagonalize()
 # the triplet is lower, so that the second is the first singlet
 # excited state
-t2 = lr[1] 
+t2 = lr[1]
 
 equal(t1.get_energy(), t2.get_energy(), 5.e-7)
+
+# course grids
+for finegrid in [1,0]:
+    lr = LrTDDFT(calc, xc=xc, finegrid=finegrid)
+    lr.diagonalize()
+    t3 = lr[0]
+    print "finegrid, t1, t3=", finegrid, t1.get_energy(),t3.get_energy()
+    equal(t1.get_energy(), t3.get_energy(), 5.e-4)
+
+# io
+fname = 'lr.dat.gz'
+lr.write(fname)
+lr = LrTDDFT(filename=fname)
+os.remove(fname)
+t4 = lr[0]
+
+equal(t3.get_energy(), t4.get_energy(), 1.e-7)
+
