1a2c0b2506f43b3f5355c49b2ca36253a0718ddf
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2007-11-29 10:44:50 +0000

    Updated the g2 test script
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1341 6419b465-d902-0410-997c-be2266baf723

--- scripts/g2test.py
+++ scripts/g2test.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python
 from gpaw import Calculator
 from gpaw.testing.g2 import get_g2, atoms
 from gpaw.testing.atomization_data import atomization_vasp
 from ASE.Utilities.Parallel import paropen
 
 cell = [12., 13., 14.]
-calc = Calculator(h=.18, xc='PBE', txt='g2test.txt',
-                  convergence={'energy': 1e-3,
-                               'density': 1e-3,
-                               'eigenstates': 1e-9,
-                               'bands': 'occupied'})
-
 data = paropen('g2data.txt', 'w')
-for formula in atoms.keys() + atomization_vasp.keys():
+systems = atoms.keys() + atomization_vasp.keys()
+
+for formula in systems:
     loa = get_g2(formula, cell)
+    calc = Calculator(h=.18,
+                      nbands=-5,
+                      xc='PBE',
+                      fixmom=True,
+                      txt=formula + '.txt')
+    if len(loa) == 1:
+        calc.set(hund=True)
     loa.SetCalculator(calc)
-    energy = loa.GetPotentialEnergy()
-    print >>data, formula, repr(energy)
+    try:
+        energy = loa.GetPotentialEnergy()
+    except:
+        print >>data, formula, 'Error'
+    else:
+        print >>data, formula, repr(energy)
+        calc.write(formula + '.gpw', mode='all')
