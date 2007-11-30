f792f53d98de2479122ff405672e90bad5f884d6
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2007-11-30 11:27:02 +0000

    This is the correct value for the latests setups - do not change it unless you know what you are doing
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1344 6419b465-d902-0410-997c-be2266baf723

--- test/spinpol.py
+++ test/spinpol.py
@@ -6,7 +6,7 @@ from gpaw.utilities import equal
 a = 4.0
 n = 16
 hydrogen = Crystal([Atom('H', (0.0, 0.0, 0.0))], cell=(a, a, a))
-calc = Calculator(gpts=(n, n, n), nbands=1, convergence={'eigenstates': 1e-10})
+calc = Calculator(gpts=(n, n, n), nbands=1, convergence={'energy': 1e-5})
 hydrogen.SetCalculator(calc)
 e1 = hydrogen.GetPotentialEnergy()
 calc.set(spinpol=True)
@@ -14,5 +14,4 @@ hydrogen[0].SetMagneticMoment(1.0)
 e2 = hydrogen.GetPotentialEnergy()
 de = e1 - e2
 print de
-# equal(de, 0.7929, 1.5e-3)
-equal(de, 0.7949, 1.e-4)
+equal(de, 0.7918, 1.e-4)
