8e13ae03755ec42f2b7e5ec1fd99c951c48ffb1f
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2007-12-03 11:07:35 +0000

    This is not the right fix, but I guess it's ok for now
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1356 6419b465-d902-0410-997c-be2266baf723

--- test/IP-oxygen.py
+++ test/IP-oxygen.py
@@ -10,7 +10,7 @@ calc = Calculator(nbands=4, h=0.2, charge=0, hund=1, fixdensity=0)
 o.SetCalculator(calc)
 e0 = o.GetPotentialEnergy()
 
-o = ListOfAtoms([Atom('O', (a/2, a/2 + 0.5, a/2), magmom=3)],
+o = ListOfAtoms([Atom('O', (a/2, a/2 + 0.5, a/2), magmom=2)],
                 periodic=0,
                 cell=(a, a + 1, a))
 calc = Calculator(nbands=4, h=0.2, charge=1)
