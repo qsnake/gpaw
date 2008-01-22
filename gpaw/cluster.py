631743ac8564fd69b0885d2821dfaa7d06e9b714
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2008-01-22 12:04:05 +0000

    revival of Cluster - still something missing in ase.atoms
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1525 6419b465-d902-0410-997c-be2266baf723

--- gpaw/cluster.py
+++ gpaw/cluster.py
@@ -2,78 +2,30 @@ import math
 import re
 import numpy as npy
 
-from ASE import Atom, ListOfAtoms
-from ASE.Utilities.GeometricTransforms import Translate as GTTranslate
-from ASE.Utilities.GeometricTransforms import RotateAboutAxis
-from ASE.IO.xyz import ReadXYZ, WriteXYZ
+from ase import Atom, Atoms
+from ase.io.xyz import read_xyz, write_xyz
 from ASE.IO.PDB import WritePDB
 from gpaw.io.Cube import ReadListOfAtomsFromCube
 from gpaw.utilities.vector import Vector3d
 
-class Cluster(ListOfAtoms):
+class Cluster(Atoms):
     """A class for cluster structures
     to enable simplified manipulation"""
 
     def __init__(self, atoms=None, cell=None,
-                 filename=None, filetype=None,
-                 timestep=0.0):
-        print 'Please use standard Atoms class from ase package!'
+                 filename=None, filetype=None):
         if atoms is None:
-            ListOfAtoms.__init__(self,[],cell=cell)
-        else:
-            ListOfAtoms.__init__(self,atoms, cell=cell,periodic=False)
+            atoms = []
+        Atoms.__init__(self,atoms, cell=cell)
 
         if filename is not None:
-            self.read(filename,filetype)
-
-        self.timestep(timestep)
-        
-    def __add__(self, other):
-        assert(type(other) == type(self))
-        result = Cluster(self.Copy())
-        for a in other:
-            result.append(a.Copy())
-        return result
-
-    def __ladd__(self, other):
-        print "__ladd__"
-        return self.add(other)
-        
-    def __radd__(self, other):
-        print "__radd__"
-        return self.add(other)
-        
-    def Center(self):
-        """Center the structure to unit cell"""
-        extr = self.extreme_positions()
-        cntr = 0.5 * (extr[0] + extr[1])
-        cell = npy.diagonal(self.GetUnitCell())
-        GTTranslate(self,tuple(.5*cell-cntr),'cartesian')
-
-    def center_of_mass(self):
-        """Return the structures center of mass"""
-        cm = npy.zeros((3,))
-        M = 0.
-        for atom in self:
-            m = atom.GetMass()
-            M += m
-            cm += m * atom.GetCartesianPosition()
-        return cm/M
-
-    def Copy(self):
-        return self.copy()
-
-    def copy(self):
-        return Cluster(ListOfAtoms.Copy(self))
+            self.read(filename, filetype)
     
     def extreme_positions(self):
         """get the extreme positions of the structure"""
-        pos = self.GetCartesianPositions()
+        pos = self.get_positions()
         return npy.array([npy.minimum.reduce(pos),npy.maximum.reduce(pos)])
 
-    def MinimalBox(self,border=0):
-        return self.minimal_box(border)
-    
     def minimal_box(self,border=0):
         """The box needed to fit the structure in.
         The structure is moved to fit into the box [(0,x),(0,y),(0,z)]
@@ -97,10 +49,10 @@ class Cluster(ListOfAtoms):
             extr[1][i]+=b[i]-extr[0][i] # shifted already
             
         # move lower corner to (0,0,0)
-        GTTranslate(self,tuple(-1.*npy.array(extr[0])),'cartesian')
-        self.SetUnitCell(tuple(extr[1]),fix=True)
+        self.translate(tuple(-1.*npy.array(extr[0])))
+        self.set_cell(tuple(extr[1]), fix=True)
 
-        return self.GetUnitCell()
+        return self.get_cell()
 
     def read(self,filename,filetype=None):
         """Read the strcuture from some file. The type can be given
@@ -115,45 +67,14 @@ class Cluster(ListOfAtoms):
             loa = ReadListOfAtomsFromCube(filename)
             self.__init__(loa)
         elif filetype == 'xyz':
-            loa = ReadXYZ(filename)
+            loa = read_xyz(filename)
             self.__init__(loa)
         else:
             raise NotImplementedError('unknown file type "'+filetype+'"')
                 
         return len(self)
 
-    def rotate(self, axis, angle=None, unit='rad'):
-        """Rotate the structure about the given axis with the given angle.
-        Note, that the right hand rule applies: If your right thumb points
-        into the direction of the axis, the other fingers show the rotation
-        direction."""
-        axis=Vector3d(axis)
-        if angle is None:
-            angle = axis.length()
-        axis.length(1.)
-        use_RAA=True
-        use_RAA=False
-        if use_RAA:
-            if unit == 'rad':
-                angle *= 180. / math.pi
-            RotateAboutAxis(self, axis, -angle)
-        else:
-            for a in self:
-                v=Vector3d(a.GetCartesianPosition())
-                v.rotate(axis, angle)
-                a.SetCartesianPosition(v)
-
-    def timestep(self,timestep=None):
-        """Set and/or get the timestep label of this structure"""
-        if timestep is not None:
-            self.ts = float(timestep)
-        return self.ts
-
-    def translate(self,trans_vector):
-        """Translate the whole structure"""
-        GTTranslate(self,tuple(trans_vector),'cartesian')
-
-    def write(self,filename,filetype=None):
+    def write(self, filename, filetype=None):
         """Write the strcuture to file. The type can be given
         or it will be guessed from the filename."""
 
@@ -165,14 +86,7 @@ class Cluster(ListOfAtoms):
         if filetype == 'pdb':
             WritePDB(filename,self)
         elif filetype == 'xyz':
-            uc = self.GetUnitCell()
-            if uc:
-                id=' unit cell'
-                for v in uc:
-                    id+=' (%g,%g,%g)' % (v[0],v[1],v[2])
-            WriteXYZ(filename,self,id=id)
-        elif filetype == 'pdb':
-            WritePDB(filename,self)
+            write_xyz(filename, self)
         else:
             raise NotImplementedError('unknown file type "'+filetype+'"')
                 
--- test/cluster.py
+++ test/cluster.py
@@ -1,5 +1,6 @@
 from ase import *
 #from gpaw.utilities.vector import Vector3d
+from gpaw.cluster import Cluster
 from gpaw.utilities import equal
 
 R = 2.0
@@ -21,3 +22,27 @@ q = CO.positions.copy()
 for c in range(3):
     equal(q[0, c], p[0, 0] / sqrt(3), 1e-10)
     equal(q[1, c], p[1, 0] / sqrt(3), 1e-10)
+
+# minimal box
+b=4.0
+CO = Cluster([Atom('C', (1, 0, 0)), Atom('O', (1, 0, R))])
+CO.minimal_box(b)
+cc = CO.get_cell() 
+for c in range(3):
+    width = 2*b
+    if c==2:
+        width += R
+    equal(cc[c, c], width, 1e-10)
+
+# I/O
+fxyz='CO.xyz'
+fpdb='CO.pdb'
+
+CO.write(fxyz)
+CO_b = Cluster(filename=fxyz)
+ 
+## CO.write(fpdb)
+## CO2=Cluster(filename=fpdb)
+
+## os.remove(fpdb)
+os.remove(fxyz)
