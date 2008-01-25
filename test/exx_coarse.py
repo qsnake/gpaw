5e35071aa806b5a04fbccc5e9bb42d2b8b854712
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2008-01-25 08:12:24 +0000

    Exx on the coarse grid
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1540 6419b465-d902-0410-997c-be2266baf723

---
+++ test/exx_coarse.py
@@ -0,0 +1,41 @@
+import sys
+
+from ase import Atoms, Atom
+
+from gpaw import Calculator
+from gpaw.utilities import equal
+from gpaw.utilities.timing import Timer
+
+timer = Timer()
+
+loa = Atoms([Atom('Be', (0, 0, 0)), Atom('Be', (2.45, 0, 0))],
+            cell= [5.9, 4.8, 5.0])
+loa.center()
+
+fgl = [False, True]
+#fgl = [True, False]
+
+txt='-'
+txt='/dev/null'
+
+E = {}
+for fg in fgl:
+    if fg:
+        tstr = 'Exx on fine grid'
+    else:
+        tstr = 'Exx on coarse grid'
+    timer.start(tstr)
+    calc = Calculator(h = .3, xc={ 'name':'PBE0', 'finegrid': fg },
+                      nbands=4,
+                      convergence={'eigenstates': 1e-4},
+                      charge=-1)
+    loa.set_calculator(calc)
+    E[fg] = loa.get_potential_energy()
+    timer.stop(tstr)
+
+timer.write()
+
+print 'Total energy on the fine grid   =', E[True]
+print 'Total energy on the coarse grid =', E[False]
+equal(E[True], E[False], 0.02)
+
