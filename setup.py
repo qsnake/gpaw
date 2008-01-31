487808952afa824dca9fa68ccbef865b7ca52436
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2008-01-31 06:35:31 +0000

    Also install the gpaw-basis script
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1557 6419b465-d902-0410-997c-be2266baf723

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ extension = Extension('_gpaw',
                       extra_objects=extra_objects)
 
 scripts = [join('tools', script)
-           for script in ('gpaw-setup', 'gpaw')]
+           for script in ('gpaw-setup', 'gpaw-basis', 'gpaw')]
 
 write_configuration(define_macros, include_dirs, libraries, library_dirs,
                     extra_link_args, extra_compile_args,
