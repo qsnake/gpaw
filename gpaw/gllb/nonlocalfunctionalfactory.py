7d3ffec0d7b77ddbc2616b9ba527bd4efa78b327
kuismam <kuismam@6419b465-d902-0410-997c-be2266baf723>
2008-02-27 14:21:04 +0000

    Updated this file also... here are different kinds of approximations, which I have to test.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1643 6419b465-d902-0410-997c-be2266baf723

--- gpaw/gllb/nonlocalfunctionalfactory.py
+++ gpaw/gllb/nonlocalfunctionalfactory.py
@@ -6,44 +6,24 @@ class NonLocalFunctionalFactory:
     
     It contains a method called get_functional_by_name, which takes
     the xc-name for non-local functional and returns the corresponding
-    XCNonLocalFunctional object. Currently there are 5 keywords
-    available: 
+    GLLBFunctional object. 
 
     * GLLB (The fermi-level reference set to HOMO)
-    * GLLBLUMO (The fermi-level reference set to LUMO)
-    * GLLBRC (GLLB with relaxed core response weights)
-    * GLLBplusC (GLLB with P86 correlation potential)
-    * GLLBplusCLUMO (GLLBplusC + fermi-level reference at LUMO)
-    * GLLBSlaterCore (Currently disabled)
-    * KLI (Currently disabled)
+    * GLLBplusC (GLB with PW91 correlation scr-potential) 
+    * GLLBexp (Something to play with)
     """
 
     def get_functional_by_name(self, name):
         print "Functional name", name
+        K_G = 0.382106112167171
+        KC_G = 0.470
+        from gpaw.gllb.gllb import GLLBFunctional
         if name == 'GLLB':
-            from gpaw.gllb.gllb import GLLBFunctional
-            return GLLBFunctional()
-        elif name == 'GLLBRC':
-            from gpaw.gllb.gllb import GLLBFunctional
-            return GLLBFunctional(relaxed_core_response=True)
+            return GLLBFunctional('X_B88-None',None, K_G)
+        elif name == 'GLLBexp':
+            return GLLBFunctional('X_LDA-None','None-C_VWN', KC_G)
         elif name == 'GLLBplusC':
-            from gpaw.gllb.gllb import GLLBFunctional
-            return GLLBFunctional(correlation=True)
-        elif name == 'GLLBLUMO':
-            from gpaw.gllb.gllb import GLLBFunctional
-            return GLLBFunctional(lumo=True)
-        elif name == 'GLLBRCLUMO':
-            from gpaw.gllb.gllb import GLLBFunctional
-            return GLLBFunctional(lumo=True, relaxed_core_response=True)
-        elif name == 'GLLBplusCLUMO':
-            from gpaw.gllb.gllbc import GLLBCFunctional
-            return GLLBCFunctional(lumo=True)
-        #elif name == 'GLLBSlaterCore':
-        #    from gpaw.gllb.gllbsc import GLLBSlaterCoreFunctional
-        #    return GLLBSlaterCoreFunctional()
-        #elif name == 'KLI':
-        #    from gpaw.gllb.kli import KLIFunctional
-        #    return KLIFunctional()
+            return GLLBFunctional('X_B88-C_PW91',None,KC_G)
         else:
             raise RuntimeError('Unkown NonLocal density functional: ' + name)
 
