cdb64e47a421906092de3ddb97385a8ba46aeed4
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2008-03-07 11:22:21 +0000

    can read a trajectory from gpaw-text now
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1702 6419b465-d902-0410-997c-be2266baf723

--- gpaw/utilities/viewmol.py
+++ gpaw/utilities/viewmol.py
@@ -1,85 +1,140 @@
+"""Trajectory module with viewmol support.""" 
+
 from math import sqrt
 import numpy as npy
 
-from ase import Angstrom, Atom, Hartree, PickleTrajectory
+from ase import Angstrom, Atoms, Atom, Hartree, PickleTrajectory
+from ase.io import filetype as ase_filetype
+from ase.parallel import paropen
+from ase.io.gpawtext import read_gpaw_text
 
 from gpaw.cluster import Cluster
 import gpaw.mpi as mpi
 MASTER = 0
 
-class ViewmolTrajectory2(list):
-    def __add__(self, other):
-        for loa in other:
-            self.append(loa)
-    
-    def __init__(self, filename=None):
-        if filename:
+def filetype_from_ending(filename):
+    # estimate file type from name ending
+    ft = filename.split('.')[-1]
+    return ft.lower()
+
+class Trajectory(PickleTrajectory):
+    """Basic trajectory class.
+
+    In the moment, this class inherits from ase.PickleTrajectory.
+    Might be other way round in some unknown future."""
+
+    def __init__(self, filename=None, mode='r', atoms=None):
+        self.images = []
+        if filename is not None:
             self.read(filename)
-    
-    def read(self, filename='trajectory.vmol'):
-        
-        f = open(filename)
-
-        # read the definition first 
-        definition=False
-        for line in f:
-            w = line.split()
-            if not definition:
-                if w[0] == '$coord':
-                    definition=True
-                    self.scale = float(w[1])
-                    loa = Cluster([])
-            else:
-                if w[0] == '$grad':
-                    # definition ends here
-                    self.definition = loa
-                    break
-                else:
-                    # we assume this is a coordinate entry
-                    coo = (float(w[0]),  float(w[1]), float(w[2]))
-                    loa.append(Atom(w[3], coo))
-##        print "<read> loa=", len(loa)
-
-        # get the iterations            
-        cycle = False
-        for line in f:
-            w = line.split()
-            if not cycle:
-                # search for the cycle keyword
-                if w[0] == 'cycle=':
-                    cycle=True
-                    n_coo=0
-                    n_F=0
-                    self.append(Cluster([]))
-            else:
-                if n_coo < len(self.definition):
-                    n_coo += 1
-                    coo = (float(w[0]),  float(w[1]), float(w[2]))
-                    self[-1].append(Atom(w[3], coo))
-                elif n_F < len(self.definition):
-                    F = (float(w[0]),  float(w[1]), float(w[2]))
-                    self[-1][n_F].F = F
-                    n_F += 1
-                    if n_F == len(self.definition):
-                        cycle=False
-        
-    # ASE interface
-    def GetListOfAtoms(self, frame):
-#        return self.definition
-        return self[frame]
-
-    def write(self, filename='trajectory.vmol', filetype=None):
-        if filetype is None:
-            # estimate file type from name ending
-            filetype = filename.split('.')[-1]
-        filetype.lower()
-        
-        if filetype == 'xyz':
-             WriteXYZ(filename, trajectory=self)
         else:
-            raise NotImplementedError('unknown file type "'+filetype+'"')
+            self.set_atoms(atoms)
 
-class ViewmolTrajectory:
+    def __len__(self):
+        if hasattr(self, 'offsets'):
+            return PickleTrajectory.__len__(self)
+        else:
+            return len(self.images)
+
+    def __del__(self):
+        pass # overwrite PickleTrajectory.__del__
+        
+    def __getitem__(self, i=-1):
+        if hasattr(self, 'offsets'):
+            return PickleTrajectory.__getitem__(self, i)
+        else:
+            return self.images[i]
+
+    def __iter__(self):
+        if hasattr(self, 'offsets'):
+            return PickleTrajectory.__iter__(self)
+        else:
+            return self.images.__iter__()
+
+    def next(self):
+        if hasattr(self, 'offsets'):
+            return PickleTrajectory.next(self)
+        else:
+            return self.images.next()
+
+    def append(self, images):
+        if type(images) == type(Atoms):
+            images = [images]
+        for image in images:
+            self.images.append(image)
+        
+    def read(self, filename, filetype=None):
+        """Read from a file"""
+
+        aft = ase_filetype(filename)
+        
+        if aft == 'traj':
+            PickleTrajectory.__init__(self, filename)
+        elif aft == 'gpaw-text':
+            # this is strange code, but I don't know how to create the full
+            # slice XXXXX
+            last = read_gpaw_text(filename)
+            self.__init__(atoms=last)
+            s = slice(-1)
+            rest = read_gpaw_text(filename, s)
+            rest.append(last)
+            self.append(rest)
+        else:
+            raise NotImplementedError('unknown file type "'+ aft +'"')
+
+    def write(self, filename, filetype=None):
+        """Write to a file"""
+
+        if filetype is None:
+            filetype = filetype_from_ending(filename)
+        if filetype == 'vmol':
+            self.write_viewmol(filename)
+        else:
+            raise NotImplementedError
+    
+    def write_viewmol(self, filename, mode='w'):
+        """Write yourself in viewmol style."""
+
+        f = paropen(filename, mode)
+        if mode == 'w':
+            print >> f, ' $coord', 1. / Angstrom
+            self.write_viewmol_positions(f, self[0])
+            print >> f, ' $grad'
+        else:
+            raise NotImplementedError('append')
+
+        for i, atoms in enumerate(self):
+            self.write_viewmol_atoms(f, atoms, i)
+            
+        f.close()
+
+    def write_viewmol_atoms(self, file, atoms, index):
+        """Write current atomic position, energy and forces
+        to the output file"""
+        print >> file, 'cycle=', index,
+        try:
+            E = atoms.get_potential_energy() * Hartree
+        except:
+            E = 0
+        print >> file, 'SCF energy=', E,
+        try:
+            forces = atoms.get_forces() * (Hartree / Angstrom)
+            max_force = 0
+            for f in forces:
+                max_force = max(max_force, sqrt(npy.sum(f * f)))
+        except:
+            forces = atoms.get_positions() * 0.
+            max_force = 0
+        print >> file, '|max dE/dxyz|=', max_force
+        self.write_viewmol_positions(file, atoms)
+        for atom, f in zip(atoms, forces):
+            print >> file, '%10.4g %10.4g %10.4g' % (f[0],f[1],f[2])
+
+    def write_viewmol_positions(self, file, atoms):
+        for c, s in zip(atoms.get_positions(), atoms.get_chemical_symbols()):
+            print  >> file, '%10.4f %10.4f %10.4f' % (c[0],c[1],c[2]), s
+        
+class ViewmolTrajectory(Trajectory):
     """Write a trajectory for viewmol (http://viewmol.sourceforge.net)
 
     You can attach the writing to the Calculator:
@@ -103,8 +158,8 @@ class ViewmolTrajectory:
             self.file = open(filename, mode)
         else:
             self.file = open('/dev/null', mode)
-        print >> self.file, ' $coord', 1. / self.Ang
-        self.write_atoms(self.atoms)
+        print >> self.file, ' $coord', 1. / Angstrom
+        self.write_viewmol_positions(self.file, self.atoms)
         print >> self.file, ' $grad'
     
     def __del__(self):
@@ -118,23 +173,17 @@ class ViewmolTrajectory:
         self.n += 1
         print >> self.file, 'cycle=', self.n,
         print >> self.file, 'SCF energy=', \
-              atoms.get_potential_energy() * self.Ha,
-        forces = atoms.get_forces() * (self.Ha / self.Ang)
+              atoms.get_potential_energy() * Hartree,
+        forces = atoms.get_forces() * (Hartree / Angstrom)
         max_force = 0
         for f in forces:
             max_force = max(max_force, sqrt(npy.sum(f * f)))
         print >> self.file, '|max dE/dxyz|=', max_force
-        self.write_atoms(atoms)
+        self.write_viewmol_positions(self.file, atoms)
         for atom, f in zip(self.atoms, forces):
             print >> self.file, '%10.4g %10.4g %10.4g' % (f[0],f[1],f[2])
         self.file.flush()
        
-    def write_atoms(self, atoms):
-        for atom in atoms:
-            c = atom.get_positions()[0]
-            print  >> self.file, '%10.4f %10.4f %10.4f' % (c[0],c[1],c[2]),\
-                      atom.get_chemical_symbols()[0]
-
     def read(self, filename='trajectory.vmol', position=0):
         """Read atom configurations of step position"""
         self.file = None
