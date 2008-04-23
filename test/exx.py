b30ede2dddd98e49da3ae1943ea5f1424059ea5e
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-04-23 07:45:50 +0000

    Reinserted extra PBE, and updated test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1872 6419b465-d902-0410-997c-be2266baf723

--- test/exx.py
+++ test/exx.py
@@ -12,20 +12,32 @@ calc = Calculator(h=0.21, nbands=3, convergence={'eigenstates': 1e-6},
                   txt='Be2.txt')
 loa.set_calculator(calc)
 
-tolerance = 0.0003 # must reproduce old gpaw results
-tolerance = 30.0003 # must reproduce old gpaw results
+ref_1871 = { # Values from revision 1871. Not true reference values
+    # xc         Energy          eigenvalue 0    eigenvalue 1
+    'PBE' : (   5.36043449926, -3.86524565488, -0.989950689923),
+    'PBE0': (-790.98523889100, -5.09079115492, -1.826444460780),
+    'EXX' : (-785.59780385600, -7.21083921161, -2.782087866830),
+    }
 
-#for setup in ['LDA', 'PBE']:#, 'oldPBE', 'LDA']:
-for setup in ['PBE', 'PBE0', 'EXX']:#, 'oldPBE', 'LDA']:
+current = {} # Current revision
+for setup in ['PBE', 'PBE0', 'EXX', 'PBE']:#, 'oldPBE', 'LDA']:
     # Generate setup
     #g = Generator('Be', setup, scalarrel=True, nofiles=True, txt=None)
     #g.run(exx=True, **parameters['Be'])
 
-    # setup gpaw calculation
+    # switch to new xc functional
     calc.set(xc=setup)
     E = loa.get_potential_energy()
-    bands = calc.get_eigenvalues()
-
-    print setup, E
-    for i in range(2): # not 3 as unoccupied eigenvalues are random!?? XXX
-        print i, bands[i]
+    bands = calc.get_eigenvalues()[:2] # not 3 as unocc. eig are random!? XXX
+    res = (E,) + tuple(bands)
+    print setup, res
+    
+    if setup in current:
+        for first, second in zip(current[setup], res):
+            equal(first, second, 2e-3)
+    else:
+        current[setup] = res
+        
+for setup in current:
+    for ref, cur in zip(ref_1871[setup], current[setup]):
+        equal(ref, cur, 2e-3)
