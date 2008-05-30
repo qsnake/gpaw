a2ee9eb95f6645c6d841e1ab9fb5bc9465696148
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2008-05-30 06:30:12 +0000

    Fixing Numeric-to-numpy-switch bug - part 3
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@1974 6419b465-d902-0410-997c-be2266baf723

--- gpaw/xas.py
+++ gpaw/xas.py
@@ -480,10 +480,8 @@ class RecursionMethod:
 
         if len(self.swaps) > 0:
             sigma0_cn = sigma_cn
-            print sigma_cn, sigma_cn.shape, self.dim, n
             sigma_cn = npy.zeros((self.dim, n))
             for swap in self.swaps:
-                print swap
                 sigma_cn += sigma0_cn.take(swap, axis=0)
             sigma_cn /= len(self.swaps)
 
