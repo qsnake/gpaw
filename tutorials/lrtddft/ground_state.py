ab616665f34a813666338b1a05cd7fa6b6464c85
jussie <jussie@6419b465-d902-0410-997c-be2266baf723>
2008-06-04 16:51:34 +0000

    Small updates to tutorials
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2002 6419b465-d902-0410-997c-be2266baf723

--- tutorials/lrtddft/ground_state.py
+++ tutorials/lrtddft/ground_state.py
@@ -1,18 +1,15 @@
 from gpaw import *
 from ase import *
-from gpaw.tddft import *
-
 
 # Sodium dimer, Na2
-d = 1.55
+d = 1.5
 atoms = Atoms( symbols='Na2',
                positions=[( 0, 0, d),
                           ( 0, 0,-d)],
                pbc=False)
 
-
-atoms.center(vacuum=4.0) # excited states might need more vacuum
-calc = Calculator(nbands=1, h=0.30, txt='Na2_gs.txt')
+atoms.center(vacuum=4.0) # For real calculations use larger vacuum (e.g. 6)
+calc = Calculator(nbands=1, h=0.35, txt='Na2_gs.txt')
 atoms.set_calculator(calc)
 e = atoms.get_potential_energy()
 
@@ -22,5 +19,4 @@ calc.set(nbands=20, convergence={'bands': 'all'},
          fixdensity=True)
 e = atoms.get_potential_energy()
 # write the wave functions to a file
-calc.write('na2_gs.gpw', 'all')
-
+calc.write('na2_gs_unocc.gpw', 'all')
