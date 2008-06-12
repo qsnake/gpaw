232767d74d72715a3871b6cf1b11c77f330cffe3
jussie <jussie@6419b465-d902-0410-997c-be2266baf723>
2008-06-12 10:06:19 +0000

    Small modification...
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2018 6419b465-d902-0410-997c-be2266baf723

--- tutorials/optimization/relax.py
+++ tutorials/optimization/relax.py
@@ -11,12 +11,13 @@ print 'hydrogen molecule energy: %5.2f eV' % e2
 print 'bondlength              : %5.2f Ang' % d0
 
 # Find the theoretical bond length:
-relax = QuasiNewton(molecule)
+relax = QuasiNewton(molecule, logfile='qn.log')
 relax.run(fmax=0.05)
 
 e2 = molecule.get_potential_energy()
 d0 = molecule.distance(0, 1)
 
+print
 print 'PBE energy minimum:'
 print 'hydrogen molecule energy: %5.2f eV' % e2
 print 'bondlength              : %5.2f Ang' % d0
