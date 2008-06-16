29369c5ed1f455384e91c3b563ecff09e14b2d28
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2008-06-16 10:04:28 +0000

    New Li setup. h=0.06, kpts=[6,6,6] PBE LiF rocksalt: lattice constant=4.062 A, bulk modulus=69.0 GPa
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2028 6419b465-d902-0410-997c-be2266baf723

--- gpaw/atom/generator.py
+++ gpaw/atom/generator.py
@@ -21,7 +21,7 @@ from gpaw.atom.filter import Filter
 parameters = {
  'H' : {'rcut': 0.9},
  'He': {'rcut': 1.5},
- 'Li': {'core': '[He]',   'rcut': 2.1},
+ 'Li': {'core': '[He]',   'rcut': 1.9},
  'Be': {'core': '[He]',   'rcut': 1.5},
  'B' : {'core': '[He]',   'rcut': 1.2},
  'C' : {'core': '[He]',   'rcut': 1.2},
