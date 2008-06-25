7c328bcfeae18685c4e358cba57f18ee1a696211
mathiasl <mathiasl@6419b465-d902-0410-997c-be2266baf723>
2008-06-25 21:55:53 +0000

    added xps calculation in h2o_dks.py test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2066 6419b465-d902-0410-997c-be2266baf723

--- test/h2o_dks.py
+++ test/h2o_dks.py
@@ -8,7 +8,6 @@ from gpaw import setup_paths
 g = Generator('O', xcname='PBE', scalarrel=True,
               corehole=(1, 0, 1.0), nofiles=True)
 g.run(name='fch1s', **parameters['O'])
-setup_paths.insert(0, '.')
 
 atoms = get_g2('H2O')
 atoms.center(vacuum=2.5)
@@ -18,8 +17,16 @@ atoms.set_calculator(calc)
 e1 = atoms.get_potential_energy() + calc.Eref * Hartree
 
 atoms[0].magmom = 1
-calc.set(charge=-1, setups={'O': 'fch1s'})
+calc.set(charge=-1, setups={'O': 'fch1s'},fixmom=True,spinpol=True)
 e2 = atoms.get_potential_energy() + calc.Eref * Hartree
 
+atoms[0].magmom = 0
+calc.set(charge=0, setups={'O': 'fch1s'},fixmom=True,spinpol=True)
+e3 = atoms.get_potential_energy() + calc.Eref * Hartree
+
+
 print 'Energy difference %.3f eV' % (e2 - e1)
+print 'XPS %.3f eV' % (e3 - e1)
+
 assert abs(e2 - e1 - 533.117) < 0.001
+assert abs(e3 - e1 - 538.623) < 0.001 
