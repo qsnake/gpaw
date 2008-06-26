cf0a82eb238565527b65092309c3485ff3d61a8e
hahansen <hahansen@6419b465-d902-0410-997c-be2266baf723>
2008-06-26 14:46:42 +0000

    one more Al exercise
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2074 6419b465-d902-0410-997c-be2266baf723

---
+++ examples/aluminium/Al_fcc_vs_bcc.py
@@ -0,0 +1,39 @@
+""" Compare Al(fcc) and Al(bcc) at two different grid spacings"""
+
+from ase import *
+from gpaw import *
+
+afcc = 3.985
+abcc = 3.190
+
+for h in [0.25, 0.20]:
+    #fcc
+    bulk =  Atoms(symbols='4Al', pbc=True,
+                  positions=[(.0, .0, .0),
+                             (.5, .5, .0),
+                             (.0, .5, .5),
+                             (.5, .0, .5)])
+    bulk.set_cell([afcc,afcc,afcc],scale_atoms=True)
+    calc = Calculator(nbands=16,
+                      txt='bulk-fcc-h%.2f.txt' % h,
+                      h=h ,
+                      kpts=(6, 6, 6))
+    bulk.set_calculator(calc)
+    Efcc=bulk.get_potential_energy()
+    #bcc
+    bulk = Atoms(symbols='2Al', pbc=True,
+                 positions=[(0, 0, 0),
+                            (.5, .5, .5)])
+    bulk.set_cell((abcc, abcc, abcc), scale_atoms=True)
+    calc = Calculator(nbands=8,
+                      txt='bulk-bcc-h%.2f.txt' % h,
+                      h=h,
+                      kpts=(8, 8, 8))
+    bulk.set_calculator(calc)
+    Ebcc=bulk.get_potential_energy()
+    print h, Efcc/4., Ebcc/2., 0.25*Efcc-0.5*Ebcc
+
+# 0.25 -4.17322125175 -4.08445636201 -0.0887648897448
+# 0.20 -4.17337174875 -4.0846250688  -0.088746679949
+# The total energy is converged within ca. 1e-4 eV/atom
+# The energy difference is convereged within ca. 1e-5 eV/atom
