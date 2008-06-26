d9c15cda86e80e61534b08c13624e47b2887befa
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-06-26 09:02:17 +0000

    Solution to lattice convergence exercise
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2068 6419b465-d902-0410-997c-be2266baf723

---
+++ examples/aluminium/Al_lattice.py
@@ -0,0 +1,24 @@
+"""Lattice parameter convergence test"""
+
+from ase import *
+from gpaw import *
+
+bulk = Atoms(symbols='4Al', pbc=True,
+             positions=[(.0, .0, .0),
+                        (.5, .5, .0),
+                        (.0, .5, .5),
+                        (.5, .0, .5)])
+
+calc = Calculator(nbands=16,  gpts=(16, 16, 16), kpts=(6, 6, 6))
+for a in [3.9, 4.0, 4.1, 4.2]:
+    calc.set(txt='bulk-%.1f.txt' % a)
+    bulk.set_cell((a, a, a), scale_atoms=True)
+    bulk.set_calculator(calc)
+    print a, bulk.get_potential_energy()
+
+# run: ag bulk-*.txt
+# Choose 'Tools -> Bulk Modulus' to get
+
+# B = 85.823 GPa, and
+# V = 63.270 A^3 <=> a = 3.985
+
