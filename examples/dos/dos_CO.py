43bb76133f650a82ce3ad48070367f2891d0ce3a
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-07-11 08:39:54 +0000

    More solutions for dos exercise
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2139 6419b465-d902-0410-997c-be2266baf723

--- examples/dos/dos_CO.py
+++ examples/dos/dos_CO.py
@@ -1,8 +1,8 @@
 import pylab as p
 from gpaw import Calculator
 
-calc = Calculator('Al-fcc.gpw', txt=None)
-p.plot(*calc.get_dos(width=.5))
+calc = Calculator('CO.gpw', txt=None)
+p.plot(*calc.get_dos(width=.1))
 p.axis('tight')
 p.xlabel(r'$\epsilon - \epsilon_F \ \rm{(eV)}$')
 p.ylabel('Density of States (1/eV)')
