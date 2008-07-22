b7a95d51387741e8af0623a498945acffe62e990
hahansen <hahansen@6419b465-d902-0410-997c-be2266baf723>
2008-07-22 14:07:24 +0000

    labels on dos plot
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2187 6419b465-d902-0410-997c-be2266baf723

--- examples/dos/dos.py
+++ examples/dos/dos.py
@@ -15,4 +15,6 @@ if calc.get_number_of_spins() == 2:
     energy, dos = calc.get_dos(spin=1, width=width)
     pylab.plot(energy, dos)
     pylab.legend(('up', 'down'), loc='upper left')
+pylab.xlabel(r'$\epsilon - \epsilon_F \ \rm{(eV)}$')
+pylab.ylabel('Density of States (1/eV)')
 pylab.show()
