471a3f717e362b17dcc925b8a5b1bafc3d5abdb0
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-08-04 07:06:43 +0000

    Added a few milestones
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2257 6419b465-d902-0410-997c-be2266baf723

--- tools/code-count.py
+++ tools/code-count.py
@@ -5,12 +5,14 @@ import glob
 import pylab
 import datetime
 
+# Milestones:
+# Rev    1: 10/19/05 16:35:46  start revision log
+# Rev  383: 08/28/06 08:21:40  gridpaw -> gpaw
+# Rev  887: 07/11/07 10:33:37  libxc introduced
+# Rev 2050: 06/20/08 09:54:24  /doc in svn
 date1 = datetime.date(2005, 10, 19)
-date2 = datetime.date(2008,  7,  7)
-date1 = datetime.date(2008, 1, 19)
-date2 = datetime.date(2008,  7,  7)
-#date2 = datetime.date(2005,  10, 21)
-delta = datetime.timedelta(days=100)
+date2 = datetime.date.today()
+delta = datetime.timedelta(days=1)
 dates = pylab.drange(date1, date2, delta)
 
 def count(dir, pattern):
@@ -18,8 +20,11 @@ def count(dir, pattern):
         return 0
     p = os.popen('wc -l `find %s -name %s` | tail -1' % (dir, pattern), 'r')
     return int(p.read().split()[0])
-dates = [date1, date2]
+
 stat = open('stat2.dat', 'w')
+date1 = datetime.date(2008, 1, 19)
+date2 = datetime.date(2008,  7,  7)
+dates = [date1, date2]
 dates = [datetime.date(y, m, 1) for y in range(2005,2008) for m in range(1,13)]
 dates = [datetime.date(2008, m, d) for m in range(1,8) for d in range(1,30,5)]
 for date in dates:
