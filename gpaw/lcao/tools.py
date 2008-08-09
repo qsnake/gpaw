93e72e1c589018f0191b1e88f7754e5ca7ead389
strange <strange@6419b465-d902-0410-997c-be2266baf723>
2008-08-09 06:49:03 +0000

    added varies tools for transport calculations
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2321 6419b465-d902-0410-997c-be2266baf723

---
+++ gpaw/lcao/tools.py
@@ -0,0 +1,103 @@
+from gpaw.utilities import unpack
+from ase import Hartree
+import pickle
+import numpy as npy
+from gpaw.mpi import world, MASTER, rank
+
+
+def tri2full(M,UL='L'):
+    """UP='L' => fill upper triangle from lower triangle
+       such that M=M^d"""
+    nbf = len(M)
+    if UL=='L':
+        for i in range(nbf-1):
+            M[i,i:] = M[i:,i].conjugate()
+    elif UL=='U':
+        for i in range(nbf-1):
+            M[i:,i] = M[i,i:].conjugate()
+
+def get_bf_centers(atoms):
+    calc = atoms.get_calculator()
+    if not calc.initialized:
+        calc.initialize(atoms)
+    nbf = calc.nao
+    pos_ac = atoms.get_positions()
+    #print pos_ac
+    natoms = len(pos_ac)
+    pos_ic = npy.zeros((nbf,3),npy.float)
+    index = 0
+    for a in xrange(natoms):
+        nao = calc.nuclei[a].get_number_of_atomic_orbitals()
+        pos_c = pos_ac[a]
+        pos_c.shape = (1,3)
+        pos_ic[index:index+nao] = npy.repeat(pos_c,nao,axis=0)
+        index += nao
+    return pos_ic
+
+
+def remove_pbc(atoms,h,s):
+    calc = atoms.get_calculator()
+    if not calc.initialized:
+        calc.initialize(atoms)
+    nbf = calc.nao
+    
+    cutoff = atoms.get_cell()[0,0] * 0.5 #XXX x-direction assump.
+    pos_i = get_bf_centers(atoms)[:,0]
+    for i in xrange(nbf):
+        dpos_i = npy.absolute(pos_i-pos_i[i])
+        mask_i = (dpos_i < cutoff).astype(npy.int)
+        h[i,:] = h[i,:] * mask_i
+        h[:,i] = h[:,i] * mask_i
+        s[i,:] = s[i,:] * mask_i
+        s[:,i] = s[:,i] * mask_i
+
+
+def dump_lcao_hamiltonian(calc, filename='hs.pickle'): # "parallel" dump of H
+    if calc.master:
+        # Dump calulation info on master
+        fd = open(filename, 'wb')
+        pickle.dump((calc.nao, calc.nspins, calc.ibzk_kc, calc.weight_k), 
+                    fd, protocol=2) 
+        fd.close()
+    world.barrier()
+        
+    if calc.kpt_comm.rank == 0:
+        Vt_skmm = calc.eigensolver.Vt_skmm
+        ef = calc.get_fermi_level()
+        hamiltonian = calc.hamiltonian
+        eigensolver = calc.eigensolver
+        hamiltonian.calculate_effective_potential_matrix(Vt_skmm) 
+        if calc.domain.comm.rank == 0:
+            fd = open(filename, 'ab')
+    world.barrier()
+    
+    if calc.kpt_comm.rank == 0:
+        for s in range(calc.nspins):
+            for k in range(len(calc.ibzk_kc)):
+                h_mm = eigensolver.get_hamiltonian_matrix(hamiltonian,ks=(k,s))
+                s_mm = hamiltonian.S_kmm[k]#XXX writes twice when spin!!
+                tri2full(h_mm)
+                tri2full(s_mm)
+                h_mm *= Hartree
+                h_mm -= ef * s_mm
+                hs_dmm = npy.array((h_mm,s_mm))
+                if calc.domain.comm.rank == 0:
+                    pickle.dump(hs_dmm, fd, 2)
+                
+        if calc.domain.comm.rank == 0:
+            fd.close()
+
+def load_lcao_hamiltonian(filename): # serial version only
+    fd = open(filename, 'rb')
+    nao, nspins, ibzk_kc, weight_k = pickle.load(fd)
+    nkpt = len(ibzk_kc)
+    HS_dskmm = npy.empty((2, nspins, nkpt, nao, nao))
+
+    for s in range(nspins):
+        for k in range(nkpt):
+            HS_dskmm[:,s,k,:,:] = pickle.load(fd)
+        
+    fd.close()
+    return nao, ibzk_kc, weight_k, HS_dskmm
+
+
