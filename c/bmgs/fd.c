ccdb2f31a8ca35f33025b3d81d91cacb2b746bc0
madsbk <madsbk@6419b465-d902-0410-997c-be2266baf723>
2008-08-13 12:01:52 +0000

    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2357 6419b465-d902-0410-997c-be2266baf723

--- c/bmgs/fd.c
+++ c/bmgs/fd.c
@@ -3,33 +3,37 @@
 void Z(bmgs_fd)(const bmgsstencil* s, const T* a, T* b)
 {
   a += (s->j[0] + s->j[1] + s->j[2]) / 2;
+
   for (int i0 = 0; i0 < s->n[0]; i0++)
     {
+      T* aa = a + i0 * (s->j[1] + s->n[1] * (s->j[2] + s->n[2]));
+      T* bb = b + i0 * s->n[1] * s->n[2];
+
       for (int i1 = 0; i1 < s->n[1]; i1++)
-	{
-	  for (int i2 = 0; i2 < s->n[2]; i2++)
-	    {
-#if defined(BMGSCOMPLEX) && defined(NO_C99_COMPLEX)
-	      double x = 0.0;
-	      double y = 0.0;
-	      for (int c = 0; c < s->ncoefs; c++)
-		{
-		  x += a[s->offsets[c]].r * s->coefs[c];
-		  y += a[s->offsets[c]].i * s->coefs[c];
-		}
-	      (*b).r = x;
-	      (*b).i = y;
-	      b++;
-#else
-	      T x = 0.0;
-	      for (int c = 0; c < s->ncoefs; c++)
-		x += a[s->offsets[c]] * s->coefs[c];
-	      *b++ = x;
-#endif
-	      a++;
-	    }
-	  a += s->j[2];
-	}
-      a += s->j[1];
+        {
+          for (int i2 = 0; i2 < s->n[2]; i2++)
+            {
+            #if defined(BMGSCOMPLEX) && defined(NO_C99_COMPLEX)
+              double x = 0.0;
+              double y = 0.0;
+              for (int c = 0; c < s->ncoefs; c++)
+                {
+                  x += a[s->offsets[c]].r * s->coefs[c];
+                  y += a[s->offsets[c]].i * s->coefs[c];
+                }
+              (*b).r = x;
+              (*b).i = y;
+              b++;
+            #else
+              T x = 0.0;
+              for (int c = 0; c < s->ncoefs; c++)
+                x += aa[s->offsets[c]] * s->coefs[c];
+              *bb++ = x;
+            #endif
+              aa++;
+            }
+          aa += s->j[2];
+        }
+      aa += s->j[1];
     }
 }
