5cd9536ff63a295b06ff37a5f3bb81d9617aea22
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-08-14 06:46:56 +0000

    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2367 6419b465-d902-0410-997c-be2266baf723

--- doc/exercises/transport/pt_h2.py
+++ doc/exercises/transport/pt_h2.py
@@ -6,21 +6,22 @@ import os
 a = 2.41
 b = 0.9
 c = 1.7
-L = 7
+L = 7.
 l = L * 0.5
 N = 5
 
-atoms1 = Atoms('Pt',[(0.0,l,l)],pbc=True,cell=(a,L,L))
-atoms1 *= (N,1,1)
+atoms1 = Atoms('Pt',[(.0, l, l)], pbc=True, cell=(a, L, L))
+atoms1 *= (N, 1, 1)
 atoms2 = atoms1.copy()
-atoms2.translate(((N-1)*a+b+2*c,0,0))
-h2 = Atoms('H2',[((N-1)*a+c,l,l),((N-1)*a+c+b,l,l)],pbc=True)
+atoms2.translate(((N - 1) * a + b + 2 * c, 0, 0))
+h2 = Atoms('H2', pbc=True, positions=[((N - 1) * a + c    , l, l),
+                                      ((N - 1) * a + c + b, l, l)])
 
 atoms = atoms1 + h2 + atoms2
-atoms.set_cell([(2*N-1)*a+b+2*c,L,L])
+atoms.set_cell([(2 * N - 1) * a + b + 2 * c, L, L])
 
-pov_options = {'display' : False}
-write('pt_h2.png', atoms, show_unit_cell=2, pause=False,**pov_options)
+pov_options = {'display': False, 'transparent': False}
+write('pt_h2.pov', atoms, show_unit_cell=2, **pov_options)
 os.system('povray pt_h2.ini')
 
 
