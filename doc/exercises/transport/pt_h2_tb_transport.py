d1b7d26656c3813f6e82b7766874c844af43a6e9
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-08-14 11:53:13 +0000

    Use pylab to plot results
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2378 6419b465-d902-0410-997c-be2266baf723

--- doc/exercises/transport/pt_h2_tb_transport.py
+++ doc/exercises/transport/pt_h2_tb_transport.py
@@ -1,5 +1,6 @@
 import numpy as npy
 from ase.transport.calculators import TransportCalculator
+import pylab
 
 # onsite energies 0.0, nearest neighbor hopping -1.0, and
 # second nearest neighbor hopping 0.2
@@ -26,42 +27,27 @@ tcalc = TransportCalculator(h=H_scat,  #Scattering Hamiltonian
 
 tcalc.set(energies=npy.arange(-3, 3, 0.02))
 T_e = tcalc.get_transmission()
+pylab.plot(tcalc.energies, T_e)
+pylab.show()
 
 tcalc.set(pdos=[0, 1])
 pdos_ne = tcalc.get_pdos()
+pylab.plot(tcalc.energies, pdos_ne[0], ':')
+pylab.plot(tcalc.energies, pdos_ne[1], '--')
+pylab.show()
 
 h_rot, s_rot, eps_n, vec_nn = tcalc.subdiagonalize_bfs([0, 1])
-tcalc.set(h=h_rot, s=s_rot)#Set the rotated matrices
+tcalc.set(h=h_rot, s=s_rot) # Set the rotated matrices
 for n in range(2):
     print "eigenvalue, eigenvector:", eps_n[n],',', vec_nn[:, n]
 
 pdos_rot_ne = tcalc.get_pdos()
+pylab.plot(tcalc.energies, pdos_rot_ne[0], ':')
+pylab.plot(tcalc.energies, pdos_rot_ne[1], '--')
+pylab.show()
 
 h_cut, s_cut = tcalc.cutcoupling_bfs([0])
 tcalc.set(h=h_cut, s=s_cut)
 T_cut_bonding_e = tcalc.get_transmission()
-
-#dump the date to txt files.
-fd = file('T.dat','w')
-for e,t in zip(tcalc.energies, T_e):
-    print >> fd, e, t
-fd.close()
-
-fd = file('T_cut_bonding.dat','w')
-for e,t in zip(tcalc.energies, T_cut_bonding_e):
-    print >> fd, e, t
-fd.close()
-
-for n in range(2):
-    fd = file('pdos%i.dat' % n,'w')
-    for e,p in zip(tcalc.energies,pdos_ne[n]):
-        print >> fd, e, p
-fd.close()
-
-for n in range(2):
-    fd = file('pdos_rot%i.dat' % n,'w')
-    for e,p in zip(tcalc.energies,pdos_rot_ne[n]):
-        print >> fd, e, p
-fd.close()
-
-
+pylab.plot(tcalc.energies, T_cut_bonding_e)
+pylab.show()
