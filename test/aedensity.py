3d0e76de1fbf148c00ee44575a8b28cd1649ce51
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2008-09-01 07:36:11 +0000

    use file from last call
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2458 6419b465-d902-0410-997c-be2266baf723

--- test/aedensity.py
+++ test/aedensity.py
@@ -5,19 +5,24 @@ from ase import *
 from gpaw import Calculator
 from gpaw.utilities import equal
 
-h = 0.17 # gridspacing
-a = [6.5, 6.5, 7.7] # unit cell
-d = 2.3608 # experimental bond length
+try:
+    calc = Calculator('NaCl.gpw')
+    NaCl = calc.get_atoms()
+except:
+    h = 0.17 # gridspacing
+    a = [6.5, 6.5, 7.7] # unit cell
+    d = 2.3608 # experimental bond length
 
-NaCl = Atoms([Atom('Na', [0, 0, 0]),
-              Atom('Cl', [0, 0, d])],
-             pbc=False, cell=a)
-NaCl.center()
-calc = Calculator(h=h, xc='LDA', nbands=5, lmax=0,
-                  convergence={'eigenstates': 1e-6}, spinpol=1)
+    NaCl = Atoms([Atom('Na', [0, 0, 0]),
+                  Atom('Cl', [0, 0, d])],
+                 pbc=False, cell=a)
+    NaCl.center()
+    calc = Calculator(h=h, xc='LDA', nbands=5, lmax=0,
+                      convergence={'eigenstates': 1e-6}, spinpol=1)
 
-NaCl.set_calculator(calc)
-NaCl.get_potential_energy()
+    NaCl.set_calculator(calc)
+    NaCl.get_potential_energy()
+    calc.write('NaCl.gpw')
 
 nt = calc.get_pseudo_valence_density()
 gridrefinement = 2 # grid-refinement-factor for all-electron density
