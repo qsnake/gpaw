13995b9c1f155ce51a625b235d92fbd0bb3a73bc
madsbk <madsbk@6419b465-d902-0410-997c-be2266baf723>
2008-09-02 14:25:49 +0000

    Added fragile files - files who cant be compiled with -O5
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2466 6419b465-d902-0410-997c-be2266baf723

--- bg_compiler.py
+++ bg_compiler.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
-"""gcc2xlc.py is a wrapper for the xlc compiler, 
+"""bg_compiler.py is a wrapper for the xlc compiler, 
    converting/removing incompatible gcc args.   """
-
 
 import sys
 from subprocess import call
-
 
 args2change = {"-fno-strict-aliasing":"",
                "-fmessage-length=0":"",
@@ -26,15 +24,24 @@ args2change = {"-fno-strict-aliasing":"",
                "-O2":"",
                "-O1":""}
 
-cmd = "mpixlc_r -O3 -qhot=nosimd -qlanglvl=extc99 -qnostaticlink -qsmp -qarch=450d -qtune=450 -qflag=e:e"
+fragile_files = ["test.c"]
 
+cmd = ""
+fragile = False
 for arg in sys.argv[1:]:
-	cmd += " "
-	t = arg.strip()
-	if t in args2change:
-		cmd += args2change[t]
-	else:
-		cmd += arg
+    cmd += " "
+    t = arg.strip()
+    if t in fragile_files:
+        fragile = True
+    if t in args2change:
+        cmd += args2change[t]
+    else:
+        cmd += arg
+if fragile:
+    flags = "-O3 -qhot=nosimd -qlanglvl=extc99 -qnostaticlink -qsmp -qarch=450d -qtune=450 -qflag=e:e"
+else:
+    flags = "-O5 -qhot=nosimd -qlanglvl=extc99 -qnostaticlink -qsmp -qarch=450d -qtune=450 -qflag=e:e"
+cmd = "mpixlc_r %s %s"%(flags, cmd)
 
 #print "\nexecmd: %s\n"%cmd
 call(cmd, shell=True)
