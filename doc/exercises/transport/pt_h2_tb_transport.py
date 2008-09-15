aa69c3552b81f316ec8d58ca70a845bf197e84e4
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-09-15 13:04:30 +0000

    Updated TB exercies. Grid-lcao version does not work right now.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2489 6419b465-d902-0410-997c-be2266baf723

--- doc/exercises/transport/pt_h2_tb_transport.py
+++ doc/exercises/transport/pt_h2_tb_transport.py
@@ -22,20 +22,22 @@ H_scat[1, 2] = H_scat[2, 1] = H_scat[3, 4] = H_scat[4, 3] = 0.2
 
 tcalc = TransportCalculator(h=H_scat,  # Scattering Hamiltonian
                             h1=H_lead, # Lead 1 (left)
-                            h2=H_lead) # Lead 2  (right)
+                            h2=H_lead, # Lead 2  (right)
+                            energies=npy.arange(-3, 3, 0.02))
 
-tcalc.set(energies=npy.arange(-3, 3, 0.02))
 T_e = tcalc.get_transmission()
 pylab.plot(tcalc.energies, T_e)
+pylab.title('Transmission function')
 pylab.show()
 
-tcalc.set(pdos=[0, 1])
+tcalc.set(pdos=[2, 3])
 pdos_ne = tcalc.get_pdos()
 pylab.plot(tcalc.energies, pdos_ne[0], ':')
 pylab.plot(tcalc.energies, pdos_ne[1], '--')
+pylab.title('Projected density of states')
 pylab.show()
 
-h_rot, s_rot, eps_n, vec_nn = tcalc.subdiagonalize_bfs([0, 1])
+h_rot, s_rot, eps_n, vec_nn = tcalc.subdiagonalize_bfs([2, 3])
 tcalc.set(h=h_rot, s=s_rot) # Set the rotated matrices
 for n in range(2):
     print "eigenvalue, eigenvector:", eps_n[n],',', vec_nn[:, n]
@@ -43,10 +45,12 @@ for n in range(2):
 pdos_rot_ne = tcalc.get_pdos()
 pylab.plot(tcalc.energies, pdos_rot_ne[0], ':')
 pylab.plot(tcalc.energies, pdos_rot_ne[1], '--')
+pylab.title('Projected density of states (rotated)')
 pylab.show()
 
-h_cut, s_cut = tcalc.cutcoupling_bfs([0])
+h_cut, s_cut = tcalc.cutcoupling_bfs([2])
 tcalc.set(h=h_cut, s=s_cut)
 T_cut_bonding_e = tcalc.get_transmission()
 pylab.plot(tcalc.energies, T_cut_bonding_e)
+pylab.title('Transmission (bonding orbital cut)')
 pylab.show()
