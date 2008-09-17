6071c77ae06053c648d1d5776002a7868e902a49
strange <strange@6419b465-d902-0410-997c-be2266baf723>
2008-09-17 09:14:18 +0000

    removed methods which are not used anymore
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2496 6419b465-d902-0410-997c-be2266baf723

--- gpaw/lcao/tools.py
+++ gpaw/lcao/tools.py
@@ -46,7 +46,7 @@ def get_realspace_hs(h_skmm,s_kmm, ibzk_kc, weight_k, R_c=(0,0,0)):
     s_mm[:] = npy.sum((s_kmm * c_k).real, axis=0)
     return h_smm, s_mm
 
-def remove_pbc(atoms, h, s, d=0):
+def remove_pbc(atoms, h, s=None, d=0):
     calc = atoms.get_calculator()
     if not calc.initialized:
         calc.initialize(atoms)
@@ -59,8 +59,9 @@ def remove_pbc(atoms, h, s, d=0):
         mask_i = (dpos_i < cutoff).astype(int)
         h[i,:] = h[i,:] * mask_i
         h[:,i] = h[:,i] * mask_i
-        s[i,:] = s[i,:] * mask_i
-        s[:,i] = s[:,i] * mask_i
+        if s != None:
+            s[i,:] = s[i,:] * mask_i
+            s[:,i] = s[:,i] * mask_i
 
 def dump_hamiltonian(filename, atoms, direction=None):
     
@@ -69,16 +70,30 @@ def dump_hamiltonian(filename, atoms, direction=None):
         d = {'x':0, 'y':1, 'z':2}[direction]
         for s in range(atoms.calc.nspins):
             for k in range(atoms.calc.nkpts):
-                remove_pbc(atoms, h_skmm[s,k], s_kmm[k], d) #XXX SPIN
+                if s==0:
+                    remove_pbc(atoms, h_skmm[s,k], s_kmm[k], d)
+                else:
+                    remove_pbc(atoms, h_skmm[s,k], None, d)
+
     
     if atoms.calc.master:
         fd = file(filename,'wb')
         pickle.dump((h_skmm, s_kmm), fd, 2)
+        atoms_data = {'cell':atoms.cell, 'positions':atoms.positions,
+                      'numbers':atoms.numbers, 'pbc':atoms.pbc}
+        
+        pickle.dump(atoms_data, fd, 2)
+        calc_data = {'input_parameters':atoms.calc.input_parameters,
+                     'weight_k':atoms.calc.weight_k, 
+                     'ibzk_kc':atoms.calc.ibzk_kc}
+        
+        pickle.dump(calc_data, fd, 2)
         fd.close()
 
     world.barrier()
 
 def get_hamiltonian(atoms):
+    """Calculate the Hamiltonian and overlap matrix."""
     calc = atoms.calc
     Ef = calc.get_fermi_level()
     eigensolver = calc.eigensolver
@@ -105,53 +120,5 @@ def get_hamiltonian(atoms):
             h_skmm[s,k] -= Ef * s_kmm[k]
 
     return h_skmm, s_kmm
-
-def dump_lcao_hamiltonian(calc, filename): # "parallel" dump of H
-    if calc.master:
-        # Dump calulation info on master
-        fd = open(filename, 'wb')
-        pickle.dump((calc.nao, calc.nspins, calc.ibzk_kc, calc.weight_k), 
-                    fd, protocol=2) 
-        fd.close()
-    world.barrier()
-        
-    if calc.kpt_comm.rank == 0:
-        Vt_skmm = calc.eigensolver.Vt_skmm
-        ef = calc.get_fermi_level()
-        hamiltonian = calc.hamiltonian
-        eigensolver = calc.eigensolver
-        hamiltonian.calculate_effective_potential_matrix(Vt_skmm) 
-        if calc.domain.comm.rank == 0:
-            fd = open(filename, 'ab')
-    world.barrier()
-    
-    if calc.kpt_comm.rank == 0:
-        for s in range(calc.nspins):
-            for k in range(len(calc.ibzk_kc)):
-                h_mm = eigensolver.get_hamiltonian_matrix(hamiltonian,k=0,s=0)
-                s_mm = hamiltonian.S_kmm[k]#XXX writes twice when spin!!
-                tri2full(h_mm)
-                tri2full(s_mm)
-                h_mm *= Hartree
-                h_mm -= ef * s_mm
-                hs_dmm = npy.array((h_mm,s_mm))
-                if calc.domain.comm.rank == 0:
-                    pickle.dump(hs_dmm, fd, 2)
-                
-        if calc.domain.comm.rank == 0:
-            fd.close()
-
-def load_lcao_hamiltonian(filename): # serial version only
-    fd = open(filename, 'rb')
-    nao, nspins, ibzk_kc, weight_k = pickle.load(fd)
-    nkpt = len(ibzk_kc)
-    HS_dskmm = npy.empty((2, nspins, nkpt, nao, nao))
-
-    for s in range(nspins):
-        for k in range(nkpt):
-            HS_dskmm[:, s, k, :, :] = pickle.load(fd)
-        
-    fd.close()
-    return nao, ibzk_kc, weight_k, HS_dskmm
 
 
