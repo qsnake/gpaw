43f6a14d4b76c3d067b6844817cb4b81cbea32d3
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-10-17 10:04:31 +0000

    Clean-up of IP_oxygen test, and added a comment about the calc.set(charge=??) bug
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2607 6419b465-d902-0410-997c-be2266baf723

--- test/IP-oxygen.py
+++ test/IP-oxygen.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 from ase import *
-from gpaw import Calculator
+from gpaw import GPAW
 
 a = 6.0
-o = Atoms([Atom('O', (a/2, a/2 + 0.5, a/2), magmom=2)],
-                pbc=0,
-                cell=(a, a + 1, a))
-calc = Calculator(nbands=4, h=0.2, charge=0, hund=1, fixdensity=0)
-o.set_calculator(calc)
-e0 = o.get_potential_energy()
+calc = GPAW(nbands=4)
+O = Atoms([Atom('O', (a/2, a/2 + 0.5, a/2), magmom=2)],
+          pbc=False, cell=(a, a + 1, a), calculator=calc)
+e0 = O.get_potential_energy()
 
-o = Atoms([Atom('O', (a/2, a/2 + 0.5, a/2), magmom=2)],
-                pbc=0,
-                cell=(a, a + 1, a))
-calc = Calculator(nbands=4, h=0.2, charge=1)
-o.set_calculator(calc)
-e1 = o.get_potential_energy()
+# calc.set(charge=1) # XXX For some reason changing charge doesn't reset WF
+O.set_calculator(GPAW(nbands=4, charge=1)) # XXX should not be needed
+
+e1 = O.get_potential_energy()
 
 print e1 - e0
 assert abs(e1 - e0 - 13.989) < 0.04
