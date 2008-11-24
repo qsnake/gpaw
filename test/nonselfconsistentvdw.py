a3661cf073fb1b0507a1a6aeeb5c268eb42f5fa6
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2008-11-24 14:11:48 +0000

    Only run vdw-test if VDW envvar is set.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2758 6419b465-d902-0410-997c-be2266baf723

--- test/nonselfconsistentvdw.py
+++ test/nonselfconsistentvdw.py
@@ -7,20 +7,24 @@ from gpaw.atom.generator import Generator, parameters
 from gpaw.xc_functional import XCFunctional
 from gpaw import setup_paths
 
-# Generate setup
-symbol = 'He'
-if rank == 0:
-    g = Generator(symbol, 'revPBE', scalarrel=True, nofiles=True)
-    g.run(exx=True, **parameters[symbol])
-barrier()
-setup_paths.insert(0, '.')
+def test():
+    # Generate setup
+    symbol = 'He'
+    if rank == 0:
+        g = Generator(symbol, 'revPBE', scalarrel=True, nofiles=True)
+        g.run(exx=True, **parameters[symbol])
+    barrier()
+    setup_paths.insert(0, '.')
 
-a = 7.5 * Bohr
-n = 16
-atoms = Atoms([Atom('He', (0.0, 0.0, 0.0))], cell=(a, a, a), pbc=True)
-calc = Calculator(gpts=(n, n, n), nbands=1, xc='revPBE')
-atoms.set_calculator(calc)
-e1 = atoms.get_potential_energy()
-calc.write('He')
-e2 = e1 + calc.get_xc_difference('vdWDF')
-print e1, e2
+    a = 7.5 * Bohr
+    n = 16
+    atoms = Atoms([Atom('He', (0.0, 0.0, 0.0))], cell=(a, a, a), pbc=True)
+    calc = Calculator(gpts=(n, n, n), nbands=1, xc='revPBE')
+    atoms.set_calculator(calc)
+    e1 = atoms.get_potential_energy()
+    calc.write('He')
+    e2 = e1 + calc.get_xc_difference('vdWDF')
+    print e1, e2
+
+if 'VDW' in os.environ:
+    test()
