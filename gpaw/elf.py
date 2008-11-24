43aac7b413a3ef21b5c4b100561072c70fad49c3
s032082 <s032082@6419b465-d902-0410-997c-be2266baf723>
2008-11-24 13:59:17 +0000

    Correct expression for ELF both for spinpol and non-spinpol. Weizsacker term still uncertain, but loooks alright.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2757 6419b465-d902-0410-997c-be2266baf723

--- gpaw/elf.py
+++ gpaw/elf.py
@@ -6,10 +6,32 @@ from numpy import pi
 from ase.units import Bohr, Hartree
 from gpaw.operators import Gradient
 
-def _elf(nt, nt_grad2, taut, ncut, elf):
+def _elf(nt, nt_grad2, taut, ncut, spinpol, elf):
+    """Pseudo electron localisation function (ELF) as defined in
+    Becke and Edgecombe, J. Chem. Phys., vol 92 (1990) 5397
 
-    D0 = 3.0/5.0*(6*pi**2)**(2.0/3.0)*nt**(5.0/3.0)
-    D = 2.0*taut - 0.25*nt_grad2/nt
+    Arguments:
+     =============== =====================================================
+     ``nt``          Pseudo valence density.
+     ``nt_grad2``    Squared norm of the density gradient.
+     ``tau``         Kinetic energy density.
+     ``ncut``        Minimum density cutoff parameter.
+     ``spinpol``     Boolean indicator for spin polarization.
+     ``elf``         Empty grid to storing ELF values in.
+     =============== =====================================================
+    """
+
+    # Uniform electron gas value of D, Becke eq. (13). TODO! explain 3/10 not 3/5!!!
+    if spinpol:
+        D0 = 3.0/10.0*(6*pi**2)**(2.0/3.0)*nt**(5.0/3.0)
+    else:
+        D0 = 3.0/10.0*(3*pi**2)**(2.0/3.0)*nt**(5.0/3.0)
+
+    # Note: The definition of tau in Becke eq. (9) misses the
+    # factor 1/2, hence it is twice that of the GPAW implementation.
+
+    #TODO! Weizsacker correction factor - extra 0.5 factor appended!!!
+    D = taut - 0.125*nt_grad2/nt*0.5
 
     elf[:] = 1.0/(1.0+(D/D0)**2.0)
 
@@ -43,6 +65,7 @@ class ELF:
         self.gd = calc.gd
         self.finegd = calc.finegd
 
+        self.spinpol = calc.spinpol
         self.nspins = calc.occupation.nspins
 
         self.density = calc.density
@@ -89,20 +112,22 @@ class ELF:
 
     def get_electronic_localization_function(self, spin=0, gridrefinement=1):
         assert self.initialized and self.updated, 'ELF instance is not initialized or updated'
+        assert spin in range(0,self.nspins), 'Invalid spin index'
 
         if gridrefinement==1:
             elf_G = self.gd.empty()
-            _elf(self.density.nt_sG[spin], self.nt_grad2_sG[spin], 2.0/self.nspins*self.density.taut_sG[spin], self.ncut, elf_G)
+            _elf(self.density.nt_sG[spin], self.nt_grad2_sG[spin], self.density.taut_sG[spin], self.ncut, self.spinpol, elf_G)
             return elf_G
         elif gridrefinement==2:
             elf_g = self.finegd.empty()
-            _elf(self.density.nt_sg[spin], self.nt_grad2_sg[spin], 2.0/self.nspins*self.density.taut_sg[spin], self.ncut, elf_g)
+            _elf(self.density.nt_sg[spin], self.nt_grad2_sg[spin], self.density.taut_sg[spin], self.ncut, self.spinpol, elf_g)
             return elf_g
         else:
             raise NotImplementedError, 'Arbitrary grid refinement is not implemented!'
 
     def get_kinetic_energy_density(self, spin=0, gridrefinement=1):
         assert self.initialized and self.updated, 'ELF instance is not initialized or updated'
+        assert spin in range(0,self.nspins), 'Invalid spin index'
 
         # Returns kinetic energy density in eV / Ang^3
         if gridrefinement==1:
