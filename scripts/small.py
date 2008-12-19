771e4300b18f2405b8d64168fdbe5796d1571208
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2008-12-19 17:27:58 +0000

    Minor maintanaince on the small.py script
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@2861 6419b465-d902-0410-997c-be2266baf723

--- scripts/small.py
+++ scripts/small.py
@@ -1,20 +1,17 @@
-from gpaw import Calculator
-from gpaw.utilities import center
-from gpaw.testing.g2 import get_g2
+from gpaw import GPAW
+from ase.data.molecules import molecule
 
 formula = 'H'
 cell = [5.5, 5.6, 5.7]
 calc_parameters = {'h'          : 0.2,
                    'xc'         : 'PBE',
                    'txt'        : '-',
-                   'mix'        : (0.25, 3, 1.0),
                    'lmax'       : 2,
                    'nbands'     : 2,
                    'setups'     : {'Li': 'nocore'},
-                   'stencils'   : (2, 'M', 3),
                    'convergence': {'eigenstates': 1e-9},
                    'eigensolver': 'rmm-diis'}
 
-loa = get_g2(formula, cell)
-loa.SetCalculator(Calculator(**calc_parameters))
-loa.GetPotentialEnergy()
+loa = molecule(formula, cell=cell, calculator=GPAW(**calc_parameters))
+loa.center()
+loa.get_potential_energy()
