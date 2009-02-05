6c47f47d29a942015a6119f3563fb10a1fa308af
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2009-02-05 10:21:52 +0000

    Fixing too long line
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3088 6419b465-d902-0410-997c-be2266baf723

--- examples/H.py
+++ examples/H.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from ase import *
-from gpaw import Calculator
+from gpaw import GPAW
 
 a = 5.0
 H = Atoms([Atom('H',(a/2, a/2, a/2), magmom=1)],
                 pbc=False,
                 cell=(a, a, a))
 
-H.set_calculator(Calculator(nbands=1, h=0.2, convergence={'eigenstates': 1e-3}))
+H.set_calculator(GPAW(nbands=1, h=0.2, convergence={'eigenstates': 1e-3}))
 e = H.get_potential_energy()
