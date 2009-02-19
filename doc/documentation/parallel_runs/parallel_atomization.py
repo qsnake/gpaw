866076ff965c518d5113c3c8a8c94ed17cabc5df
strange <strange@6419b465-d902-0410-997c-be2266baf723>
2009-02-19 10:50:20 +0000

    updated to ase and numpy from Numeric and ASE.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3216 6419b465-d902-0410-997c-be2266baf723

--- doc/documentation/parallel_runs/parallel_atomization.py
+++ doc/documentation/parallel_runs/parallel_atomization.py
@@ -2,41 +2,41 @@
 processes, each process working on a separate system."""
 
 from gpaw import GPAW, mpi
-import Numeric as num
-from ASE import ListOfAtoms, Atom
+import numpy as np
+from ase import Atoms, Atom
 
 cell = (8., 8., 8.)
-pos = 4.
+p = 4.
 separation = 1.103
 
 rank = mpi.world.rank
 
 # Master process calculates energy of N, while the other one takes N2
 if rank == 0:
-    system = ListOfAtoms([Atom('N', (pos, pos, pos), magmom=3)], cell=cell)
+    system = Atoms('N', [(p, p, p)], magmoms=[3], cell=cell)
 elif rank == 1:
-    system = ListOfAtoms([Atom('N', (pos, pos, pos + separation/2.)),
-                          Atom('N', (pos, pos, pos - separation/2.))],
-                         cell=cell)
+    system = Atoms('N2', [(p, p, p + separation / 2.),
+                          (p, p, p - separation / 2.)], 
+                   cell=cell)
 else:
     raise Exception('This example uses only two processes')
 
 # Open different files depending on rank
 output = '%d.txt' % rank
-
-calc = GPAW(communicator=[rank], txt=output, xc='PBE')
-system.SetCalculator(calc)
-energy = system.GetPotentialEnergy()
+comm = mpi.world.new_communicator(np.array([rank]))
+calc = GPAW(communicator=comm, txt=output, xc='PBE')
+system.set_calculator(calc)
+energy = system.get_potential_energy()
 
 # Now send the energy from the second process to the first process,
 if rank == 1:
     # Communicators work with arrays from Numeric only:
-    mpi.world.send(num.array([energy]), 0)
+    mpi.world.send(np.array([energy]), 0)
 else:
     # The first process receives the number and prints the atomization energy
-    container = num.array([0.])
+    container = np.array([0.])
     mpi.world.receive(container, 1)
 
     # Ea = E[molecule] - 2 * E[atom]
     atomization_energy = container[0] - 2 * energy
-    print 'Energy:',atomization_energy
+    print 'Atomization energy: %.4f eV' % atomization_energy
