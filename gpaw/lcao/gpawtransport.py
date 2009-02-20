957ff49a46f75a3dcddc3015c9a299fda04f62e9
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-02-20 12:44:12 +0000

    do zero_shift before get the path
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3238 6419b465-d902-0410-997c-be2266baf723

--- gpaw/lcao/gpawtransport.py
+++ gpaw/lcao/gpawtransport.py
@@ -497,6 +497,8 @@ class GPAWTransport:
             self.print_info('Warning*: hamiltonian boundary difference %f' %
                                                      e_diff)
             self.do_shift = True
+            for i in range(nspins):
+                self.h_spkmm[i] -= self.zero_shift * self.s_pkmm
         self.zero_shift = e_diff
         matdiff = self.d_spkmm[:, :, :pl1, :pl1] - self.d1_spkmm
         print_diff = np.max(abs(matdiff))
