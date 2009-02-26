c121ab735ea18dab4976eaa841dc9f4013aa53da
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2009-02-26 15:08:19 +0000

    Add a pure python tridiagonal solver.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3339 6419b465-d902-0410-997c-be2266baf723

--- gpaw/utilities/tools.py
+++ gpaw/utilities/tools.py
@@ -248,3 +248,37 @@ def geth(cell, h=.2, nodes=None):
     if nodes is not None:
         from gpaw.domain import decompose_domain
         print 'Domain decomposition:', decompose_domain(N_c, nodes)
+
+def tridiag(a, b, c, r, u):
+    """Solve linear system with tridiagonal coefficient matrix.
+
+    a is the lower band, b is the diagonal, c is the upper band, and
+    r is the right hand side.
+    The solution is returned in u.
+
+
+    [b1 c1  0  ...            ] [u1]   [r1] 
+    [a1 b2 c2 0 ...           ] [ :]   [ :]
+    [ 0 a2 b3 c3 0 ...        ] [  ] = [  ]
+    [                         ] [  ]   [  ]
+    [     ... 0 an-2 bn-1 cn-1] [ :]   [ :]
+    [          ... 0 an-1 bn  ] [un]   [rn]
+    """
+    n = len(b)
+    tmp = np.zeros(n-1) # necessary temporary array
+    if b[0] == 0:
+        raise RuntimeError, 'System is effectively order N-1'
+
+    beta = b[0]
+    u[0] = r[0] / beta
+    for i in range(1, n):
+        # Decompose and forward substitution
+        tmp[i-1] = c[i-1] / beta
+        beta = b[i] - a[i-1] * tmp[i-1]
+        if beta == 0:
+            raise RuntimeError, 'Method failure'
+        u[i] = (r[i] - a[i-1] * u[i-1]) / beta
+
+    for i in range(n-1, 0, -1):
+        # Backward substitution
+        u[i-1] -= tmp[i-1] * u[i]
