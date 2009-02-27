1f731d727f0a4694b893db655887e5ca357431f2
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-02-27 10:11:56 +0000

    write basis information to gpw file
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3353 6419b465-d902-0410-997c-be2266baf723

--- gpaw/io/__init__.py
+++ gpaw/io/__init__.py
@@ -166,7 +166,7 @@ def write(paw, filename, mode, db=True, private="666", **kwargs):
         w['UseSymmetry'] = p['usesymm']
         w['Converged'] = scf.converged
         w['FermiWidth'] = paw.occupations.kT
-        #w['BasisSet'] = p['basis']
+        w['BasisSet'] = p['basis']
         w['MixClass'] = paw.density.mixer.__class__.__name__
         w['MixBeta'] = paw.density.mixer.beta
         w['MixOld'] = paw.density.mixer.nmaxold
