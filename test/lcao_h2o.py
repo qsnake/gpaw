d09beaeefa56cae55f9647423965f04b59b34208
askhl <askhl@6419b465-d902-0410-997c-be2266baf723>
2009-03-01 17:27:30 +0000

    improved lcao tests.  lcao_force now uses 2 kpts rather than 1, and lcao_h2o now checks its result.  Thereby both gamma point and non-gamma point calculations are tested.  Also renamed these tests for consistency.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3379 6419b465-d902-0410-997c-be2266baf723

--- test/lcao_h2o.py
+++ test/lcao_h2o.py
@@ -2,7 +2,7 @@
 from ase import *
 from gpaw import GPAW
 
-a = 10
+a = 6
 b = a / 2
 mol = Atoms([Atom('O',(b, b, 0.1219 + b)),
              Atom('H',(b, 0.7633 + b, -0.4876 + b)),
@@ -11,3 +11,12 @@ mol = Atoms([Atom('O',(b, b, 0.1219 + b)),
 calc = GPAW(nbands=4, h=0.2, mode='lcao')
 mol.set_calculator(calc)
 e = mol.get_potential_energy()
+
+eref = -10.3830100992
+err = abs(e - eref)
+
+print 'Energy', e
+print 'Ref', eref
+print 'Err', err
+
+assert err < 1e-4
