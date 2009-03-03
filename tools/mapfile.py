60e2c162452f89e8fbc15fa088a1697468c4dba2
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-03-03 03:10:55 +0000

    Generalize mapfile to use arbitrary number of processors per node.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3392 6419b465-d902-0410-997c-be2266baf723

--- tools/mapfile.py
+++ tools/mapfile.py
@@ -1,6 +1,7 @@
 from sys import argv
 import numpy as np
 nodes = int(argv[1])
+ppn = 4
 shape = [int(x) for x in argv[2].split(',')]
 mode = 'normal'
 if len(argv) == 4:
@@ -18,8 +19,8 @@ layout = {   64: (4,  4,  4 ),
           32768: (32, 32, 32),
           40960: (40, 32, 32)}[nodes]
 domains = shape[0] * shape[1] * shape[2]
-blocks = nodes * 4 // domains
-assert blocks * domains == nodes * 4
+blocks = nodes * ppn // domains
+assert blocks * domains == nodes * ppn
 A = layout[0] // shape[0]
 B = layout[1] // shape[1]
 check = np.zeros(layout + (4,), int)
@@ -28,22 +29,22 @@ if mode == 'normal':
     for n in range(blocks):
         x0 = (n % A) * shape[0]
         y0 = ((n // A) % B) * shape[1]
-        z0 = (n // (A * B)) * shape[2] // 4
+        z0 = (n // (A * B)) * shape[2] // ppn
         for a in range(shape[0]):
             for b in range(shape[1]):
                 for c in range(shape[2]):
                     x = x0 + a
                     y = y0 + b
-                    z = z0 + c // 4
-                    t = c % 4
+                    z = z0 + c // ppn
+                    t = c % ppn
                     check[x, y, z, t] = 1
                     print x, y, z, t
                 rank += 1
 else:
     assert mode == 'shared'
     for n in range(blocks):
-        t = n % 4
-        m = n // 4
+        t = n % ppn
+        m = n // ppn
         x0 = (m % A) * shape[0]
         y0 = ((m // A) % B) * shape[1]
         z0 = (m // (A * B)) * shape[2]
@@ -56,4 +57,4 @@ else:
                     check[x, y, z, t] = 1
                     print x, y, z, t
                 rank += 1
-assert check.sum() == nodes * 4
+assert check.sum() == nodes * ppn
