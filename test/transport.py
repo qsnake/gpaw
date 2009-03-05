287bcc71f9ed564dc3f015c77022b66fa5538b05
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-03-05 17:26:11 +0000

    cancel wrong commit
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3439 6419b465-d902-0410-997c-be2266baf723

--- test/transport.py
+++ test/transport.py
@@ -7,7 +7,7 @@ import pickle
 a = 3.6
 L = 7.00 
 
-#basis = BasisMaker('Na').generate(1, 1)
+basis = BasisMaker('Na').generate(1, 1)
 
 atoms = Atoms('Na4', pbc=(1, 1, 1), cell=[L, L, 4 * a])
 atoms.positions[:4, 2] = [i * a for i in range(4)]
@@ -15,8 +15,7 @@ atoms.positions[:, :2] = L / 2.
 atoms.center()
 atoms.set_calculator(GPAW(h=0.3,
                           xc='PBE',
-                          #basis={'Na': basis},
-                          basis='szp',
+                          basis={'Na': basis},
                           kpts=(1,1,3),
                           width=0.01,
                           mode='lcao',
@@ -32,7 +31,7 @@ gpawtran = GPAWTransport(atoms=atoms,
                          pl_atoms=(pl_atoms1, pl_atoms2),
                          pl_cells=(pl_cell1, pl_cell2),
                          d=2) 
-gpawtran.negf_prepare(scat_restart=False, lead_restart=True)
-gpawtran.get_selfconsistent_hamiltonian(bias=0, gate=0,verbose=1)
+gpawtran.negf_prepare(scat_restart=False, lead_restart=False)
+gpawtran.get_selfconsistent_hamiltonian(bias=0, gate=0,verbose=0)
 filename = 'Na4_eq'
 gpawtran.output(filename)
