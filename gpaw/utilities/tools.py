774daebdbb3e42530ee859780c09bd5099ea588c
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2009-03-20 14:22:50 +0000

    trying to fix md5 depreciation warnings again
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3554 6419b465-d902-0410-997c-be2266baf723

--- gpaw/utilities/tools.py
+++ gpaw/utilities/tools.py
@@ -290,7 +290,7 @@ def md5_hash():
     """
     try:
         # To replace md5, deprecated in Python 2.5
-        import hashlib.md5 as md5
+        from hashlib import md5
     except ImportError:
         # Fallback to md5 to ensure 2.4 compatibility
         import md5
