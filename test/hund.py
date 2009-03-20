4b923d796c2b946f574b78b5f1cc993f2a7d7ef3
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2009-03-20 15:18:06 +0000

    update hund test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3560 6419b465-d902-0410-997c-be2266baf723

--- test/hund.py
+++ test/hund.py
@@ -4,20 +4,37 @@ from gpaw import *
 atoms = Atoms('H')
 atoms.center(vacuum=4.)
 
+params = dict(h=.3, convergence=dict(density=.005, eigenstates=1e-6))
+
 # No magmom, no hund
-atoms.set_calculator(GPAW())
+atoms.set_calculator(GPAW(**params))
 E_nom_noh = atoms.get_potential_energy()
+assert np.all(atoms.get_magnetic_moments() == 0.)
+assert atoms.calc.get_number_of_spins() == 1
 
 # No magmom, hund
-atoms.set_calculator(GPAW(hund=True))
+atoms.set_calculator(GPAW(hund=True, **params))
 E_nom_h = atoms.get_potential_energy()
+assert np.all(atoms.get_magnetic_moments() == 1.)
+assert atoms.calc.get_number_of_spins() == 2
 
 atoms.set_initial_magnetic_moments([1.])
 
 # magmom, no hund
-atoms.set_calculator(GPAW())
+atoms.set_calculator(GPAW(fixmom=True, **params))
 E_m_noh = atoms.get_potential_energy()
+assert np.all(atoms.get_magnetic_moments() == 1.)
+assert atoms.calc.get_number_of_spins() == 2
 
 # magmom, hund
-atoms.set_calculator(GPAW(hund=True))
+atoms.set_calculator(GPAW(hund=True, **params))
 E_m_h = atoms.get_potential_energy()
+assert np.all(atoms.get_magnetic_moments() == 1.)
+assert atoms.calc.get_number_of_spins() == 2
+
+print E_nom_noh
+print E_nom_h
+print E_m_noh
+print E_m_h
+
+assert E_m_h == E_m_noh == E_nom_h
