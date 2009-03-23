a8f641b7d16c674e60301f430377f1f43c055952
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-03-23 16:54:40 +0000

    Test now runs on 4+ cpu's.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3579 6419b465-d902-0410-997c-be2266baf723

--- test/parallel/blacs_redistribute.py
+++ test/parallel/blacs_redistribute.py
@@ -1,15 +1,17 @@
 import numpy as np
 from gpaw.mpi import world, rank, size
 import _gpaw
-A = np.arange(9, dtype=float).reshape(3, 3)
-d12 = _gpaw.blacs_create(world.new_communicator(np.array([0, 1])),
-                         3, 3, 1, 2, 3, 2)
-d22 = _gpaw.blacs_create(world.new_communicator(np.array([0, 1, 2, 3])),
-                         3, 3, 2, 2, 2, 2)
+A = np.arange(16, dtype=float).reshape(4, 4)
+c1 = world.new_communicator(np.array([0, 1]))
+d12 = _gpaw.blacs_create(c1, 4, 4, 1, 2, 2, 2)
+c2 = world.new_communicator(np.array([0, 1, 2, 3]))
+d22 = _gpaw.blacs_create(c2, 4, 4, 2, 2, 2, 2)
+C = None
 if rank == 0:
-    C = _gpaw.scalapack_redist(A[:2], d12, d22)
+    print A
+    C = _gpaw.scalapack_redist(A[:2], d12, d22, d22)
 elif rank == 1:
-    C = _gpaw.scalapack_redist(A[2:], d12, d22)
+    C = _gpaw.scalapack_redist(A[2:], d12, d22, d22)
 elif rank < 4:
-    C = _gpaw.scalapack_redist(None, d12, d22)
+    C = _gpaw.scalapack_redist(None, d12, d22, d22)
 print rank, C
