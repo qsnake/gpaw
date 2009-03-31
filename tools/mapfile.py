c71d2a06f55e8fe3aca6fa00d7b65ed430e1146f
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-03-31 17:53:20 +0000

    This generates a better mapping for the cases when shape[1] neq = P_x and/or shape[2] neq = P_y, where P_x, P_y are the partition dimensions. Other cases are not affected.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3643 6419b465-d902-0410-997c-be2266baf723

--- tools/mapfile.py
+++ tools/mapfile.py
@@ -1,9 +1,18 @@
+# Mostly been tested on BG/P at Argonne National Laboratory
+# Longest partition dimensions (P_x, P_y, P_z) for this BG/P
+# is Z-axis. We fold the torus dimension into the Z-axis, making
+# it even longer. For band parallelization, we "stack" adjacent
+# bands along Z-axis from bottom (0,0,0) to top (0,0,P_z). This
+# should also work for spin polarized calculation for a judicous
+# choice of --state-parallelization=B.
+
 from sys import argv
 import numpy as np
 nodes = int(argv[1])
-ppn = 4
+ppn = 2
 shape = [int(x) for x in argv[2].split(',')]
 mode = 'normal'
+# print "Shape =", shape
 if len(argv) == 4:
     mode = argv[3]
 layout = {   64: (4,  4,  4 ),
@@ -20,16 +29,20 @@ layout = {   64: (4,  4,  4 ),
           40960: (40, 32, 32)}[nodes]
 domains = shape[0] * shape[1] * shape[2]
 blocks = nodes * ppn // domains
+# print "State parallelization = ", blocks
 assert blocks * domains == nodes * ppn
 A = layout[0] // shape[0]
 B = layout[1] // shape[1]
+C = layout[2] // shape[2] * ppn # torus dimension folds into z-axes
 check = np.zeros(layout + (4,), int)
 rank = 0
 if mode == 'normal':
     for n in range(blocks):
-        x0 = (n % A) * shape[0]
-        y0 = ((n // A) % B) * shape[1]
-        z0 = (n // (A * B)) * shape[2] // ppn
+        x0 = (n // (B*C)) * shape[0]
+        y0 = ((n // C) % B) * shape[1]
+        z0 = (n % C) * shape[2] // ppn 
+        # coordinate of block below
+        # print "n, x0,y0,z0=", n, x0,y0,z0
         for a in range(shape[0]):
             for b in range(shape[1]):
                 for c in range(shape[2]):
