7fffa89a25b3178413ad749b2711af5415f40b11
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-04-01 08:13:42 +0000

    update code
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3647 6419b465-d902-0410-997c-be2266baf723

--- doc/exercises/transport/pt_h2_lcao.py
+++ doc/exercises/transport/pt_h2_lcao.py
@@ -15,8 +15,8 @@ atoms.positions[5:7, 0] = [4 * a + c, 4 * a + c + b]
 atoms.positions[:, 1:] = L / 2.
 
 # Attach a GPAW calculator
-atoms.set_calculator(GPAW(h=0.3, xc='PBE', basis='szp', width=0.1,
-                          eigensolver='lcao', txt='pt_h2_lcao.txt',
+atoms.set_calculator(GPAW(h=0.3, xc='PBE', basis='szp', width=0.1, kpts=(1,1,1),
+                          mode='lcao', txt='pt_h2_lcao.txt',
                           mixer=Mixer(0.1, 5, metric='new', weight=100.0)))
 
 # Setup the GPAWTransport calculator
