0b1d6541c6b51373b11af67361133422566de6ad
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2009-04-09 14:09:20 +0000

    Checks wf io and automatic wf update now
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3711 6419b465-d902-0410-997c-be2266baf723

--- test/lrtddft2.py
+++ test/lrtddft2.py
@@ -1,30 +1,53 @@
 import os
+import numpy as np
 from ase import *
 from gpaw import GPAW
 from gpaw.utilities import equal
 from gpaw.lrtddft import LrTDDFT
 
 txt = '-'
-#txt = None
+txt = None
+load = True
 load = False
 
 R = 0.7  # approx. experimental bond length
-a = 3.0
-c = 4.0
+a = 4.0
+c = 5.0
 H2 = Atoms([Atom('H', (a / 2, a / 2, (c - R) / 2)),
             Atom('H', (a / 2, a / 2, (c + R) / 2))],
            cell=(a, a, c))
+
 calc = GPAW(xc='PBE', nbands=2, spinpol=False, txt=txt)
 H2.set_calculator(calc)
 H2.get_potential_energy()
-calc.write('H2saved.gpw', 'all')
+calc.write('H2saved_wfs.gpw', 'all')
+calc.write('H2saved.gpw')
+wfs_error = calc.wfs.eigensolver.error
 
 xc = 'LDA'
 
-# this works ---------------------
+#print "-> starting directly after a gs calculation"
+lr = LrTDDFT(calc, xc=xc, txt='-')
+lr.diagonalize()
 
-# without spin
-lr = LrTDDFT(calc, xc=xc)
+#print "-> reading gs with wfs"
+gs = GPAW('H2saved_wfs.gpw', txt=txt)
 
-# this fails ---------------------
-lr = LrTDDFT(GPAW('H2saved.gpw', txt=txt), xc=xc)
+# check that the wfs error is read correctly, 
+# but take rounding errors into account
+assert( abs(calc.wfs.eigensolver.error/gs.wfs.eigensolver.error - 1) < 1e-8)
+lr1 = LrTDDFT(gs, xc=xc, txt='-')
+lr1.diagonalize()
+# check the oscillator strrength
+assert(abs(lr1[0].get_oscillator_strength()[0]/
+           lr[0].get_oscillator_strength()[0]   -1) < 1e-10)
+
+#print "-> reading gs without wfs"
+gs = GPAW('H2saved.gpw', txt=None)
+# the wave functions err should be infinity
+assert(gs.wfs.eigensolver.error >  calc.wfs.eigensolver.error)
+lr2 = LrTDDFT(gs, xc=xc, txt='-')
+lr2.diagonalize()
+# check the oscillator strrength
+assert(abs(lr2[0].get_oscillator_strength()[0]/
+           lr[0].get_oscillator_strength()[0]   -1) < 1e-5)
