63230743a2c2e3fcc6b4675659655b5b0f25d8af
carstenr <carstenr@6419b465-d902-0410-997c-be2266baf723>
2009-04-14 08:53:04 +0000

    Change test according to change in get_nonselfconsistent_eigs
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3720 6419b465-d902-0410-997c-be2266baf723

--- test/nonself_eigs.py
+++ test/nonself_eigs.py
@@ -22,63 +22,42 @@ calc.write('nontest', mode='all')
 # Do the non-selfconsistent calculations
 
 calc = GPAW('nontest')
-E_nPBE, eigs_nPBE = calc.get_nonselfconsistent_eigenvalues('PBE')
-E_nPBE += calc.get_reference_energy()
+eigs_nPBE = calc.get_nonselfconsistent_eigenvalues('PBE')
 #calc.set(xc='PBE', **noconv)
-E_PBE = calc.get_potential_energy() + calc.get_reference_energy()
 eigs_PBE = get_eigs(calc)
 
 calc = GPAW('nontest')
-E_nLDA, eigs_nLDA = calc.get_nonselfconsistent_eigenvalues('LDA')
-E_nLDA += calc.get_reference_energy()
-print calc.get_reference_energy()
+eigs_nLDA = calc.get_nonselfconsistent_eigenvalues('LDA')
 calc.set(xc='LDA', **noconv)
-print calc.get_reference_energy()
-E_LDA = atoms.get_potential_energy() + calc.get_reference_energy()
 eigs_LDA = get_eigs(calc)
 
 calc = GPAW('nontest')
-E_nEXX, eigs_nEXX = calc.get_nonselfconsistent_eigenvalues('EXX')
-E_nEXX += calc.get_reference_energy()
-print calc.get_reference_energy()
+eigs_nEXX = calc.get_nonselfconsistent_eigenvalues('EXX')
 calc.set(xc='EXX', **noconv)
-print calc.get_reference_energy()
-E_EXX = atoms.get_potential_energy() + calc.get_reference_energy()
 eigs_EXX = get_eigs(calc)
 
 print 'LDA'
-print E_LDA, E_nLDA
 print eigs_LDA - eigs_nLDA
 
 print 'PBE'
-print E_PBE, E_nPBE
 print eigs_PBE - eigs_nPBE
 
 print 'EXX'
-print E_EXX, E_nEXX
 print eigs_EXX - eigs_nEXX
 
 
 # selfconsistent LDA
-## -0.883282451388
-## -12.1289625969
 ## [[[-7.24822465  3.13856831]]
 ##  [[-2.43790837  4.22731969]]]
 
 # selfconsistent PBE
-## -1.0996835981
-## -12.4901666629
 ## [[[-7.52740492  3.04583427]]
 ##  [[ 5.14286664  7.01543167]]]
 
 # selfconsistent EXX starting fom selfcon. LDA orbitals
-## -13.6018033066
-## 0.0
 ## [[[-13.61181534   5.70652396]]
 ##  [[  1.44180798   6.18816302]]]
 
 # selfconsistent EXX starting fom selfcon. PBE orbitals
-## -13.6818871849
-## 0.0
 ## [[[-13.65755584   5.71060805]]
 ##  [[  1.45560588   6.18935012]]]
