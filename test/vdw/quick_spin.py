f4a1da005e58f382ec59b2c2ebe9e751e6153db1
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-04-22 12:29:38 +0000

    Generate revPBE setup for hydrogen.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3763 6419b465-d902-0410-997c-be2266baf723

--- test/vdw/quick_spin.py
+++ test/vdw/quick_spin.py
@@ -1,6 +1,17 @@
 import os
 from ase import *
 from gpaw import GPAW
+from ase.parallel import rank, barrier
+from gpaw.atom.generator import Generator, parameters
+from gpaw import setup_paths
+
+# Generate setup
+symbol = 'H'
+if rank == 0:
+    g = Generator(symbol, 'revPBE', scalarrel=True, nofiles=True)
+    g.run(**parameters[symbol])
+barrier()
+setup_paths.insert(0, '.')
 
 if 'GPAW_VDW' in os.environ:
     L = 2.5
