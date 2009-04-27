23e9311207e1b054585569eb82027870edecacee
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-04-27 10:48:43 +0000

    Use lists instead of dicts for reference numbers - so that we can do an ordered loop.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3795 6419b465-d902-0410-997c-be2266baf723

--- test/CH4.py
+++ test/CH4.py
@@ -3,13 +3,13 @@ from ase import *
 from gpaw.utilities import equal
 from gpaw import GPAW
 
-ref_3775 = { # Values from revision 3775.
+ref_3775 = [ # Values from revision 3775.
     # d         Energy
-    1.0  : -23.4215573953,
-    1.05 : -23.541808956,
-    1.1  : -23.5629210099,
-    1.15 : -23.50919649,
-    }
+    (1.00, -23.4215573953),
+    (1.05, -23.541808956),
+    (1.10, -23.5629210099),
+    (1.15, -23.50919649),
+    ]
 
 a = 4.0
 n = 20
@@ -21,12 +21,12 @@ atoms = Atoms([Atom('C', (0.0, 0.0, 0.0)),
                Atom('H', (x, -x, -x)),
                Atom('H', (-x, x, -x))],
               cell=(a, a, a), pbc=True)
-atoms.set_calculator(GPAW(gpts=(n, n, n), nbands=4, txt='-'))
+atoms.set_calculator(GPAW(gpts=(n, n, n), nbands=4, txt=None))
 e0 = atoms.get_potential_energy()
 
-for d in ref_3775.keys():
+for d, eref in ref_3775:
     x = d / 3**0.5
     atoms.positions[1] = (x, x, x)
     e = atoms.get_potential_energy()
-    print d, e - e0
-    equal(ref_3775[d], e, 2e-5)
+    print d, e - e0, e-eref
+    equal(eref, e, 7e-5)
--- test/bulk.py
+++ test/bulk.py
@@ -4,29 +4,31 @@ from gpaw.utilities import equal
 from gpaw import GPAW
 
 # Values from revision 3775.
-ref_3775 = { # Values from revision 3775.
+ref_3775 = [ # Values from revision 3775.
     # A         Energy
-    2.6  : -1.9857831386443334,
-    2.65 : -1.9878176493286341,
-    2.7  : -1.984691908434324,
-    2.75 : -1.9773280974295946,
-    2.8  : -1.9662827649879555,
-    }
+    (2.60, -1.9857831386443334),
+    (2.65, -1.9878176493286341),
+    (2.70, -1.984691908434324),
+    (2.75, -1.9773280974295946),
+    (2.80, -1.9662827649879555),
+    ]
 
 bulk = Atoms([Atom('Li')], pbc=True)
 k = 4
 g = 8
 calc = GPAW(gpts=(g, g, g), kpts=(k, k, k), nbands=2)
 bulk.set_calculator(calc)
+A = []
 e = []
-for a in ref_3775.keys():
+for a, eref in ref_3775:
     bulk.set_cell((a, a, a))
     e_a = bulk.get_potential_energy()
-    equal(ref_3775[a], e_a, 2e-5)
+    equal(eref, e_a, 2e-5)
     e.append(e_a)
+    A.append(a)
 print e
 
 import numpy as np
-a = np.roots(np.polyder(np.polyfit( ref_3775.keys(), e, 2), 1))[0]
+a = np.roots(np.polyder(np.polyfit(A, e, 2), 1))[0]
 print 'a =', a
 assert abs(a - 2.6430) < 0.0001
