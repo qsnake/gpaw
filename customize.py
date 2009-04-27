abb70accf5b04b485cbf203759c9d01039b0b1a4
mvanin <mvanin@6419b465-d902-0410-997c-be2266baf723>
2009-04-27 07:01:17 +0000

    Back to a non-empty customize.py
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3792 6419b465-d902-0410-997c-be2266baf723

---
+++ customize.py
@@ -0,0 +1,55 @@
+#User provided customizations for the gpaw setup
+
+#Here, one can override the default arguments, or append own
+#arguments to default ones
+#To override use the form
+#     libraries = ['somelib','otherlib']
+#To append use the form
+#     libraries += ['somelib','otherlib']
+
+# Valid values for scalapack are False, or True:
+# False (the default) - no ScaLapack compiled in
+# True - ScaLapack compiled in
+#scalapack = True
+
+#compiler = 'mpcc'
+#libraries = []
+#libraries += []
+
+#library_dirs = []
+#library_dirs += []
+
+#include_dirs = []
+#include_dirs += []
+
+#extra_link_args = []
+#extra_link_args += []
+
+#extra_compile_args = []
+#extra_compile_args += []
+
+#runtime_library_dirs = []
+#runtime_library_dirs += []
+
+#extra_objects = []
+#extra_objects += []
+
+#define_macros = []
+#define_macros += []
+
+#mpicompiler = None
+#mpilinker = None
+#mpi_libraries = []
+#mpi_libraries += []
+
+#mpi_library_dirs = []
+#mpi_library_dirs += []
+
+#mpi_include_dirs = []
+#mpi_include_dirs += []
+
+#mpi_runtime_library_dirs = []
+#mpi_runtime_library_dirs += []
+
+#mpi_define_macros = []
+#mpi_define_macros += []
