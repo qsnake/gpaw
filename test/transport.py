09d99ed926a00a9f583a126ad295700b4efc540d
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-05-07 19:15:42 +0000

    a new transport test file
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3861 6419b465-d902-0410-997c-be2266baf723

--- test/transport.py
+++ test/transport.py
@@ -1,6 +1,6 @@
 from ase import *
 from gpaw import *
-from gpaw.lcao.gpawtransport import GPAWTransport 
+from gpaw.transport.calculator import Transport 
 from gpaw.atom.basis import BasisMaker
 from gpaw.poisson import PoissonSolver
 import pickle
@@ -14,25 +14,26 @@ atoms = Atoms('Na4', pbc=(1, 1, 1), cell=[L, L, 4 * a])
 atoms.positions[:4, 2] = [i * a for i in range(4)]
 atoms.positions[:, :2] = L / 2.
 atoms.center()
-atoms.set_calculator(GPAW(h=0.3,
-                          xc='LDA',
-                          basis={'Na': basis},
-                          kpts=(1,2,2),
-                          width=0.01,
-                          mode='lcao',
-                          poissonsolver=PoissonSolver(relax='GS'),
-                          txt='Na_lcao.txt',
-                          usesymm=None,
-                          mixer=Mixer(0.1, 5, metric='new', weight=100.0)))
 pl_atoms1 = range(4)     
-pl_atoms2 = range(-4, 0)
+pl_atoms2 = range(4)
 pl_cell1 = (L, L, 4 * a) 
 pl_cell2 = pl_cell1
-
-gpawtran = GPAWTransport(atoms=atoms,
-                         pl_atoms=(pl_atoms1, pl_atoms2),
-                         pl_cells=(pl_cell1, pl_cell2),
-                         d=2) 
-gpawtran.negf_prepare(scat_restart=False, lead_restart=False, savefile=False)
-gpawtran.get_selfconsistent_hamiltonian(bias=0, gate=0,verbose=0)
+t = Transport(h=0.3,
+              xc='LDA',
+              basis={'Na': basis},
+              kpts=(1,1,5),
+              width=0.01,
+              mode='lcao',
+              poissonsolver=PoissonSolver(relax='GS'),
+              txt='Na_lcao.txt',
+              usesymm=None,
+              mixer=Mixer(0.1, 5, metric='new', weight=100.0),
+              pl_atoms=[pl_atoms1, pl_atoms2],
+              pl_cells=[pl_cell1, pl_cell2],
+              pl_kpts=(1,1,5),
+              bias=[0,0],
+             )
+atoms.set_calculator(t)
+t.negf_prepare()
+t.get_selfconsistent_hamiltonian()
 
