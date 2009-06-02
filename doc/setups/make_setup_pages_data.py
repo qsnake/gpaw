9482a16628e9334a6f15704e02489eaa195a4d97
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-06-02 11:46:24 +0000

    Small modifications to setup page generator.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@3982 6419b465-d902-0410-997c-be2266baf723

--- doc/setups/make_setup_pages_data.py
+++ doc/setups/make_setup_pages_data.py
@@ -11,58 +11,42 @@ from gpaw.atom.generator import Generator, parameters
 from gpaw.atom.analyse_setup import analyse
 from gpaw.testing.dimer import TestAtom
 from gpaw.testing.atomization_data import atomization_vasp
-from gpaw.mpi import world
 
 systems = atomization_vasp.keys()
-
 
 def make(symbol, show):
     Z = atomic_numbers[symbol]
     name = atomic_names[Z]
 
-    if not os.path.isfile(symbol + '.pckl'):
-        if os.path.isfile(symbol + '-generator.pckl'):
-            gen = pickle.load(open(symbol + '-generator.pckl'))
-        else:
-            gen = Generator(symbol, 'PBE', scalarrel=True)
-            gen.run(logderiv=True, **parameters[symbol])
-            gen.txt = None
-            pickle.dump(gen, open(symbol + '-generator.pckl', 'w'))
+    gen = Generator(symbol, 'PBE', scalarrel=True)
+    gen.run(logderiv=True, **parameters[symbol])
+    gen.txt = None
     
-        tables = analyse(gen, show=show)
+    tables = analyse(gen, show=show)
 
-        t = TestAtom(symbol)
-        t.run(True, False)
-        hmin, B, n = t.summary(show=show)
+    t = TestAtom(symbol)
+    t.run(True, False)
+    hmin, B, n = t.summary(show=show)
 
-        molecules = []
-        for x in systems:
-            if symbol in string2symbols(molecule_data[x]['symbols']):
-                molecules.append(molecule_data[x]['name'])
-        mols = ''
-        if molecules:
-            names = [rest(m) for m in molecules]
-            if len(molecules) == 1:
-                mols = names[0]
-            elif len(molecules) > 5:
-                mols = ', '.join(names[:5]) + ' ...'
-            else:
-                mols = ', '.join(names[:-1]) + ' and ' + names[-1]
-        pickle.dump((tables, hmin, B, n, mols), open(symbol + '.pckl', 'w'))
-    else:
-        tables, hmin, B, n, mols = pickle.load(open(symbol + '.pckl'))
-
-    return tables, hmin, B, n, mols
+    molecules = []
+    for x in systems:
+        if symbol in string2symbols(molecule_data[x]['symbols']):
+            molecules.append(molecule_data[x]['name'])
+    mols = ''
+    if molecules:
+        names = [rest(m) for m in molecules]
+        if len(molecules) == 1:
+            mols = names[0]
+        elif len(molecules) > 5:
+            mols = ', '.join(names[:5]) + ' ...'
+        else:
+            mols = ', '.join(names[:-1]) + ' and ' + names[-1]
+    pickle.dump((tables, hmin, B, n, mols), open(symbol + '.pckl', 'wb'),
+                protocol=pickle.HIGHEST_PROTOCOL)
 
 if len(argv) == 1:
     symbols = parameters.keys()
-    symbols = ['H', 'Li', 'He', 'Be']
-    show = False
+    pass  # ...
 else:
-    symbols = argv[1:]
-    show = True
-
-for n, symbol in enumerate(symbols):
-    if n % world.size == world.rank:
-        make(symbol, show)
-world.barrier()
+    symbol = argv[1]
+    make(symbol, show=False)
