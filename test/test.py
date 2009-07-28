735d4125e5afe9e7d599aa39572b34d0e470a687
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2009-07-28 11:20:28 +0000

    Simple SingleCalculatorNEB added.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4473 6419b465-d902-0410-997c-be2266baf723

--- test/test.py
+++ test/test.py
@@ -136,6 +136,7 @@ if len(tests) == 0:
         'lcao_force.py',
         'wannier-ethylene.py',
         'CH4.py',
+        'neb.py',
         #'hgh_h2o.py',
         'apmb.py',
         'relax.py',
