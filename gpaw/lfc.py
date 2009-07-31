Fix normalization bug for zero-functions in NewLFC.
git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4518 6419b465-d902-0410-997c-be2266baf723

gpaw/lfc.py
@@ -128,7 +128,8 @@ class Sphere:
 
     def normalize(self, integral, a, dv, comm):
         """Normalize localized functions."""
-        if self.normalized:
+        if self.normalized or integral < 1e-15:
+            self.normalized = True
             yield None
             yield None
             yield None
@@ -171,7 +172,7 @@ class Sphere:
 
         w = 0
         for M, A_gm in zip(self.M_w, self.A_wgm):
-            if M == 0 and integral > 1e-15:
+            if M == 0:
                 A_gm *= integral / I_M[0]
             else:
                 A_gm -= (I_M[M:M + A_gm.shape[1]] / integral *
@@ -682,23 +683,23 @@ class NewLocalizedFunctionsCollection(BaseLFC):
             sphere = self.sphere_a[a]
             M2 = M1 + sphere.Mmax
             if c_iv is not None:
+                I = self.integral_a[a]
+                if I < 1e-15:
+                    c_iv[:] = 0.0
+                    continue
                 if len(sphere.ranks) > 0:
                     c_Mv[M1:M2] += c_ariv[a].sum(axis=0)
-                I = self.integral_a[a]
                 I_L = sphere.I_M
                 I0 = I_L[0]
                 c_Lv = c_Mv[M1:M2, :3]
                 b_Lv = c_Mv[M1:M2, 3:6]
                 A0 = c_Mv[M1, 6]
-                if I0 < 1e-15:
-                    c_iv[0] = 0.0
-                else:
-                    c_iv[0, :] = (I / I0 * c_Lv[0] -
-                                  I / I0**2 * b_Lv[0] * A0)
-                    c_iv[1:, :] = (c_Lv[1:] -
-                                   np.outer(I_L[1:] / I0, c_Lv[0]) -
-                                   A0 / I0 * b_Lv[1:] +
-                                   A0 / I0**2 * np.outer(I_L[1:], b_Lv[0]))
+                c_iv[0, :] = (I / I0 * c_Lv[0] -
+                              I / I0**2 * b_Lv[0] * A0)
+                c_iv[1:, :] = (c_Lv[1:] -
+                               np.outer(I_L[1:] / I0, c_Lv[0]) -
+                               A0 / I0 * b_Lv[1:] +
+                               A0 / I0**2 * np.outer(I_L[1:], b_Lv[0]))
                                
             M1 = M2
 