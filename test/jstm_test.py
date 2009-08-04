40b2e02047597ae1a4e227dc8a1910c3ff79d676
s042606 <s042606@6419b465-d902-0410-997c-be2266baf723>
2009-08-04 11:09:05 +0000

    test for transport.jstm
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4550 6419b465-d902-0410-997c-be2266baf723

---
+++ test/jstm_test.py
@@ -0,0 +1,71 @@
+from ase import Atoms
+from gpaw import GPAW
+from gpaw.transport.jstm import STM 
+import numpy as np
+
+# GPAW calculations
+a = 0.75 # Bond length
+cell = np.diag([5,5,12*a])
+cell[0,1] = 2*np.sqrt(2)
+cell[1,0] = 1 
+
+atoms = Atoms('H12', pbc=(1, 1, 1), cell=cell) 
+atoms.positions[:, 2] = [i * a for i in range(12)]
+
+calc = GPAW(h=0.3,
+            mode='lcao',
+            basis='sz')
+
+# Lead calculation
+lead = atoms.copy()
+del(lead[4:])
+cell_lead = cell.copy()
+cell_lead[2,2]=a*len(lead)
+lead.set_cell(cell_lead)
+calc.set(kpts=(1,1,14))
+lead.set_calculator(calc)
+lead.get_potential_energy()
+calc.write('lead.gpw')
+
+# Tip calculation
+tip = atoms.copy()
+tip.positions[:] += (atoms.cell/2.0)[0,:]+(atoms.cell/2.0)[1,:]
+del(tip[:4])
+tip.translate([0,0,10])
+tip.cell[2,2]+=10
+calc.set(kpts=(1,1,1))
+tip.set_calculator(calc)
+tip.get_potential_energy()
+calc.write('tip.gpw')
+
+# Surface calculation
+srf = atoms.copy()
+del(srf[8:])
+srf.cell[2,2]+=10
+srf.set_calculator(calc)
+srf.get_potential_energy()
+calc.write('srf.gpw')
+
+#STM simulation
+tip = GPAW('tip')
+srf = GPAW('srf')
+lead = GPAW('lead')
+
+stm = STM(tip, srf,
+          lead1=lead,
+          lead2=lead,
+          align_bf=0,
+          cvl1=2,
+          cvl2=2)
+
+stm.hs_from_paw()
+stm.set(dmin=5)
+stm.initialize()
+
+stm.scan()
+stm.linescan()
+
+if 1:
+    stm.plot()
+
+
