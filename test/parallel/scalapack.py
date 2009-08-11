2cc8a5fba01f232a4113a6f070cb1068361d15b5
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-08-11 22:24:07 +0000

    More progress. Need to test and do complex case.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4611 6419b465-d902-0410-997c-be2266baf723

--- test/parallel/scalapack.py
+++ test/parallel/scalapack.py
@@ -1,41 +1,53 @@
-# Simple test case for new BLACS/ScaLAPACK
-# We compare the results here to those obtained
-# from
-# test/parallel/bandpar3?.py
-# with analogous parameters.
-#
 # Note that GPAW does not do transpose for calls
 # to LAPACK involving operations on symmetric matrices. 
 # Hence, UPLO = 'U' in Fortran equals UPLO = 'L' in
-# NumPy C-style. To do a simple comparison we will make
-# convert everything here to Fortran style.
+# NumPy C-style. For simplicity, we will
+# convert everything here to Fortran style since
+# this is the default for ScaLAPACK
+#
+# Here we compare ScaLAPACK results for a 16-by-16 matrix
+# with those obtain with serial LAPACK
 import numpy as np
+
+from gpaw.mpi import world
 from gpaw.utilities.lapack import diagonalize, inverse_cholesky
+from gpaw.utilites.blacs import *
+
+w_tol = 1.e-12
+z_tol = 1.e-7
+c_tol = 1.e-7
+
 
 N = 16;
+B = 4;
+M = N/B;
+
+if world.size != B:
+    raise NameError('scalapack tests only works on 4 MPI tasks!')
+    
 A = np.empty((N,N))
-A[:,0:4] = 0.0*np.eye(N,4,0)
-A[:,0:4] = A[:,0:4]+ 0.1*np.eye(N,4,+1) # shift off of diag +1
-A[:,4:8] = 1.0*np.eye(N,4,-4)
-A[:,4:8] = A[:,4:8] + 0.1*np.eye(N,4,-3) # shift off of diag +1
-A[:,8:12] = 2.0*np.eye(N,4,-8)
-A[:,8:12] = A[:,8:12] + 0.1*np.eye(N,4,-7) # shift off of diag +1 
-A[:,12:16] = 3.0*np.eye(N,4,-12)
-A[:,12:16] = A[:,12:16]+ 0.1*np.eye(N,4,-11) # shift off of diag +1
+A[:,0:M] = 0.0*np.eye(N,M,0)
+A[:,0:M] = A[:,0:M]+ 0.1*np.eye(N,M,-M*0+1) # shift off of diag +1
+A[:,M:2*M] = 1.0*np.eye(N,M,-M)
+A[:,M:2*M] = A[:,M:2*M] + 0.1*np.eye(N,M,-M*1+1) # shift off of diag +1
+A[:,2*M:3*M] = 2.0*np.eye(N,M,-M*2)
+A[:,2*M:3*M] = A[:,2*M:3*M] + 0.1*np.eye(N,M,-M*2+1) # shift off of diag +1 
+A[:,3*M:4*M] = 3.0*np.eye(N,M,-M*3)
+A[:,3*M:4*M] = A[:,3*M:4*M]+ 0.1*np.eye(N,M,-M*3+1) # shift off of diag +1
 A = A.copy("Fortran")
 print "Hamiltonian =", A
 
-B = np.empty((N,N))
-B[:,0:4] = 1.0*np.eye(N,4,0)
-B[:,0:4] = B[:,0:4]+ 0.2*np.eye(N,4,+1) # shift off of diag +1
-B[:,4:8] = 1.0*np.eye(N,4,-4)
-B[:,4:8] = B[:,4:8] + 0.2*np.eye(N,4,-3) # shift off of diag +1
-B[:,8:12] = 1.0*np.eye(N,4,-8)
-B[:,8:12] = B[:,8:12] + 0.2*np.eye(N,4,-7) # shift off of diag +1 
-B[:,12:16] = 1.0*np.eye(N,4,-12)
-B[:,12:16] = B[:,12:16] + 0.2*np.eye(N,4,-11) # shift off of diag +1
-B = B.copy("Fortran")
-print "Overlap = ", B
+S = np.empty((N,N))
+S[:,0:M] = 1.0*np.eye(N,M,0)
+S[:,0:M] = S[:,0:M]+ 0.2*np.eye(N,M,-M*0+1) # shift off of diag +1
+S[:,M:2*M] = 1.0*np.eye(N,M,-M)
+S[:,M:2*M] = S[:,M:2*M] + 0.2*np.eye(N,M,-M*1+1) # shift off of diag +1
+S[:,2*M:3*M] = 1.0*np.eye(N,M,-M*2)
+S[:,2*M:3*M] = S[:,8:12] + 0.2*np.eye(N,4,-M*2+1) # shift off of diag +1 
+S[:,3*M:4*M] = 1.0*np.eye(N,M,-M*3)
+S[:,3*M:4*M] = S[:,3*M:4*M] + 0.2*np.eye(N,4,-M*3+1) # shift off of diag +1
+S = S.copy("Fortran")
+print "Overlap = ", S
 
 w = np.empty(N)
 
@@ -44,21 +56,95 @@ A2 = A.copy("Fortran")
 info = diagonalize(A2, w)
 
 if info != 0:
-    print "WARNING: diagonalize info=", info
+    print "WARNING: diagonalize info = ", info
 print "lambda", w
 print "eigenvectors", A2
 
-# We need to make a backup of B since LAPACK diagonalize will destroy it
-B2 = B.copy("Fortran")
-info = diagonalize(A, w, B2)
+wg = np.empty(N)
+
+# We need to make a backup of S since LAPACK diagonalize will destroy it
+S2 = S.copy("Fortran")
+info = diagonalize(A, wg, S2)
 
 if info != 0:
-    print "WARNING: general diagonalize info = print", info
-print "lambda", w
+    print "WARNING: general diagonalize info = ", info
+print "lambda", w_g
 print "eigenvectors", A
 
-info = inverse_cholesky(B)
+# For consistency, also make a backup of S since LAPACK will destroy it
+C = S.copy("Fortran")
+info = inverse_cholesky(C)
 
 if info != 0:
-    print "WARNING: general diagonalize info = print", info
-print "overlap", B
+    print "WARNING: general diagonalize info = ", info
+print "overlap", C
+
+# Initialize distributed matrices to None;
+# Otherwise, scalapack_redist will complain of UnboundLocalError
+# This happens because scalapack local array does not exist on
+# both the source and destination communications. As this is a
+# simple example, we should be OK here without it. 
+A_nm = None
+S_nm = None
+C_nm = None
+C_mm = None
+
+# Create arrays in parallel
+A_nm = world.rank*np.eye(N,M,-M*world.rank)
+A_nm = H_nm[:,0:N/B] + 0.1*np.eye(N,M,-M*world.rank+1)
+A_nm = H_nm.copy("Fortran") # Fortran order required for ScaLAPACK
+S_nm = np.eye(N,M,-M*world.rank)
+S_nm = S_nm[:,0:N/B] + 0.2*np.eye(N,M,-M*world.rank+1)
+S_nm = S_nm.copy("Fortran") # Fortran order required for ScaLAPACK
+C_nm = S_nm.copy("Fortran")
+
+# Create descriptors
+# Desc for serial : 0-D grid
+desc0 = blacs_create(world,N,N,1,1,N,N)
+# Desc for A_nm : 1-D grid
+desc1 = blacs_create(world,N,N,1,B,N,M)
+# Desc for H_mm : 2-D grid
+desc2 = blacs_create(world,N,N,B,B,M,M)
+
+# Redistribute from 1-D -> 2-D grid
+# in practice we must do this for performance
+# reasons so this is not hypothetical
+H_mm = scalapack_redist(H_nm,desc1,desc2)
+S_mm = scalapack_redist(S_nm,desc1,desc2)
+
+W, Z_mm = scalapack_diagonalize_dc(H_mm, desc2)
+
+Wg, Zg_mm = scalapack_general_diagonalize(H_mm, S_mm, desc2)
+
+scalapack_inverse_cholesky(C_mm, desc2)
+
+assert len(W) = len(w)
+
+for i in range(len(W)):
+    if abs(W[i]-w[i]) > w_tol:
+        raise NameError('scalapack_diagonalize_dc eigenvalues wrong!')
+        
+
+
+assert len(Wg) = len(wg)
+
+for i in range(len(W)):
+    if abs(Wg[i]-wg[i]) > w_tol:
+        raise NameError('scalapack_general_diagonalize eigenvalues wrong!')
+
+# Check eigenvectors
+# Easier to do this if everything if everything is collect on one node
+Z_0 = scalapack_redist(Z_mm,desc2,desc0)
+Zg_0 = scalapack_redist(Z_mm,desc2,desc0)
+C_0 = scalapack_redist(C_00, desc2, desc0)
+
+assert Z_0.shape = A2.shape = Zg_0.shape = A.shape = C_0.shape = C.shape
+
+for i in Z_0.shape[0]:
+    for j in Z_0.shape[0]:
+        if abs(Z_0[i,j]-A2[i,j]) > z_tol:
+            raise NameError('scalapack_diagonalize_dc eigenvectors failed!')
+        if abs(Zg_0[i,j]-A[i,j]) > z_tol:
+            raise NameError('scalapack_diagonalize_dc eigenvectors failed!')        
+        if abs(C_0[i,j]-C[i,j]) > c_tol:
+            raise NameError('scalapack_inverse_cholesky failed!')
