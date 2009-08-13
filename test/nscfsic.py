c648833b507f6e27debab66aefc98da00a797be8
kuismam <kuismam@6419b465-d902-0410-997c-be2266baf723>
2009-08-13 14:00:52 +0000

    Fixed a typo, and specified nscfsic.py to run with newxccorrections
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4636 6419b465-d902-0410-997c-be2266baf723

--- test/nscfsic.py
+++ test/nscfsic.py
@@ -1,27 +1,36 @@
 from ase import *
-from gpaw import GPAW
+from gpaw import GPAW, extra_parameters
 from gpaw.utilities.sic import NSCFSIC
 
-atoms = ['He','Be'] #,'Ne'] # Ne deviates already 2.5 eV
-EE = []
-EREF = [-79.4,-399.8,-3517.6]
+usenewxc = extra_parameters.get('usenewxc')
+extra_parameters['usenewxc'] = True
 
-for a in atoms:
-    s = Atoms([Atom(a)])
-    s.center(vacuum=4.0)
-    calc = GPAW(h=0.15)
-    s.set_calculator(calc)
-    E = s.get_potential_energy()
-    EE.append(NSCFSIC(calc).calculate())
+try:
+    atoms = ['He','Be'] #,'Ne'] # Ne deviates already 2.5 eV
+    EE = []
+    EREF = [-79.4,-399.8,-3517.6]
 
-print "Difference to table VI of Phys. Rev. B 23, 5048 in eV"
-#http://prola.aps.org/abstract/PRB/v23/i10/p5048_1
-print "%10s%10s%10s%10s" % ("atom", "ref.", "gpaw", "diff")
-for a, er, e in zip(atoms, EREF, EE):
-    print "%10s%10.2f%10.2f%10.2f" % (a, er, e, er-e)
-    assert abs(er-e)<0.1 
-    # Arbitary 0.1 eV tolerance for non-self consistent SIC
-    # Note that Ne already deviates 2.5 eV
+    for a in atoms:
+        s = Atoms([Atom(a)])
+        s.center(vacuum=4.0)
+        calc = GPAW(h=0.15)
+        s.set_calculator(calc)
+        E = s.get_potential_energy()
+        EE.append(NSCFSIC(calc).calculate())
 
+    print "Difference to table VI of Phys. Rev. B 23, 5048 in eV"
+    #http://prola.aps.org/abstract/PRB/v23/i10/p5048_1
+    print "%10s%10s%10s%10s" % ("atom", "ref.", "gpaw", "diff")
+    for a, er, e in zip(atoms, EREF, EE):
+        print "%10s%10.2f%10.2f%10.2f" % (a, er, e, er-e)
+        assert abs(er-e)<0.1 
+        # Arbitary 0.1 eV tolerance for non-self consistent SIC
+        # Note that Ne already deviates 2.5 eV
 
+except:
+    extra_parameters['usenewxc'] = usenewxc
+    raise
+else:
+    extra_parameters['usenewxc'] = usenewxc
+    
 
