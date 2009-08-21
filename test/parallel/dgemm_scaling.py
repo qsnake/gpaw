68de314b3cd0a5260f9525d3c37420d8869074b3
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-08-21 16:13:45 +0000

    Closer in accuracy to bandpar test examaples.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4728 6419b465-d902-0410-997c-be2266baf723

--- test/parallel/dgemm_scaling.py
+++ test/parallel/dgemm_scaling.py
@@ -1,11 +1,11 @@
 from time import time
 import sys
 import numpy as np
-from gpaw.utilities.blas import gemm
+from gpaw.utilities.blas import gemm, scal
 from _gpaw import hpm_start, hpm_stop
 
-G = 20  # number of grid points (G x G x G)
-N = 2000  # total number of bands
+G = 15  # number of grid points (G x G x G)
+N = 2000 # total number of bands
 
 beta = 1.0
 dv = 0.1
@@ -18,8 +18,9 @@ for B in (1,2,4,8,16):
     M = N // B     # number of bands per group
     assert M * B == N
 
+    Q = B // 2 + 1
     C_bnbn = C_nn.reshape((B, M, B, M))
-    S_mm = np.random.uniform(-0.5, 0.5, (M,M))
+    S_mm = np.empty((Q,M,M))
 
     # Random wave functions:
     shape = (M, G, G, G)
@@ -32,22 +33,29 @@ for B in (1,2,4,8,16):
     ta = time()
     reg = "Overlap" + str(B)
     hpm_start(reg)
-    for n in range(B):
-        gemm(dv, tmp_mG, psit_mG, 0.0, S_mm, 'c')
+    for n in range(Q - 1):
+        gemm(dv, tmp_mG, psit_mG, 0.0, S_mm[n], 'c')
+        tmp_mG, psit_mG = psit_mG, tmp_mG
+    gemm(dv, tmp_mG, psit_mG, 0.0, S_mm[Q - 1], 'c')
     hpm_stop(reg)
     ttot = time() - ta
 
-    print "Overlap, B:", B, "Time:", ttot
+    # print "Overlap, B:", B, "Time:", ttot
 
     # Matrix multiply
     ttot = 0.0
     ta = time()
     reg = "Matrix_Mutiply" + str(B)
     hpm_start(reg)
-    for n in range(B):
+    beta = 0.0
+    for n in range(B - 1):
         C_mm = C_bnbn[0, :, n % B] 
         gemm(1.0, tmp_mG, C_mm, beta, psit_mG)
+        beta = 1.0
+        tmp_mG, psit_mG = psit_mG, tmp_mG
+    C_mm = C_bnbn[0, :, -1] 
+    gemm(1.0, tmp_mG, C_mm, beta, psit_mG)      
     hpm_stop(reg)
     ttot = time() - ta
 
-    print "Matrix_Multiply, B:", B, "Time:", ttot
+    # print "Matrix_Multiply, B:", B, "Time:", ttot
