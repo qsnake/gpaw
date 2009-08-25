5f18da36dfc857c02bf6bc7e8087e2a5a393f3cf
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-08-25 15:13:40 +0000

    use pbc solver at last
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4756 6419b465-d902-0410-997c-be2266baf723

--- gpaw/transport/newsurrounding.py
+++ gpaw/transport/newsurrounding.py
@@ -13,10 +13,39 @@ def collect_D_asp(density):
         all_D_asp.append(D_sp)      
     return all_D_asp
 
+def collect_D_asp2(D_asp, setups, ns, comm, rank_a):
+    all_D_asp = []
+    for a, setup in enumerate(setups):
+        D_sp = D_asp.get(a)
+        if D_sp is None:
+            ni = setup.ni
+            D_sp = np.empty((ns, ni * (ni + 1) // 2))
+        if comm.size > 1:
+            comm.broadcast(D_sp, rank_a[a])
+        all_D_asp.append(D_sp)      
+    return all_D_asp
+
+def collect_D_asp3(ham):
+    all_D_asp = []
+    for a, setup in enumerate(ham.setups):
+        D_sp = ham.dH_asp.get(a)
+        if D_sp is None:
+            ni = setup.ni
+            D_sp = np.empty((ham.nspins, ni * (ni + 1) // 2))
+        if ham.gd.comm.size > 1:
+            ham.gd.comm.broadcast(D_sp, ham.rank_a[a])
+        all_D_asp.append(D_sp)      
+    return all_D_asp
+
 def distribute_D_asp(D_asp, density):
     for a in range(len(density.setups)):
         if density.D_asp.get(a) is not None:
             density.D_asp[a] = D_asp[a]
+
+def distribute_D_asp2(dH_asp, hamiltonian):
+    for a in range(len(hamiltonian.setups)):
+        if hamiltonian.dH_asp.get(a) is not None:
+            hamiltonian.dH_asp[a] = dH_asp[a]
    
 class Side:
     def __init__(self, type, atoms, direction):
@@ -61,11 +90,12 @@ class Side:
         self.boundary_nt_sG = self.slice(nn, nt_sG)
         
         self.D_asp = collect_D_asp(calc.density)
+        self.dH_asp = collect_D_asp3(calc.hamiltonian)
         
     def slice(self, nn, in_array):
         if self.type == 'LR':
-            seq1 = np.arange(-nn + 1, 1)
-            #seq1 = np.arange(nn)            
+            #seq1 = np.arange(-nn + 1, 1)
+            seq1 = np.arange(nn)            
             seq2 = np.arange(nn)
             di = len(in_array.shape) - 1
             if self.direction == '-':
@@ -96,7 +126,7 @@ class Surrounding:
                 self.side_basis_index[direction] = self.tp.lead_index[i]                
                 self.nn.append(side.N_c[2])
             self.nn = np.array(self.nn)
-            self.operator = self.tp.hamiltonian.poisson.operators[0]
+            self.operator = self.tp.extended_calc.hamiltonian.poisson.operators[0]
             
         elif self.type == 'all':
             raise NotImplementError()
@@ -119,18 +149,18 @@ class Surrounding:
             
     def get_extra_density(self, vHt_g):
         if self.type == 'LR':
-            rhot_g = self.tp.finegd.zeros()
+            rhot_g = self.tp.finegd1.zeros()
             self.operator.apply(vHt_g, rhot_g)
             nn = self.nn[0] * 2
-            self.extra_rhot_g = self.uncapsule(nn, rhot_g, self.tp.finegd,
-                                                       self.tp.finegd0)
+            self.extra_rhot_g = self.uncapsule(nn, rhot_g, self.tp.finegd1,
+                                                       self.tp.finegd)
             self.boundary_charge = np.sum(
-                        self.sides['-'].boundary_rhot_g) * self.tp.finegd0.dv
+                        self.sides['-'].boundary_rhot_g) * self.tp.finegd.dv
             
 
-    def capsule(self, nn, loc_in_array, gd, gd0):
+    def capsule(self, nn, loc_in_array, in_cap_array, gd, gd0):
         ns = self.tp.nspins
-        cap_array = gd.collect(self.tp.hamiltonian.vHt_g, True)
+        cap_array = gd.collect(in_cap_array, True)
         in_array = gd0.collect(loc_in_array, True)
         if len(in_array.shape) == 4:
             local_cap_array = gd.zeros(ns)
@@ -143,12 +173,15 @@ class Surrounding:
     
     def uncapsule(self, nn, in_array, gd, gd0, nn2=None):
         nn1 = nn
+        ns = self.tp.nspins
         if nn2 == None:
             nn2 = nn1
         di = 2
         if len(in_array.shape) == 4:
             di += 1
-        local_uncap_array = gd0.zeros()
+            local_uncap_array = gd0.zeros(ns)
+        else:
+            local_uncap_array = gd0.zeros()            
         global_in_array = gd.collect(in_array, True)
         seq = np.arange(nn1, global_in_array.shape[di] - nn2)    
         uncap_array = np.take(global_in_array, seq, axis=di)
@@ -158,83 +191,65 @@ class Surrounding:
     def combine(self):
         if self.type == 'LR':
             nn = self.nn[0] * 2
-            ham = self.tp.hamiltonian
+            ham = self.tp.extended_calc.hamiltonian
             if ham.vt_sg is None:
                 ham.vt_sg = ham.finegd.empty(ham.nspins)
                 ham.vHt_g = ham.finegd.zeros()
                 ham.vt_sG = ham.gd.zeros(ham.nspins)
                 ham.poisson.initialize()
+                self.tp.inner_poisson.initialize()
+              
             vHt_g = ham.finegd.zeros(global_array=True)
-            #extra_vHt_g = ham.finegd.zeros(global_array=True)
-            #loc_extra_vHt_g = ham.finegd.zeros()
+            extra_vHt_g = ham.finegd.zeros(global_array=True)
+            loc_extra_vHt_g = ham.finegd.zeros()
+            self.nt_sg = ham.finegd.zeros(self.tp.nspins)
+            
 
             bias_shift0 = self.bias_index['-'] / Hartree
             bias_shift1 = self.bias_index['+'] / Hartree
             vHt_g[:, :, :nn] = self.sides['-'].boundary_vHt_g + bias_shift0
             vHt_g[:, :, -nn:] = self.sides['+'].boundary_vHt_g + bias_shift1
-            #extra_vHt_g[:, :, :nn] = bias_shift0
-            #extra_vHt_g[:, :, -nn:] = bias_shift1
+            extra_vHt_g[:, :, :nn] = bias_shift0
+            extra_vHt_g[:, :, -nn:] = bias_shift1
+            
+            self.nt_sg[:, :, :, :nn] = self.sides['-'].boundary_nt_sg
+            self.nt_sg[:, :, :, -nn:] = self.sides['+'].boundary_nt_sg
+
             ham.finegd.distribute(vHt_g, ham.vHt_g)
-            #ham.finegd.distribute(extra_vHt_g, loc_extra_vHt_g)
-            #self.get_extra_density(loc_extra_vHt_g)
-            self.get_extra_density(ham.vHt_g)
+            ham.finegd.distribute(extra_vHt_g, loc_extra_vHt_g)
+            self.get_extra_density(loc_extra_vHt_g)
+            #self.get_extra_density(ham.vHt_g)
             #self.calculate_extra_hartree_potential()
             #self.calculate_gate()
 
     def combine_vHt_g(self, vHt_g):
         nn = self.nn[0] * 2
-        self.tp.hamiltonian.vHt_g = self.capsule(nn, vHt_g, self.tp.finegd,
-                                                  self.tp.finegd0)
-    def refresh_vt_sG(self):
-        nn = self.nn[0]
-        gd = self.tp.gd
-        bias_shift0 = self.bias_index['-'] / Hartree
-        bias_shift1 = self.bias_index['+'] / Hartree        
-        vt_sG = gd.collect(self.tp.hamiltonian.vt_sG)
-        vt_sG[:, :, :, :nn] = self.sides['-'].boundary_vt_sG + bias_shift1
-        vt_sG[:, :, :, -nn:] = self.sides['+'].boundary_vt_sG + bias_shift2
-        gd.distribute(vt_sG, self.tp.hamiltonian.vt_sG)
-
-    def combine_nt_sG(self):
-        nn = self.nn[0]
-        gd = self.tp.gd
-        nt_sG = gd.collect(self.tp.density.nt_sG, True)
-        nt_sG[:, :, :, :nn] = self.sides['-'].boundary_nt_sG
-        nt_sG[:, :, :, -nn:] = self.sides['+'].boundary_nt_sG
-        gd.distribute(nt_sG, self.tp.density.nt_sG)
+        extended_vHt_g = self.tp.extended_calc.hamiltonian.vHt_g
+        vHt_g = self.capsule(nn, vHt_g, extended_vHt_g,
+                                             self.tp.finegd1, self.tp.finegd)
         
-    def combine_nt_sg(self):
-        nn = self.nn[0] * 2
-        gd = self.tp.finegd
-        nt_sg = gd.collect(self.tp.density.nt_sg, True)
-        nt_sg[:, :, :, :nn] = self.sides['-'].boundary_nt_sg
-        nt_sg[:, :, :, -nn:] = self.sides['+'].boundary_nt_sg
-        gd.distribute(nt_sg, self.tp.density.nt_sg)        
-
-    def combine_D_asp(self):
-        density = self.tp.density
-        all_D_asp = collect_D_asp(density)
-        nao = len(self.tp.original_atoms)
+    def combine_dH_asp(self, dH_asp):
+        ham = self.tp.extended_calc.hamiltonian
+        all_dH_asp = dH_asp[:]
         for i in range(self.lead_num):
             direction = self.directions[i]
             side = self.sides[direction]
             for n in range(side.n_atoms):
-                all_D_asp[nao + n] = side.D_asp[n]
-            nao += side.n_atoms
-        distribute_D_asp(all_D_asp, density)     
-       
-    def abstract_inner_rhot(self):
-        nn = self.nn[0] * 2
-        rhot_g = self.uncapsule(nn, self.tp.density.rhot_g, self.tp.finegd,
-                                                       self.tp.finegd0)
-        #rhot_g -= self.extra_rhot_g
-        return rhot_g
-
-    def calculate_extra_hartree_potential(self):
-        poisson = self.tp.inner_poisson
-        self.extra_vHt_g = np.zeros(self.extra_rhot_g.shape)
-        poisson.solve_neutral(self.extra_vHt_g, self.extra_rhot_g,
-                                               eps=poisson.eps)
+                all_dH_asp.append(side.dH_asp[n])
+        ham.dH_asp = {}
+        for a, D_sp in self.tp.extended_D_asp.items():
+            ham.dH_asp[a] = np.zeros_like(D_sp)
+        distribute_D_asp2(all_dH_asp, ham)
+        
+    def refresh_vt_sG(self):
+        nn = self.nn[0]
+        gd = self.tp.extended_calc.gd
+        bias_shift0 = self.bias_index['-'] / Hartree
+        bias_shift1 = self.bias_index['+'] / Hartree        
+        vt_sG = gd.collect(self.tp.extended_calc.hamiltonian.vt_sG)
+        vt_sG[:, :, :, :nn] = self.sides['-'].boundary_vt_sG + bias_shift0
+        vt_sG[:, :, :, -nn:] = self.sides['+'].boundary_vt_sG + bias_shift1
+        gd.distribute(vt_sG, self.tp.extended_calc.hamiltonian.vt_sG)
        
     def calculate_gate(self):
         gd0 = self.tp.finegd0
@@ -248,11 +263,12 @@ class Surrounding:
         global_gate_vHt_g[:, :, nn:-nn] = 1e-3
         gate_vHt_g = gd.zeros()
         gate_rhot_g = gd.zeros()
-        
+      
         gd.distribute(global_gate_vHt_g, gate_vHt_g)
         self.operator.apply(gate_vHt_g, gate_rhot_g)
         self.gate_vHt_g = self.uncapsule(nn, gate_vHt_g, gd, gd0)
         self.gate_rhot_g = self.uncapsule(nn, gate_rhot_g, gd, gd0)
+        
         
     def normalize(self, comp_charge):    
         nn = self.nn[0]
