ae9f5f71132f10ab535845cfcb1fb9c732addb34
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-09-01 07:58:50 +0000

    Generate sz basis for H.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4795 6419b465-d902-0410-997c-be2266baf723

--- test/jstm_test.py
+++ test/jstm_test.py
@@ -1,47 +1,52 @@
-from ase import Atoms
-from gpaw import GPAW
-from gpaw.transport.jstm import STM 
 import numpy as np
+from ase import Atoms
+from gpaw import GPAW, setup_paths
+from gpaw.transport.jstm import STM 
+from gpaw.atom.basis import BasisMaker
+
+basis = BasisMaker('H', 'sz').generate(1, 0)
+basis.write_xml()
+setup_paths.insert(0, '.')
 
 # GPAW calculations
 a = 0.75 # Bond length
-cell = np.diag([5,5,12*a])
+cell = np.diag([5, 5, 12 * a])
 
 atoms = Atoms('H12', pbc=(1, 1, 1), cell=cell) 
 atoms.positions[:, 2] = [i * a for i in range(12)]
 
 calc = GPAW(h=0.2,
-            width = 0.1,
+            width=0.1,
             mode='lcao',
             basis='sz',
-            usesymm = False)
+            usesymm=False)
 
 # Lead calculation
 lead = atoms.copy()
-del(lead[4:])
+del lead[4:]
 cell_lead = cell.copy()
-cell_lead[2,2]=a*len(lead)
+cell_lead[2, 2] = a * len(lead)
 lead.set_cell(cell_lead)
-calc.set(kpts=(1,1,14))
+calc.set(kpts=(1, 1, 14))
 lead.set_calculator(calc)
 lead.get_potential_energy()
 calc.write('lead.gpw')
 
 # Tip calculation
 tip = atoms.copy()
-tip.positions[:] += (atoms.cell/2.0)[0,:]+(atoms.cell/2.0)[1,:]
-del(tip[:4])
-tip.translate([0,0,10])
-tip.cell[2,2]+=10
-calc.set(kpts=(1,1,1))
+tip.positions[:] += (atoms.cell / 2.0)[0, :] + (atoms.cell / 2.0)[1, :]
+del tip[:4]
+tip.translate([0, 0, 10])
+tip.cell[2, 2] += 10
+calc.set(kpts=(1, 1, 1))
 tip.set_calculator(calc)
 tip.get_potential_energy()
 calc.write('tip.gpw')
 
 # Surface calculation
 srf = atoms.copy()
-del(srf[8:])
-srf.cell[2,2]+=10
+del srf[8:]
+srf.cell[2, 2] += 10
 srf.set_calculator(calc)
 srf.get_potential_energy()
 calc.write('srf.gpw')
@@ -66,6 +71,6 @@ stm.scan()
 stm.linescan()
 
 if 0:
-    stm.plot(repeat = [3,3])
+    stm.plot(repeat=[3, 3])
 
 
