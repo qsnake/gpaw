05f57c54afd2efbed4e15b5fb2d14f648af74273
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2009-09-01 11:29:08 +0000

    new customize on thul
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4804 6419b465-d902-0410-997c-be2266baf723

--- doc/install/Linux/Niflheim/customize-fjorm.py
+++ doc/install/Linux/Niflheim/customize-fjorm.py
@@ -1,7 +1,9 @@
 scalapack = True
 compiler = 'gcc43'
 libraries =[
-        'gfortran','goto','acml','scalapack','mpiblacsF77init','mpiblacs','scalapack','mpi','mpi_f77'
+        'gfortran','goto','acml',
+        'scalapack','mpiblacsF77init','mpiblacs','scalapack',
+        'mpi','mpi_f77'
         ]
 library_dirs =[
         '/opt/openmpi/1.3.3-1.el5.fys.gfortran43.4.3.2/lib64',
