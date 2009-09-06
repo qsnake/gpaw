be25b851deeb79ec55940bd37ec03c7c7cae0a1e
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2009-09-06 08:11:12 +0000

    another change of customize.py on thul: atlas sometimes crashes or is extremely slow
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4844 6419b465-d902-0410-997c-be2266baf723

--- doc/install/Linux/Niflheim/customize-thul-netlib.py
+++ doc/install/Linux/Niflheim/customize-thul-netlib.py
@@ -1,27 +1,23 @@
 scalapack = True
 compiler = 'gcc43'
 libraries = [
-    'gfortran', 'goto', 'acml',
-    'scalapack', 'mpiblacsF77init', 'mpiblacs', 'scalapack',
-    'mpi','mpi_f77'
+    'gfortran', 'scalapack', 'mpiblacsF77init', 'mpiblacs', 'scalapack',
+    'blas', 'lapack',
+    'mpi', 'mpi_f77'
     ]
 library_dirs = [
     '/opt/openmpi/1.3.3-1.el5.fys.gfortran43.4.3.2/lib64',
-    '/opt/goto/1.26/1.el5.fys.gfortran43.4.3.2.smp/lib64',
-    '/opt/acml/4.3.0/gfortran4364/lib',
     '/opt/blacs/1.1/24.el5.fys.gfortran43.4.3.2.openmpi.1.3.3/lib64',
-    '/opt/scalapack/1.8.0/1.el5.fys.gfortran43.4.3.2.openmpi.1.3.3.goto.1.26.acml.4.3.0/lib64'
+    '/opt/scalapack/1.8.0/1.el5.fys.gfortran43.4.3.2.openmpi.1.3.3.blas.3.0.37.el5.lapack.3.0.37.el5/lib64'
     ]
 include_dirs += ['/opt/openmpi/1.3.3-1.el5.fys.gfortran43.4.3.2/include']
 extra_link_args = [
     '-Wl,-rpath=/opt/openmpi/1.3.3-1.el5.fys.gfortran43.4.3.2/lib64,'
-    '-rpath=/opt/goto/1.26/1.el5.fys.gfortran43.4.3.2.smp/lib64,'
-    '-rpath=/opt/acml/4.3.0/gfortran4364/lib,'
     '-rpath=/opt/blacs/1.1/24.el5.fys.gfortran43.4.3.2.openmpi.1.3.3/lib64,'
-    '-rpath=/opt/scalapack/1.8.0/1.el5.fys.gfortran43.4.3.2.openmpi.1.3.3.goto.1.26.acml.4.3.0/lib64'
+    '-rpath=/opt/scalapack/1.8.0/1.el5.fys.gfortran43.4.3.2.openmpi.1.3.3.blas.3.0.37.el5.lapack.3.0.37.el5/lib64'
     ]
 extra_compile_args = ['-O3', '-std=c99', '-funroll-all-loops', '-fPIC']
 define_macros += [('GPAW_MKL', '1')]
 mpicompiler = '/opt/openmpi/1.3.3-1.el5.fys.gfortran43.4.3.2/bin/mpicc'
 mpilinker = mpicompiler
-platform_id = 'opteron'
+platform_id = 'xeon'
