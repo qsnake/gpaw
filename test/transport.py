8e49b20671936245cfcbf6f73b73b45eac54f1a8
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-09-14 13:55:06 +0000

    use sparse matrix
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4919 6419b465-d902-0410-997c-be2266baf723

--- test/transport.py
+++ test/transport.py
@@ -6,35 +6,32 @@ from gpaw.poisson import PoissonSolver
 import pickle
 
 a = 3.6
-L = 5.00
+L = 7.00
 
 basis = BasisMaker('Na').generate(1, 1, energysplit=0.3)
 
-atoms = Atoms('Na4', pbc=(1, 1, 1), cell=[L, L, 4 * a])
-atoms.positions[:4, 2] = [i * a for i in range(4)]
+atoms = Atoms('Na9', pbc=(0, 0, 1), cell=[L, L, 9 * a])
+atoms.positions[:9, 2] = [i * a for i in range(9)]
 atoms.positions[:, :2] = L / 2.
 atoms.center()
 pl_atoms1 = range(4)     
-pl_atoms2 = range(4)
+pl_atoms2 = range(5, 9)
 pl_cell1 = (L, L, 4 * a) 
 pl_cell2 = pl_cell1
 t = Transport(h=0.3,
               xc='LDA',
               basis={'Na': basis},
-              kpts=(1,1,5),
+              kpts=(1,1,1),
               width=0.1,
               mode='lcao',
-              poissonsolver=PoissonSolver(relax='GS'),
-              use_linear_vt_mm=True,
+              poissonsolver=PoissonSolver(nn=2, relax='GS'),
               txt='Na_lcao.txt',
-              usesymm=None,
-              fixed_boundary=False,
-              identical_leads=False,
               mixer=Mixer(0.1, 5, weight=100.0),
               pl_atoms=[pl_atoms1, pl_atoms2],
               pl_cells=[pl_cell1, pl_cell2],
               pl_kpts=(1,1,5),
-              bias=[0,0],
-             )
+              edge_atoms=[[0, 3], [0, 8]],
+              mol_atoms=[4],
+              guess_steps=1)
 atoms.set_calculator(t)
 t.calculate_iv(0.5, 2)
