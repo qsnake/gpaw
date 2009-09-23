08a42ad968e73af1985ab59c479183120ec2a1e4
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-09-23 19:32:02 +0000

    non-sc plotting
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@4988 6419b465-d902-0410-997c-be2266baf723

--- doc/exercises/transport/analysis.py
+++ doc/exercises/transport/analysis.py
@@ -1,8 +1,7 @@
 from gpaw.transport.analysor import Transport_Plotter
 from pylab import *
 
-plotter = Transport_Plotter()
-plot(plotter.bias_steps[0].tc[0,0,0])
-show()
-
+plotter = Transport_Plotter('bias', 'Pt_H2_nsc.dat')
+plotter.set_ele_steps(None, 0)
+plotter.plot_ele_step_info('ham', [0], 0,0)
 
