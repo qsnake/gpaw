a0cefc2cd099ef23a0d3a910d32aebd5332f1da9
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-10-02 06:42:21 +0000

    Using h=0.2 instead of 0.5.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5079 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/nsc_MGGA.py
+++ gpaw/test/nsc_MGGA.py
@@ -1,22 +1,13 @@
-import os
 from ase import *
 from gpaw.test import equal
 from gpaw import GPAW
-from gpaw.atom.generator import Generator, parameters
-from gpaw.xc_functional import XCFunctional
-from gpaw import setup_paths
-from gpaw.test import equal
+from gpaw.atom.generator import Generator
 
-# test with revision 4432
+g = Generator('H', 'TPSS', scalarrel=True, nofiles=True)
 
-symbol = 'H'
-g = Generator(symbol, 'TPSS', scalarrel=True, nofiles=True)
-
-a = 6 * Bohr
-n = 12
-atoms = Atoms([Atom('H', (0.0, 0.0, 0.0),magmom=1)], cell=(a, a, a), pbc=True)
+atoms = Atoms('H', magmoms=[1], pbc=True)
 atoms.center(vacuum=3)
-calc = GPAW(gpts=(n, n, n), nbands=1, xc='PBE', txt='Hnsc.txt')
+calc = GPAW(nbands=1, xc='PBE', txt='Hnsc.txt')
 atoms.set_calculator(calc)
 e1 = atoms.get_potential_energy()
 e1ref = calc.get_reference_energy()
@@ -27,21 +18,18 @@ print '================'
 print 'e1 = ', e1
 print 'de12t = ', de12t
 print 'de12m = ', de12m
-print 'tpss = ', e1+de12t
-print 'm06l = ', e1+de12m
+print 'tpss = ', e1 + de12t
+print 'm06l = ', e1 + de12m
 print '================'
 
-equal(e1+de12t, 15.70495612949, 0.005)
-equal(e1+de12m, 15.775609818, 0.005)
+equal(e1 + de12t, -1.13140473115, 0.005)
+equal(e1 + de12m, -1.19069632478, 0.005)
 
-symbol = 'He'
-g = Generator(symbol, 'TPSS', scalarrel=True, nofiles=True)
+g = Generator('He', 'TPSS', scalarrel=True, nofiles=True)
 
-a = 6 * Bohr
-n = 12
-atomsHe = Atoms([Atom('He', (0.0, 0.0, 0.0))], cell=(a, a, a), pbc=True)
+atomsHe = Atoms('He', pbc=True)
 atomsHe.center(vacuum=3)
-calc = GPAW(gpts=(n, n, n), nbands=1, xc='PBE', txt='Hensc.txt')
+calc = GPAW(nbands=1, xc='PBE', txt='Hensc.txt')
 atomsHe.set_calculator(calc)
 e1He = atomsHe.get_potential_energy()
 e1refHe = calc.get_reference_energy()
@@ -52,9 +40,9 @@ print '================'
 print 'e1He = ', e1He
 print 'de12tHe = ', de12tHe
 print 'de12mHe = ', de12mHe
-print 'tpss = ', e1He+de12tHe
-print 'm06l = ', e1He+de12mHe
+print 'tpss = ', e1He + de12tHe
+print 'm06l = ', e1He + de12mHe
 print '================'
 
-equal(e1He+de12tHe, 3.21494509843, 0.005)
-equal(e1He+de12mHe, 2.60645239522, 0.005)
+equal(e1He+de12tHe, -0.448532905095, 0.005)
+equal(e1He+de12mHe, -0.51400253951, 0.005)
