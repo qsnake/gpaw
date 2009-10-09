4208783b5e7e70dd7754a71f4ba67bec15fa20e4
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-10-09 10:14:23 +0000

    Don't use 'ps' command.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5150 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/ltt.py
+++ gpaw/test/ltt.py
@@ -1,4 +1,3 @@
-import os
 from ase import *
 from gpaw import GPAW
 from gpaw.test import equal
@@ -10,13 +9,8 @@ H = Atoms('H', [(b - 0.1, b, b)], pbc=True, cell=(a, a, a))
 calc = GPAW(nbands=1, gpts=(n, n, n), txt='ltt.txt')
 H.set_calculator(calc)
 e0 = H.get_potential_energy()
-cmd = 'ps -eo comm,pmem | grep python'
-mem0 = float(os.popen(cmd).readlines()[-1].split()[-1])
 for i in range(50):
     e = H.get_potential_energy()
     H.positions += (0.09123456789, 0.0423456789, 0.03456789)
-mem = float(os.popen(cmd).readlines()[-1].split()[-1])
 equal(e, e0, 0.0006)
 print e, e0, e-e0
-print mem, mem0
-assert mem < mem0 + 0.21
