38766196a16e7ac312aca98401d1698e2c4e9c81
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-10-12 12:50:34 +0000

    Do only one MPI_Reduce + MPI_Bcast instead of several MPI_Allreduce.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5163 6419b465-d902-0410-997c-be2266baf723

--- gpaw/forces.py
+++ gpaw/forces.py
@@ -30,29 +30,34 @@ class ForceCalculator:
         # Force from projector functions (and basis set):
         wfs.calculate_forces(hamiltonian, self.F_av)
 
-        wfs.band_comm.sum(self.F_av)
-        wfs.kpt_comm.sum(self.F_av)
+        wfs.band_comm.sum(self.F_av, 0)
+
+        if wfs.band_comm.rank == 0:
+            wfs.kpt_comm.sum(self.F_av, 0)
+
+        if wfs.band_comm.rank == 0 and wfs.kpt_comm.rank == 0:
+            # Force from compensation charges:
+            dF_aLv = density.ghat.dict(derivative=True)
+            density.ghat.derivative(hamiltonian.vHt_g, dF_aLv)
+            for a, dF_Lv in dF_aLv.items():
+                self.F_av[a] += np.dot(density.Q_aL[a], dF_Lv)
+
+            # Force from smooth core charge:
+            dF_av = density.nct.dict(derivative=True)
+            density.nct.derivative(vt_G, dF_av)
+            for a, dF_v in dF_av.items():
+                self.F_av[a] += dF_v[0]
+
+            # Force from zero potential:
+            dF_av = hamiltonian.vbar.dict(derivative=True)
+            hamiltonian.vbar.derivative(density.nt_g, dF_av)
+            for a, dF_v in dF_av.items():
+                self.F_av[a] += dF_v[0]
+
+            wfs.gd.comm.sum(self.F_av, 0)
+
+        wfs.world.broadcast(self.F_av, 0)
         
-        # Force from compensation charges:
-        dF_aLv = density.ghat.dict(derivative=True)
-        density.ghat.derivative(hamiltonian.vHt_g, dF_aLv)
-        for a, dF_Lv in dF_aLv.items():
-            self.F_av[a] += np.dot(density.Q_aL[a], dF_Lv)
-
-        # Force from smooth core charge:
-        dF_av = density.nct.dict(derivative=True)
-        density.nct.derivative(vt_G, dF_av)
-        for a, dF_v in dF_av.items():
-            self.F_av[a] += dF_v[0]
-
-        # Force from zero potential:
-        dF_av = hamiltonian.vbar.dict(derivative=True)
-        hamiltonian.vbar.derivative(density.nt_g, dF_av)
-        for a, dF_v in dF_av.items():
-            self.F_av[a] += dF_v[0]
-
-        wfs.gd.comm.sum(self.F_av)
-
         # Add non-local contributions:
         for kpt in wfs.kpt_u:
             self.F_av += hamiltonian.xcfunc.get_non_local_force(kpt)
