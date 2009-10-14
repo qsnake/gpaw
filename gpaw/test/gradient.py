Fix test for Gradient class (c -> v).
git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5172 6419b465-d902-0410-997c-be2266baf723

gpaw/test/gradient.py
@@ -6,7 +6,7 @@ gd = GridDescriptor((8, 1, 1), (8.0, 1.0, 1.0))
 a = gd.zeros()
 dadx = gd.zeros()
 a[:, 0, 0] = np.arange(gd.beg_c[0], gd.end_c[0])
-gradx = Gradient(gd, c=0)
+gradx = Gradient(gd, v=0)
 print a.itemsize, a.dtype, a.shape
 print dadx.itemsize, dadx.dtype, dadx.shape
 gradx.apply(a, dadx)
@@ -23,7 +23,7 @@ assert dadx[3, 0, 0] == 1.0 and np.sum(dadx[:, 0, 0]) == 0.0
 gd = GridDescriptor((1, 8, 1), (1.0, 8.0, 1.0), (1, 0, 1))
 dady = gd.zeros()
 a = gd.zeros()
-grady = Gradient(gd, c=1)
+grady = Gradient(gd, v=1)
 a[0, :, 0] = np.arange(gd.beg_c[1], gd.end_c[1]) - 1
 grady.apply(a, dady)
 