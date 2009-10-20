26c9e8f238564671eeb198bfae344d7e89308055
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2009-10-20 12:07:28 +0000

    AngularIntegral introduced, cleanup
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5222 6419b465-d902-0410-997c-be2266baf723

--- gpaw/analyse/expandyl.py
+++ gpaw/analyse/expandyl.py
@@ -1,6 +1,6 @@
 from math import pi
 
-import numpy as npy
+import numpy as np
 from ase.units import Bohr, Hartree
 
 import gpaw.mpi as mpi
@@ -8,7 +8,112 @@ from gpaw.spherical_harmonics import Y
 from gpaw.utilities.vector import Vector3d
 from gpaw.utilities.timing import StepTimer
 
-class ExpandYl:
+class AngularIntegral:
+    """Perform an angular integral on the grid.
+
+    center:
+      the center (Ang)
+    gd:
+      grid_descriptor of the grids to expand
+    Rmax:
+      maximal radius of the expansion (Ang)
+    dR:
+      grid spacing in the radius (Ang)
+    """
+    def __init__(self, center, gd, Rmax=None, dR=None):
+
+        center = Vector3d(center) / Bohr
+
+        self.center = center
+        self.gd = gd
+
+        # set Rmax to the maximal radius possible
+        # i.e. the corner distance
+        if not Rmax:
+            Rmax = 0
+            extreme = gd.h_c * gd.N_c
+            for corner in ([0,0,0],[1,0,0],[0,1,0],[1,1,0],
+                           [0,0,1],[1,0,1],[0,1,1],[1,1,1]):
+                Rmax = max(Rmax,
+                           self.center.distance(np.array(corner) * 
+                                                extreme) )
+        else:
+            Rmax /= Bohr
+        self.Rmax = Rmax
+
+        if not dR:
+            dR = min(gd.h_c)
+        else:
+            dR /= Bohr
+        self.dR = dR
+
+        self.initialize()
+
+    def initialize(self):
+        """Initialize grids."""
+        
+        Rmax = self.Rmax
+        dR = self.dR
+        gd = self.gd
+
+        # initialize the ylm and Radial grids
+
+        # self.V_R will contain the volume of the R shell
+        # self.R_R will contain the mean radius of the R shell
+        # self.R_g will contain the radial indicees corresponding to
+        #     each grid point
+        # self.ball_g will contain the mask of the ball of radius Rmax
+        nR = int(Rmax / dR + 1)
+        V_R = np.zeros((nR,))
+        R_R = np.zeros((nR,))
+        R_g = gd.zeros(dtype=int) - 1
+        ball_g = gd.zeros(dtype=int)
+        for i in range(gd.beg_c[0], gd.end_c[0]):
+            ii = i - gd.beg_c[0]
+            for j in range(gd.beg_c[1], gd.end_c[1]):
+                jj = j - gd.beg_c[1]
+                for k in range(gd.beg_c[2], gd.end_c[2]):
+                    kk = k - gd.beg_c[2]
+                    vr = self.center - Vector3d([i * gd.h_c[0],
+                                                 j * gd.h_c[1],
+                                                 k * gd.h_c[2]])
+                    r = vr.length()
+                    if r>0 and r<Rmax:
+                        rhat = vr / r
+                        iR = int(r / dR)
+                        R_g[ii,jj,kk] = iR
+                        ball_g[ii,jj,kk] = 1
+                        V_R[iR] += 1
+                        R_R[iR] += r            
+        gd.comm.sum(V_R)
+        gd.comm.sum(R_R)
+
+        self.R_g = R_g
+        self.ball_g = ball_g
+        self.V_R = V_R * gd.dv
+        self.R_R = R_R / V_R
+
+    def integrate(self, f_g):
+        """Integrate a function on the grid"""
+        
+        int_R = []
+        for i, dV in enumerate(self.V_R):
+            # get the R shell
+            R_g = np.where(self.R_g == i, 1, 0)
+            int_R.append(self.gd.integrate(f_g * R_g))
+
+        return np.array(int_R) / self.radii()**2
+
+    def radii(self, model='nominal'):
+        """Return the radii of the radial shells"""
+        if model == 'nominal':
+            return self.dR * (np.arange(len(self.V_R)) + .5)
+        elif model == 'mean':
+            return self.R_R
+        else:
+            raise NonImplementedError
+
+class ExpandYl(AngularIntegral):
     """Expand the smooth wave functions in spherical harmonics
     relative to a given center.
 
@@ -23,89 +128,74 @@ class ExpandYl:
     dR:
       grid spacing in the radius (Ang)
     """
-    def __init__(self,center,gd,lmax=6,Rmax=None,dR=None):
+    def __init__(self, center, gd, lmax=6, Rmax=None, dR=None):
 
-        a0 = Bohr
-        center = Vector3d(center) / a0
-
-        self.a0 = a0
-        self.center = center
-        self.lmax=lmax
-        self.gd = gd
-
-        self.L_l= []
+        self.lmax = lmax
+        self.L_l = []
         for l in range(lmax+1):
             for m in range(2*l+1):
                 self.L_l.append(l)
+
+        AngularIntegral.__init__(self, center, gd, Rmax, dR)
+
+    def initialize(self):
+        """Initialize grids."""
+        
+        center = self.center
+        Rmax = self.Rmax
+        dR = self.dR
+        gd = self.gd
         nL = len(self.L_l)
 
-        # set Rmax to the maximal radius possible
-        # i.e. the corner distance
-        if not Rmax:
-            Rmax = 0
-            extreme = gd.h_c*gd.N_c
-            for corner in ([0,0,0],[1,0,0],[0,1,0],[1,1,0],
-                           [0,0,1],[1,0,1],[0,1,1],[1,1,1]):
-                Rmax = max(Rmax,
-                           self.center.distance(npy.array(corner) * extreme) )
-        else:
-            Rmax /= a0
-        self.Rmax = Rmax
-
-        if not dR:
-            dR = min(gd.h_c)
-        else:
-            dR /= a0 
-        self.dR = dR
-        
         # initialize the ylm and Radial grids
 
-        # self.R_V will contain the volume of the R shell
+        # self.V_R will contain the volume of the R shell
         # self.R_g will contain the radial indicees corresponding to
         #     each grid point
         # self.ball_g will contain the mask of the ball of radius Rmax
         # self.y_Lg will contain the YL values corresponding to
         #     each grid point
-        R_V = npy.zeros((int(Rmax / dR + 1),))
+        V_R = np.zeros((int(Rmax / dR + 1),))
         y_Lg = gd.zeros((nL,))
-        R_g = npy.zeros(y_Lg[0].shape, int) - 1
-        ball_g = npy.zeros(y_Lg[0].shape, int)
+        R_g = np.zeros(y_Lg[0].shape, int) - 1
+        ball_g = np.zeros(y_Lg[0].shape, int)
         for i in range(gd.beg_c[0],gd.end_c[0]):
             ii = i - gd.beg_c[0]
             for j in range(gd.beg_c[1],gd.end_c[1]):
                 jj = j - gd.beg_c[1]
                 for k in range(gd.beg_c[2],gd.end_c[2]):
                     kk = k - gd.beg_c[2]
-                    vr = center - Vector3d([i*gd.h_c[0],
-                                            j*gd.h_c[1],
-                                            k*gd.h_c[2]])
+                    vr = self.center - Vector3d([i * gd.h_c[0],
+                                                 j * gd.h_c[1],
+                                                 k * gd.h_c[2]])
                     r = vr.length()
                     if r>0 and r<Rmax:
                         rhat = vr/r
                         for L in range(nL):
-                            y_Lg[L,ii,jj,kk] = Y(L, rhat[0], rhat[1], rhat[2])
+                            y_Lg[L,ii,jj,kk] = Y(L, rhat[0], 
+                                                 rhat[1], rhat[2])
                         iR = int(r/dR)
                         R_g[ii,jj,kk] = iR
                         ball_g[ii,jj,kk] = 1
-                        R_V[iR] += 1
-        gd.comm.sum(R_V)
+                        V_R[iR] += 1
+        gd.comm.sum(V_R)
 
         self.R_g = R_g
         self.ball_g = ball_g
-        self.R_V = R_V * gd.dv
+        self.V_R = V_R * gd.dv
         self.y_Lg = y_Lg
 
     def expand(self,psit_g):
         """Expand a wave function"""
       
-        gamma_l = npy.zeros((self.lmax+1))
+        gamma_l = np.zeros((self.lmax+1))
         nL = len(self.L_l)
         L_l = self.L_l
         dR = self.dR
         
-        for i,dV in enumerate(self.R_V):
+        for i,dV in enumerate(self.V_R):
             # get the R shell and it's Volume
-            R_g = npy.where(self.R_g == i, 1, 0)
+            R_g = np.where(self.R_g == i, 1, 0)
             if dV > 0:
                 for L in range(nL):
                     psit_LR = self.gd.integrate(psit_g * R_g * self.y_Lg[L])
@@ -143,9 +233,9 @@ class ExpandYl:
 
         print >> f, '# Yl expansion','of smooth wave functions'
         lu = 'Angstrom'
-        print >> f, '# center =',self.center * self.a0, lu
-        print >> f, '# Rmax =', self.Rmax * self.a0, lu
-        print >> f, '# dR =', self.dR * self.a0, lu
+        print >> f, '# center =', self.center * Bohr, lu
+        print >> f, '# Rmax =', self.Rmax * Bohr, lu
+        print >> f, '# dR =', self.dR * Bohr, lu
         print >> f, '# lmax =', self.lmax 
         print >> f, '# s    k     n',
         print >> f, '     e[eV]      occ',
@@ -164,7 +254,7 @@ class ExpandYl:
                     norm = self.gd.integrate(psit_G**2)
 
                     gl, weight = self.expand(psit_G)
-                    gsum = npy.sum(gl)
+                    gsum = np.sum(gl)
                     gl = 100 * gl / gsum
 
                     print >> f, '%2d %5d %5d' % (s, k, n),
