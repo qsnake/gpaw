71b42a5ff7f458b02403d6226409036e1e643616
kuismam <kuismam@6419b465-d902-0410-997c-be2266baf723>
2009-10-29 17:00:59 +0000

    Fix 1D-atomic test for GLLB.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5322 6419b465-d902-0410-997c-be2266baf723

--- test/gllb/atomic1d.py
+++ test/gllb/atomic1d.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from ase import *
-from gpaw.utilities import equal
 from gpaw.atom.all_electron import AllElectron
+from gpaw.test import equal
 
 data = []
 def out(a,b,c,d,e,f):
@@ -12,23 +12,27 @@ EX = {'Be': -2.666 - 0.010, 'Ne': -12.107 -0.122, 'Mg': -15.992 -0.092 }
 EHOMO = {'Be': -0.309 + 0.008, 'Ne': -0.851 + 0.098, 'Mg': -0.253 + 0.006}
 eignum = {'Be': 0, 'Ne':3, 'Mg':0 }
 
-xcname = 'GLLB'
-atoms = ['Be','Ne','Mg']
-for atom in atoms:
-    # Test AllElectron GLLB
-    GLLB = AllElectron(atom, xcname = xcname, scalarrel = False, gpernode = 600)
-    GLLB.run()
+for xcname in ['GLLB','GLLBSC']:
+    atoms = ['Be','Ne','Mg']
+    for atom in atoms:
+        # Test AllElectron GLLB
+        GLLB = AllElectron(atom, xcname = xcname, scalarrel = False, gpernode = 600)
+        GLLB.run()
+        
+        out("Total energy", xcname+"1D", atom,  ETotal[atom] , GLLB.ETotal,"Ha")
+        out("Exchange energy", xcname+"1D", atom, EX[atom], GLLB.Exc,"Ha")
+        out("HOMO Eigenvalue", xcname+"1D", atom, EHOMO[atom], GLLB.e_j[-1],"Ha")
+        if xcname == 'GLLB':
+            equal(GLLB.ETotal, ETotal[atom], tolerance=1e-2)
+            equal(GLLB.Exc, EX[atom], tolerance=1e-2)
+            equal(GLLB.e_j[-1], EHOMO[atom], tolerance=1e-2)
+            
+    print "             Quanity        Method    Symbol     Ref[1]         GPAW      Unit  "
+    for a,b,c,d,e,f in data:
+        print "%20s %10s %10s   %10.3f   %10.3f   %5s" % (a,b,c,d,e,f)
     
-    out("Total energy", "1D", atom,  ETotal[atom] , GLLB.ETotal,"Ha")
-    out("Exchange energy", "1D", atom, EX[atom], GLLB.Exc,"Ha")
-    out("HOMO Eigenvalue", "1D", atom, EHOMO[atom], GLLB.e_j[-1],"Ha")
-
-print "             Quanity        Method    Symbol     Ref[1]         GPAW      Unit  "
-for a,b,c,d,e,f in data:
-    print "%20s %10s %10s   %10.3f   %10.3f   %5s" % (a,b,c,d,e,f)
-    
-#        print """References:
-#[1] Self-consistent approximation to the Kohn-Sham exchange potential
-#Gritsenko, Oleg; Leeuwen, Robert van; Lenthe, Erik van; Baerends, Evert Jan
-#Phys. Rev. A Vol. 51 p. 1944"""
+print """References:
+[1] Self-consistent approximation to the Kohn-Sham exchange potential
+Gritsenko, Oleg; Leeuwen, Robert van; Lenthe, Erik van; Baerends, Evert Jan
+Phys. Rev. A Vol. 51 p. 1944"""
 
