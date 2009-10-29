1dbab3547d9606d0b2215d873f065ffa4aa7cc28
kuismam <kuismam@6419b465-d902-0410-997c-be2266baf723>
2009-10-29 20:07:29 +0000

    Test Dxc in parallel for atom. First version of guc test.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5325 6419b465-d902-0410-997c-be2266baf723

--- test/gllb/si.py
+++ test/gllb/si.py
@@ -1,9 +1,9 @@
 from gpaw import GPAW, restart
 from ase import *
 from ase.calculators import numeric_force
-from gpaw.utilities import equal
+from gpaw.test import equal
 
-def zincblende(symbol1, symbol2, a):
+def zincblende4(symbol1, symbol2, a):
     """Zinc Blende - Zinc Sulfide"""
     atoms = Atoms(symbols='%s2%s2' % (symbol1, symbol2), pbc=True,
                   positions=[(.0, .0, .0),
@@ -13,34 +13,47 @@ def zincblende(symbol1, symbol2, a):
     atoms.set_cell([a / sqrt(2), a / sqrt(2), a], scale_atoms=True)
     return atoms
 
+def zincblende8(symbol1, symbol2, a):
+    atoms = Atoms(symbols='%s4%s4' % (symbol1, symbol2), pbc=True,
+                  positions=[(0, 0, 0),
+                             (0, 0.5, 0.5),
+                             (0.5, 0, 0.5),
+                             (0.5, 0.5, 0),
+                             (0.25, 0.25, 0.25),
+                             (0.25, 0.75, 0.75),
+                             (0.75, 0.25, 0.75),
+                             (0.75, 0.75, 0.25)])
+    atoms.set_cell([a,a,a], scale_atoms=True)
+    return atoms
+
 xc = 'GLLB'
 a = 5.404 # Si
 if 1:
-	bulk = zincblende('Si','Si', a)
+    # 8 atom unit cell
+    bulk = zincblende8('Si','Si', a)
+    calc = GPAW(nbands=8*3,
+                width=0.01,
+                kpts=(10, 10, 10), h = 0.25, xc=xc)
+    bulk.set_calculator(calc)
+    E8_tot = bulk.get_potential_energy()
+    calc.write('Si8_GLLB.gpw')
+    response = calc.hamiltonian.xc.xcfunc.xc.xcs['RESPONSE']
+    response.calculate_delta_xc()
+    EKs, Dxc = response.calculate_delta_xc_perturbation()
 
-	calc = GPAW(nbands=8*3,
-	            width=0.01,
-	            kpts=(5, 5, 7), h = 0.25, xc=xc)
-	bulk.set_calculator(calc)
-	bulk.get_potential_energy()
-	eigs = calc.get_eigenvalues(kpt=0)
-	#calc.write('temp.gpw')
-        response = calc.hamiltonian.xc.xcfunc.xc.xcs['RESPONSE']
-        response.calculate_delta_xc_perturbation()
+    # 4 atom unit cell
+    bulk = zincblende4('Si','Si', a)
+    calc = GPAW(nbands=8*3,
+                width=0.01,
+                kpts=(10, 10, 10), h = 0.25, xc=xc)
+    bulk.set_calculator(calc)
+    E4_tot = bulk.get_potential_energy()
+    calc.write('Si4_GLLB.gpw')
+    response = calc.hamiltonian.xc.xcfunc.xc.xcs['RESPONSE']
+    response.calculate_delta_xc()
+    EKs2, Dxc2 = response.calculate_delta_xc_perturbation()
 
-	del bulk
-	del calc
-
-if 0:
-    N = 15
-    d = 0.5 / N
-    N1 = N +1
-    q = 1e-5
-    kk1 = [ [ q,q,q+i*d ] for i in range(0,N1) ]
-    kk2 = [ [ q,q+i*d, 0.5+q ] for i in range(0,N1) ]
-    kk3 = [ [ q+i*0.05,q+0.5, q+0.5 ] for i in range(0,N1) ]
-    kk4 = [ [ q+0.5-d*i,q+0.5-d*i, q+0.5-d*i ] for i in range(0,N1) ]
-    bulk, calc = restart('temp.gpw', fixdensity=True, kpts=kk1+kk2+kk3+kk4)
-    bulk.get_potential_energy()
-    calc.write('si_bands.gpw')
-    
+    equals(E4_tot*2, E8_tot, 1e-2)
+    equals(Eks, Eks2, 1e-2)
+    equals(Dxc, Dxc2, 1e-2)
+        
