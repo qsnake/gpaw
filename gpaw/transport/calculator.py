b2c9bea0087f3ba331d891bbf067d075dfc57adc
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2009-10-30 15:03:50 +0000

    adapt to the change in occupations
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5334 6419b465-d902-0410-997c-be2266baf723

--- gpaw/transport/calculator.py
+++ gpaw/transport/calculator.py
@@ -1108,7 +1108,7 @@ class Transport(GPAW):
         return cvg
  
     def initialize_scf(self):
-        self.intctrl = IntCtrl(self.occupations.width * Hartree,
+        self.intctrl = IntCtrl(self.occupations.kT * Hartree,
                                 self.lead_fermi, self.bias,
                                 self.env_bias, self.min_energy,
                                 self.neintmethod, self.neintstep)            
