dd77d4a865042e4458818ce5ff0a2dba353b0d4a
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-10-30 10:38:31 +0000

    Add test for non-selfconsistent EXX.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5331 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/atomize.py
+++ gpaw/test/atomize.py
@@ -14,8 +14,10 @@ atom.set_calculator(calc)
 
 e1 = atom.get_potential_energy()
 niter1 = calc.get_number_of_iterations()
-de12t = calc.get_xc_difference('TPSS')
-de12m = calc.get_xc_difference('M06L')
+de1t = calc.get_xc_difference('TPSS')
+de1m = calc.get_xc_difference('M06L')
+de1x = calc.get_xc_difference({'name': 'EXX', 'finegrid': True})
+de1xb = calc.get_xc_difference({'name': 'EXX', 'finegrid': False})
 
 # Hydrogen molecule:
 d = 0.74  # Experimental bond length
@@ -27,21 +29,32 @@ calc.set(txt='H2.txt')
 molecule.set_calculator(calc)
 e2 = molecule.get_potential_energy()
 niter2 = calc.get_number_of_iterations()
-de22t = calc.get_xc_difference('TPSS')
-de22m = calc.get_xc_difference('M06L')
+de2t = calc.get_xc_difference('TPSS')
+de2m = calc.get_xc_difference('M06L')
+de2x = calc.get_xc_difference({'name': 'EXX', 'finegrid': True})
+de2xb = calc.get_xc_difference({'name': 'EXX', 'finegrid': False})
 
 print 'hydrogen atom energy:     %5.2f eV' % e1
 print 'hydrogen molecule energy: %5.2f eV' % e2
 print 'atomization energy:       %5.2f eV' % (2 * e1 - e2)
-print 'atomization energy  tpss: %5.2f eV' % (2 * (e1+de12t) - (e2+de22t))
-print 'atomization energy  m06l: %5.2f eV' % (2 * (e1+de12m) - (e2+de22m))
-PBETPSSdifference = (2 * e1 - e2)-(2 * (e1+de12t) - (e2+de22t))
-PBEM06Ldifference = (2 * e1 - e2)-(2 * (e1+de12m) - (e2+de22m))
+print 'atomization energy  TPSS: %5.2f eV' % (2 * (e1 + de1t) - (e2 + de2t))
+print 'atomization energy  M06L: %5.2f eV' % (2 * (e1 + de1m) - (e2 + de2m))
+print 'atomization energy   EXX: %5.2f eV' % (2 * (e1 + de1x) - (e2 + de2x))
+print 'atomization energy   EXX: %5.2f eV' % (2 * (e1 + de1xb) - (e2 + de2xb))
+PBETPSSdifference = (2 * e1 - e2) - (2 * (e1 + de1t) - (e2 + de2t))
+PBEM06Ldifference = (2 * e1 - e2) - (2 * (e1 + de1m) - (e2 + de2m))
+PBEEXXdifference = (2 * e1 - e2) - (2 * (e1 + de1x) - (e2 + de2x))
+PBEEXXbdifference = (2 * e1 - e2) - (2 * (e1 + de1xb) - (e2 + de2xb))
 print PBETPSSdifference
 print PBEM06Ldifference
+print PBEEXXdifference
+print PBEEXXbdifference
 # TPSS value is from JCP 120 (15) 6898, 2004
+# EXX value is from PRL 77, 3865 (1996)
 equal(PBETPSSdifference, -0.3599, 0.002)
 equal(PBEM06Ldifference, -0.169, 0.003)
+equal(PBEEXXdifference, 0.91, 0.005)
+equal(PBEEXXbdifference, 0.91, 0.005)
 
 energy_tolerance = 0.00001
 niter_tolerance = 0
