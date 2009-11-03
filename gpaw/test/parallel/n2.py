55e46775334371f1c9af8a82b2c8fb919fa4aed7
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2009-11-03 09:21:16 +0000

    Test get_pseudo_wave_function() with BLACS.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5353 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/parallel/n2.py
+++ gpaw/test/parallel/n2.py
@@ -12,6 +12,9 @@ c = GPAW(nbands=8,
          occupations=FermiDirac(width=0.01))
 a.set_calculator(c)
 e1 = a.get_potential_energy()
+c.write('N2')
+c.write('N2', mode='all')
+wf1 = c.get_pseudo_wave_function(4)
 assert c.gd.comm.size == 1
 
 extra_parameters['blacs'] = False
@@ -19,5 +22,7 @@ del sl_diagonalize[:]
 c = GPAW(occupations=FermiDirac(width=0.01))
 a.set_calculator(c)
 e2 = a.get_potential_energy()
+wf2 = c.get_pseudo_wave_function(4)
 assert c.gd.comm.size == 4
 assert abs(e1 -e2) < 1e-9
+assert (wf1 - wf2).ptp() < 1e-9 or (wf1 + wf2).ptp() < 1e-9
