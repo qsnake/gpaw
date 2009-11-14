bafc0ed841648203003af840cc0debd960247596
s032082 <s032082@6419b465-d902-0410-997c-be2266baf723>
2009-11-14 15:27:44 +0000

    Make parallel/n2.py test work for GPAW builds without ScalaPack too. Still requires --debug due to Python wrappers needed for root keyword to diagonalize.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5427 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/parallel/n2.py
+++ gpaw/test/parallel/n2.py
@@ -1,9 +1,10 @@
 from ase import *
-from gpaw import GPAW, sl_diagonalize, extra_parameters, FermiDirac
+from gpaw import GPAW, mpi, sl_diagonalize, extra_parameters, FermiDirac
 from gpaw.eigensolvers.rmm_diis import RMM_DIIS
-import gpaw.mpi as mpi
-sl_diagonalize += [2, 2, 2, 'd']
-extra_parameters['blacs'] = True
+from gpaw.utilities import scalapack
+if scalapack():
+    sl_diagonalize += [2, 2, 2, 'd']
+    extra_parameters['blacs'] = True
 a = molecule('N2')
 a.center(vacuum=3)
 c = GPAW(nbands=8,
@@ -17,8 +18,9 @@ c.write('N2', mode='all')
 wf1 = c.get_pseudo_wave_function(4)
 assert c.gd.comm.size == 1
 
-extra_parameters['blacs'] = False
-del sl_diagonalize[:]
+if scalapack():
+    extra_parameters['blacs'] = False
+    del sl_diagonalize[:]
 c = GPAW(occupations=FermiDirac(width=0.01))
 a.set_calculator(c)
 e2 = a.get_potential_energy()
