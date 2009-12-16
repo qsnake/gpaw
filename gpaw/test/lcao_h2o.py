927c37958d99c5f9a5ec9ec25d28cfef6d559a0c
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2009-12-16 23:00:22 +0000

    This should not have been modified in the last commit.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5644 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/lcao_h2o.py
+++ gpaw/test/lcao_h2o.py
@@ -2,7 +2,6 @@
 from ase import *
 from gpaw import GPAW
 from gpaw.test import equal
-from gpaw.hs_operators import Operator
 
 a = 6
 b = a / 2
@@ -10,7 +9,7 @@ mol = Atoms([Atom('O',(b, b, 0.1219 + b)),
              Atom('H',(b, 0.7633 + b, -0.4876 + b)),
              Atom('H',(b, -0.7633 + b, -0.4876 + b))],
             pbc=False, cell=[a, a, a])
-calc = GPAW(nbands=4, h=0.2, mode='fd')
+calc = GPAW(nbands=4, h=0.2, mode='lcao')
 mol.set_calculator(calc)
 e = mol.get_potential_energy()
 niter = calc.get_number_of_iterations()
