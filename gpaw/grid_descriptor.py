fix bug in potential matrix derivative function causing errors in LCAO/GUC force.  This bug was caused by copy/pasting of code.  Duplicates of the same line all across lfc.py have now been cleaned up.
git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5651 6419b465-d902-0410-997c-be2266baf723

gpaw/grid_descriptor.py
@@ -129,6 +129,7 @@ class GridDescriptor(Domain):
         self.n_c = self.end_c - self.beg_c
 
         self.h_c = (self.cell_cv**2).sum(1)**0.5 / N_c
+        self.h_cv = self.cell_cv / self.N_c[:, np.newaxis]
         self.dv = abs(np.linalg.det(self.cell_cv)) / self.N_c.prod()
 
         # Sanity check for grid spacings:
@@ -140,7 +141,6 @@ class GridDescriptor(Domain):
         self.orthogonal = not (self.cell_cv -
                                np.diag(self.cell_cv.diagonal())).any()
 
-        
     def get_size_of_global_array(self, pad=False):
         if pad:
             return self.N_c

gpaw/lfc.py
@@ -116,10 +116,9 @@ class Sphere:
 
     def spline_to_grid(spline, gd, start_c, end_c, spos_c):
         dom = gd
-        h_cv = dom.cell_cv / gd.N_c[:, np.newaxis]
         pos_v = np.dot(spos_c, dom.cell_cv)
-        return _gpaw.spline_to_grid(spline.spline, start_c, end_c, pos_v, h_cv,
-                                    gd.n_c, gd.beg_c)
+        return _gpaw.spline_to_grid(spline.spline, start_c, end_c, pos_v,
+                                    gd.h_cv, gd.n_c, gd.beg_c)
 
     spline_to_grid = staticmethod(spline_to_grid) # TODO This belongs in Spline!
 
@@ -553,8 +552,7 @@ class NewLocalizedFunctionsCollection(BaseLFC):
                 nm = 2 * spline.get_angular_momentum_number() + 1
                 cspline_M.extend([spline.spline] * nm)
         gd = self.gd
-        h_cv = gd.cell_cv / gd.N_c[:, np.newaxis]
-        self.lfc.derivative(a_xG, c_xMv, h_cv, gd.n_c, cspline_M,
+        self.lfc.derivative(a_xG, c_xMv, gd.h_cv, gd.n_c, cspline_M,
                             gd.beg_c, self.pos_Wv, q)
 
         comm = self.gd.comm
@@ -656,8 +654,7 @@ class NewLocalizedFunctionsCollection(BaseLFC):
                 nm = 2 * spline.get_angular_momentum_number() + 1
                 cspline_M.extend([spline.spline] * nm)
         gd = self.gd
-        h_cv = gd.cell_cv / gd.N_c[:, np.newaxis]
-        self.lfc.normalized_derivative(a_G, c_Mv, h_cv, gd.n_c, cspline_M,
+        self.lfc.normalized_derivative(a_G, c_Mv, gd.h_cv, gd.n_c, cspline_M,
                                        gd.beg_c, self.pos_Wv)
 
         comm = self.gd.comm
@@ -864,10 +861,9 @@ class BasisFunctions(NewLocalizedFunctionsCollection):
                 nm = 2 * spline.get_angular_momentum_number() + 1
                 cspline_M.extend([spline.spline] * nm)
         gd = self.gd
-        h_cv = gd.cell_cv / gd.N_c
         for c in range(3): # XXX
             self.lfc.calculate_potential_matrix_derivative(vt_G, DVt_vMM[c],
-                                                           h_cv,
+                                                           gd.h_cv,
                                                            gd.n_c, q, c,
                                                            np.array(cspline_M),
                                                            gd.beg_c,

gpaw/spline.py
@@ -65,7 +65,7 @@ class Spline:
         return np.array(map(self, r_g))
 
     def get_functions(self, gd, start_c, end_c, spos_c):
-        h_cv = gd.cell_cv / gd.N_c[:, np.newaxis]
+        h_cv = gd.h_cv
         # start_c is the new origin so we translate gd.beg_c to start_c
         origin_c = np.array([0,0,0]) 
         pos_v = np.dot(spos_c, gd.cell_cv) - np.dot(start_c, h_cv)