Correct calculation of dipole moment for GUC calculations.
git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5801 6419b465-d902-0410-997c-be2266baf723

gpaw/grid_descriptor.py
@@ -525,20 +525,14 @@ class GridDescriptor(Domain):
         b_xg[..., npbx:, npby:, npbz:] = a_xg
         return b_xg
 
-    def calculate_dipole_moment(self, rho_xyz):
+    def calculate_dipole_moment(self, rho_g):
         """Calculate dipole moment of density."""
-        if not self.orthogonal:
-            print 'Warning: dipole moment not correct!'
-        rho_xy = np.sum(rho_xyz, axis=2)
-        rho_xz = np.sum(rho_xyz, axis=1)
-        rho_cg = [np.sum(rho_xy, axis=1),
-                  np.sum(rho_xy, axis=0),
-                  np.sum(rho_xz, axis=0)]
-        d_c = np.zeros(3, dtype=rho_xyz.dtype)
-        for c in range(3):
-            r_g = (np.arange(self.n_c[c], dtype=float) +
-                   self.beg_c[c]) * self.h_c[c]
-            d_c[c] = -np.dot(r_g, rho_cg[c]) * self.dv
+        rho_01 = rho_g.sum(axis=2)
+        rho_02 = rho_g.sum(axis=1)
+        rho_cg = [rho_01.sum(axis=1), rho_01.sum(axis=0), rho_02.sum(axis=0)]
+        rhog_c = [np.dot(np.arange(self.beg_c[c], self.end_c[c]), rho_cg[c])
+                  for c in range(3)]
+        d_c = -np.dot(rhog_c, self.h_cv) * self.dv
         self.comm.sum(d_c)
         return d_c
 