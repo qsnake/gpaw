196d69c1682ff4ea9a465826569e99c84490f5c5
naromero <naromero@6419b465-d902-0410-997c-be2266baf723>
2010-02-02 10:54:14 +0000

    Delete old scalapack_realspace, created new test case for BlacsBandDescriptor.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@5852 6419b465-d902-0410-997c-be2266baf723

---
+++ gpaw/test/parallel/realspace_blacs.py
@@ -0,0 +1,68 @@
+"""Test of BLACS Redistributor.
+
+Requires at least 8 MPI tasks.
+"""
+
+import sys
+
+import numpy as np
+
+from gpaw.band_descriptor import BandDescriptor
+from gpaw.grid_descriptor import GridDescriptor
+from gpaw.mpi import world, distribute_cpus
+from gpaw.utilities.blacs import scalapack_set 
+from gpaw.blacs import BlacsGrid, Redistributor, parallelprint, \
+    BlacsBandDescriptor
+
+G = 120  # number of grid points (G x G x G)
+N = 10  # number of bands
+
+# B: number of band groups
+# D: number of domains
+B = 2
+D = 2
+
+M = N // B     # number of bands per group
+assert M * B == N, 'M=%d, B=%d, N=%d' % (M,B,N)
+
+h = 0.2        # grid spacing
+a = h * G      # side length of box
+
+# Set up communicators:
+domain_comm, kpt_comm, band_comm = distribute_cpus(parsize=D, parsize_bands=B, \
+                                                   nspins=1, nibzkpts=2)
+assert world.size >= D*B*kpt_comm.size
+
+if world.rank == 0:
+    print 'MPI: %d domains, %d band groups, %d kpts' % (domain_comm.size, band_comm.size, kpt_comm.size)
+
+# Set up band and grid descriptors:
+bd = BandDescriptor(N, band_comm, False)
+gd = GridDescriptor((G, G, G), (a, a, a), True, domain_comm, parsize=D)
+
+mcpus, ncpus, blocksize = 2, 2, 6
+
+# horrible acronym
+def main(seed=42, dtype=float):
+    bbd = BlacsBandDescriptor(world, gd, bd, kpt_comm, mcpus, ncpus, blocksize)
+    nbands = bd.nbands
+    mynbands = bd.mynbands
+    # We would *not* create H_Nn in the real-space code this way.
+    # This is just for testing purposes.
+    # Note after MPI_Reduce, only meaningful information on gd masters
+    H_Nn = bbd.Nndescriptor.zeros(dtype=dtype)
+    scalapack_set(bbd.Nndescriptor, H_Nn, 0.1, 75.0, 'U')
+    # We would create C_nN in the real-space code this way.
+    C_nN = np.empty((mynbands, nbands), dtype=dtype)
+    diagonalizer = bbd.get_diagonalizer()
+    eps_n = np.zeros(bd.mynbands)
+    diagonalizer.diagonalize(H_Nn, C_nN, eps_n)
+    print 'after broadcast'
+    parallelprint(world, C_nN)
+    parallelprint(world, eps_n)
+
+if __name__ == '__main__':
+    main(dtype=float)
+    # main(dtype=complex)
+
+
