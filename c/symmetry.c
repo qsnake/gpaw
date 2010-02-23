b666e6bbbdc768c802e2b85838d7a3c4671e5c19
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-02-23 06:47:40 +0000

    Symmetry operations should also work for complex arrays.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6018 6419b465-d902-0410-997c-be2266baf723

--- c/symmetry.c
+++ c/symmetry.c
@@ -23,19 +23,32 @@ PyObject* symmetrize(PyObject *self, PyObject *args)
   if (!PyArg_ParseTuple(args, "OOO", &a_g_obj, &b_g_obj, &op_cc_obj)) 
     return NULL;
 
-  const double* a_g = (const double*)a_g_obj->data;
-  double* b_g = (double*)b_g_obj->data;
   const long* C = (const long*)op_cc_obj->data;
   int ng0 = a_g_obj->dimensions[0];
   int ng1 = a_g_obj->dimensions[1];
   int ng2 = a_g_obj->dimensions[2];
-  for (int g0 = 0; g0 < ng0; g0++)
-    for (int g1 = 0; g1 < ng1; g1++)
-      for (int g2 = 0; g2 < ng2; g2++) {
-        int p0 = ((C[0] * g0 + C[3] * g1 + C[6] * g2) % ng0 + ng0) % ng0;
-        int p1 = ((C[1] * g0 + C[4] * g1 + C[7] * g2) % ng1 + ng1) % ng1;
-        int p2 = ((C[2] * g0 + C[5] * g1 + C[8] * g2) % ng2 + ng2) % ng2;
-        b_g[(p0 * ng1 + p1) * ng2 + p2] += *a_g++;
-      }
+  if (PyArray_ISCOMPLEX(a_g_obj)) {
+    const double complex* a_g = (const double complex*)a_g_obj->data;
+    double complex* b_g = (double complex*)b_g_obj->data;
+    for (int g0 = 0; g0 < ng0; g0++)
+      for (int g1 = 0; g1 < ng1; g1++)
+        for (int g2 = 0; g2 < ng2; g2++) {
+          int p0 = ((C[0] * g0 + C[3] * g1 + C[6] * g2) % ng0 + ng0) % ng0;
+          int p1 = ((C[1] * g0 + C[4] * g1 + C[7] * g2) % ng1 + ng1) % ng1;
+          int p2 = ((C[2] * g0 + C[5] * g1 + C[8] * g2) % ng2 + ng2) % ng2;
+          b_g[(p0 * ng1 + p1) * ng2 + p2] += *a_g++;
+        }
+  } else {
+    const double* a_g = (const double*)a_g_obj->data;
+    double* b_g = (double*)b_g_obj->data;
+    for (int g0 = 0; g0 < ng0; g0++)
+      for (int g1 = 0; g1 < ng1; g1++)
+        for (int g2 = 0; g2 < ng2; g2++) {
+          int p0 = ((C[0] * g0 + C[3] * g1 + C[6] * g2) % ng0 + ng0) % ng0;
+          int p1 = ((C[1] * g0 + C[4] * g1 + C[7] * g2) % ng1 + ng1) % ng1;
+          int p2 = ((C[2] * g0 + C[5] * g1 + C[8] * g2) % ng2 + ng2) % ng2;
+          b_g[(p0 * ng1 + p1) * ng2 + p2] += *a_g++;
+        }
+  }
   Py_RETURN_NONE;
 }
