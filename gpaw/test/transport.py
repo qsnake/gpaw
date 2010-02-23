3e89da8b57a2ed16cb72b744f90c5aa634813862
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2010-02-23 11:53:31 +0000

    a test file covers more
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6025 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/transport.py
+++ gpaw/test/transport.py
@@ -10,31 +10,31 @@ L = 7.00
 
 basis = BasisMaker('Na').generate(1, 1, energysplit=0.3)
 
-atoms = Atoms('Na9', pbc=(0, 0, 1), cell=[L, L, 9 * a])
-atoms.positions[:9, 2] = [i * a for i in range(9)]
+atoms = Atoms('Na12', pbc=(1, 1, 1), cell=[L, L, 12 * a])
+atoms.positions[:12, 2] = [i * a for i in range(12)]
 atoms.positions[:, :2] = L / 2.
 atoms.center()
 pl_atoms1 = range(4)     
-pl_atoms2 = range(5, 9)
+pl_atoms2 = range(8, 12)
 pl_cell1 = (L, L, 4 * a) 
 pl_cell2 = pl_cell1
+
 t = Transport(h=0.3,
               xc='LDA',
               basis={'Na': basis},
-              kpts=(1,1,1),
+              kpts=(2,2,1),
               occupations=FermiDirac(0.1),
               mode='lcao',
               poissonsolver=PoissonSolver(nn=2, relax='GS'),
               txt='Na_lcao.txt',
               mixer=Mixer(0.1, 5, weight=100.0),
+              guess_steps=10,
               pl_atoms=[pl_atoms1, pl_atoms2],
               pl_cells=[pl_cell1, pl_cell2],
-              pl_kpts=(1,1,5),
-              edge_atoms=[[0, 3], [0, 8]],
-              mol_atoms=[4],
-              guess_steps=1,
-              fixed_boundary=False,
-              foot_print=False)
+              pl_kpts=(2,2,15),
+              analysis_data_list=['tc'],
+              edge_atoms=[[0, 3], [0, 11]],
+              mol_atoms=range(4, 8))
 atoms.set_calculator(t)
 t.calculate_iv(0.5, 2)
 
