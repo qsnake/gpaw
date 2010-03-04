e0993b2adbd282db9e229ae7ec533bca883a8bed
s032082 <s032082@6419b465-d902-0410-997c-be2266baf723>
2010-03-04 22:00:01 +0000

    Forgot to make asserts work for both 4 and 8 cores
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6095 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/parallel/n2.py
+++ gpaw/test/parallel/n2.py
@@ -2,13 +2,14 @@ from ase import *
 from gpaw import GPAW, mpi, sl_diagonalize, extra_parameters, FermiDirac
 from gpaw.eigensolvers.rmm_diis import RMM_DIIS
 from gpaw.utilities import scalapack, gcd
+B = gcd(mpi.size, 4) # needs mynbands>1
 if scalapack():
     sl_diagonalize += [2, 2, 2, 'd']
     extra_parameters['blacs'] = True
 a = molecule('N2')
 a.center(vacuum=3)
 c = GPAW(nbands=8,
-         parsize_bands=gcd(mpi.size, 4), # needs mynbands>1
+         parsize_bands=B,
          eigensolver=RMM_DIIS(0),
          occupations=FermiDirac(width=0.01))
 a.set_calculator(c)
@@ -16,7 +17,7 @@ e1 = a.get_potential_energy()
 c.write('N2')
 c.write('N2', mode='all')
 wf1 = c.get_pseudo_wave_function(4)
-assert c.wfs.gd.comm.size == 1
+assert c.wfs.bd.comm.size == B and c.wfs.gd.comm.size == mpi.size//B
 
 if scalapack():
     extra_parameters['blacs'] = False
@@ -25,6 +26,6 @@ c = GPAW(occupations=FermiDirac(width=0.01))
 a.set_calculator(c)
 e2 = a.get_potential_energy()
 wf2 = c.get_pseudo_wave_function(4)
-assert c.wfs.gd.comm.size == 4
+assert c.wfs.bd.comm.size == 1 and c.wfs.gd.comm.size == mpi.size
 assert abs(e1 -e2) < 1e-9
 assert (wf1 - wf2).ptp() < 1e-9 or (wf1 + wf2).ptp() < 1e-9
