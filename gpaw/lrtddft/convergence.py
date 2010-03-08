fd0daa19516059180f642da4e4a1fd77050953b2
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2010-03-08 09:58:29 +0000

    energy_range enabled
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6109 6419b465-d902-0410-997c-be2266baf723

--- gpaw/lrtddft/convergence.py
+++ gpaw/lrtddft/convergence.py
@@ -1,5 +1,7 @@
 import os
 
+from ase.units import Hartree
+
 from gpaw.lrtddft import LrTDDFT
 from gpaw.lrtddft.spectrum import spectrum
 
@@ -7,6 +9,7 @@ def check_convergence(lr,             # LrTDDFT object
                       dirname='conv', # directory name to store the files
                       title=None,     # title for gnuplot
                       dn=None,        # steps to vary istart/jend
+                      dE=None,        # steps to vary energy range
                       emin=None,
                       emax=None,
                       folding='Gauss',
@@ -70,40 +73,58 @@ def check_convergence(lr,             # LrTDDFT object
 
     if dn is None:
         dn= -istart0 + jend0
-        dn = int(dn/10.)
+        dn = int(dn / 10.)
 
-    # vary istart
-    print >> fgpl, 'plot "'+fname('full.dat')+'" t "istart=' + \
-          str(istart0) + '" w l lt 1, \\'
-    for i in range(1,4):
-        istart = istart0 + i * dn
-        lr.diagonalize(istart=istart, jend=jend0)
-        fn= fname('istart' + str(istart)+'.dat')
-        spectrum(lr, fn, width=width)
-        print >> fgpl, '    "'+fn+'" t "istart='+str(istart)+'" w l lt',i+1,
-        if i < 3:
-            print >> fgpl,', \\',
-        print >> fgpl
-    print >> fgpl, 'pause -10'
+    if dE is None: 
+        # vary istart
+        print >> fgpl, 'plot "'+fname('full.dat')+'" t "istart=' + \
+              str(istart0) + '" w l lt 1, \\'
+        for i in range(1,4):
+            istart = istart0 + i * dn
+            lr.diagonalize(istart=istart, jend=jend0)
+            fn= fname('istart' + str(istart) + '.dat')
+            spectrum(lr, fn, width=width)
+            print >> fgpl, '    "'+fn+'" t "istart='+str(istart)+'" w l lt',i+1,
+            if i < 3:
+                print >> fgpl,', \\',
+            print >> fgpl
+        print >> fgpl, 'pause -10'
 
-    # vary jend
-    print >> fgpl, 'plot "'+fname('full.dat')+'" t "jend=' + \
-          str(jend0) + '" w l lt 1, \\'
-    for i in range(1,4):
-        jend = jend0 - i * dn
-        lr.diagonalize(jend=jend, istart=istart0)
-        fn = fname('jend' + str(jend)+'.dat')
-        spectrum(lr, fn, width=width)
-        print >> fgpl, '    "'+fn+'" t "jend='+str(jend)+'" w l lt',i+1,
-        if i < 3:
-            print >> fgpl,', \\',
-        print >> fgpl
-    print >> fgpl, 'pause -10'
-
+        # vary jend
+        print >> fgpl, 'plot "'+fname('full.dat')+'" t "jend=' + \
+              str(jend0) + '" w l lt 1, \\'
+        for i in range(1,4):
+            jend = jend0 - i * dn
+            lr.diagonalize(jend=jend, istart=istart0)
+            fn = fname('jend' + str(jend) + '.dat')
+            spectrum(lr, fn, width=width)
+            print >> fgpl, '    "'+fn+'" t "jend='+str(jend)+'" w l lt',i+1,
+            if i < 3:
+                print >> fgpl,', \\',
+            print >> fgpl
+        print >> fgpl, 'pause -10'
+    else:
+        # vary the enrgy range
+        max_kss_energy = 0.
+        for kss in lr.Om.kss:
+            max_kss_energy = max(max_kss_energy, kss.get_energy() * Hartree)
+        print >> fgpl, 'plot "'+ fname('full.dat') + '" t "full"' + \
+            ' w l lt 1, \\'
+        for i in range(1,4):
+            max_energy = max_kss_energy - i * dE
+            lr.diagonalize(energy_range = max_energy)
+            fn = fname('max_energy' + str(max_energy) + '.dat')
+            spectrum(lr, fn, width=width)
+            print >> fgpl, '    "'+ fn + '" t "dE=-'+ str(i * dE) + '" w l lt',
+            print >> fgpl, i + 1,
+            if i < 3:
+                print >> fgpl,', \\',
+            print >> fgpl
+            
     # plot different directions
-    print >> fgpl, 'plot "'+fname('full.dat')+'" u 1:3 t "x" w l lt 1, \\'
-    print >> fgpl, '     "'+fname('full.dat')+'" u 1:4 t "y" w l lt 2, \\'
-    print >> fgpl, '     "'+fname('full.dat')+'" u 1:5 t "z" w l lt 3'
+    print >> fgpl, 'plot "' + fname('full.dat') + '" u 1:3 t "x" w l lt 1, \\'
+    print >> fgpl, '     "' + fname('full.dat') + '" u 1:4 t "y" w l lt 2, \\'
+    print >> fgpl, '     "' + fname('full.dat') + '" u 1:5 t "z" w l lt 3'
     print >> fgpl, 'pause -10'
 
     fgpl.close()
