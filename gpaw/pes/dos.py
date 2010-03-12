620e321ac2ceb29adec2e1e569c2c720838f6148
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2010-03-12 07:41:47 +0000

    Make it work with fractional occupations
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6144 6419b465-d902-0410-997c-be2266baf723

--- gpaw/pes/dos.py
+++ gpaw/pes/dos.py
@@ -1,36 +1,38 @@
+"""Photoelectron spectra from the (shifted) DOS approach.
+
+"""
+from ase import Hartree
 from gpaw.pes import BasePES
-from ase import Hartree as Ha
-from ase import *
-from gpaw import *
-import numpy as np
 
 class DOSPES(BasePES):
-    def __init__(self, Mother, Daughter):
-        self.c_m=Mother
-        self.c_d=Daughter
-        self.f=None
-        self.be=None
-        self.first_peak_energy=None
+    """PES derived from density of states with shifted KS-energies.
 
+    """
+    def __init__(self, mother, daughter):
+        self.c_m = mother
+        self.c_d = daughter
+        self.f = None
+        self.be = None
+        self.first_peak_energy = None
 
     def _calculate(self):
-        
-        e_m=self.c_m.get_potential_energy()
-        e_d=self.c_d.get_potential_energy()
+        """Evaluate energies and spectroscopic factors."""
+        e_m = self.c_m.get_potential_energy()
+        e_d = self.c_d.get_potential_energy()
 
-        if self.first_peak_energy==None:
-            self.first_peak_energy=e_d-e_m
+        if self.first_peak_energy == None:
+            self.first_peak_energy = e_d - e_m
 
-        ex_m=[]
-    
+        ex_m = []
         for kpt in self.c_m.wfs.kpt_u:
-            for j in range(len(kpt.f_n)):
-                ex_m+=[kpt.eps_n[j]* Ha]*int(kpt.f_n[j])
+            for e, f in zip(kpt.eps_n, kpt.f_n):
+                # XXX use occupation numbers
+                ex_m += [e * Hartree] * int(f + .5)
 
         ex_m.sort()
-        self.be=[]
-
+        self.be = []
         for j in range(len(ex_m)):
-            self.be.append(-ex_m[j]+ex_m[-1]+(self.first_peak_energy))
+            self.be.append(-ex_m[j] + ex_m[-1] + self.first_peak_energy)
 
+        # XXX use occupation numbers
         self.f = [1] * len(self.be)
