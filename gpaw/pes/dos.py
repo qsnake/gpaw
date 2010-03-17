ce9315fabc75967e6cd8274e9c3ccd682dec3820
miwalter <miwalter@6419b465-d902-0410-997c-be2266baf723>
2010-03-17 13:06:47 +0000

    unshifted DOS enabled
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6167 6419b465-d902-0410-997c-be2266baf723

--- gpaw/pes/dos.py
+++ gpaw/pes/dos.py
@@ -8,31 +8,33 @@ class DOSPES(BasePES):
     """PES derived from density of states with shifted KS-energies.
 
     """
-    def __init__(self, mother, daughter):
+    def __init__(self, mother, daughter, shift=True):
         self.c_m = mother
         self.c_d = daughter
         self.f = None
         self.be = None
-        self.first_peak_energy = None
+        self.shift = shift
+        self.first_peak_energy = 0.
 
-    def _calculate(self):
+    def _calculate(self, f_min=0.9):
         """Evaluate energies and spectroscopic factors."""
-        e_m = self.c_m.get_potential_energy()
-        e_d = self.c_d.get_potential_energy()
 
-        if self.first_peak_energy == None:
-            self.first_peak_energy = e_d - e_m
-
+        self.be = []
+        self.f = []
         ex_m = []
         for kpt in self.c_m.wfs.kpt_u:
             for e, f in zip(kpt.eps_n, kpt.f_n):
-                # XXX use occupation numbers
-                ex_m += [e * Hartree] * int(f + .5)
+                if f > f_min:
+                    self.be.append(- e * Hartree)
+                    self.f.append(f)
+                    ex_m.append(- e * Hartree)
 
-        ex_m.sort()
-        self.be = []
-        for j in range(len(ex_m)):
-            self.be.append(-ex_m[j] + ex_m[-1] + self.first_peak_energy)
+        if self.shift is True:
+            ex_m.sort()
+            e_m = self.c_m.get_potential_energy()
+            e_d = self.c_d.get_potential_energy()
+            energy_shift = e_d - e_m - ex_m[-1]
+        else:
+            energy_shift = self.shiftex_m[-1]
 
-        # XXX use occupation numbers
-        self.f = [1] * len(self.be)
+        self.be += energy_shift
