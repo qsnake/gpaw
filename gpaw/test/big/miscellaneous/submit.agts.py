297424a45ce28ad7b972ef788b255480f9c69431
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-04-12 17:53:36 +0000

    Move some of the slow tests from the old test suite to the new weekly-test system.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6237 6419b465-d902-0410-997c-be2266baf723

---
+++ gpaw/test/big/miscellaneous/submit.agts.py
@@ -0,0 +1,7 @@
+# Submit tests from the test suite that were remove becase they were
+# too long.
+def agtsmain(env):
+    env.add('H2Al110.py')
+    env.add('dscf_CO.py')
+    env.add('revtpss_tpss_scf.py')
+    env.add('ltt.py')
