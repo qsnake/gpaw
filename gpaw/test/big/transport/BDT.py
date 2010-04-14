9f54baa22b2df71c1417a3c4869a072b81503433
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2010-04-14 13:40:14 +0000

    simplified
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6270 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/transport/BDT.py
+++ gpaw/test/big/transport/BDT.py
@@ -1,10 +1,6 @@
 from gpaw import *
 from ase import *
-from ase.lattice.surface import fcc111
 from gpaw.transport.calculator import Transport
-from gpaw.mpi import world
-import pickle
-from mytools import *
 
 system = read('BDT.traj', -1)
 pl_atoms1 = range(36)     
@@ -16,8 +12,7 @@ pl_cell2 = pl_cell1
 t = Transport( h=0.2,     
                xc='PBE',
                basis={'Au': 'sz', 'H': 'dzp', 'C': 'dzp', 'S':'dzp'},
-               kpts=(4, 4, 1),
-               parsize=(1,1,2),
+               kpts=(1, 1, 1),
                occupations=FermiDirac(0.1),
                mode='lcao',
                txt='ABA.txt',
@@ -25,12 +20,11 @@ t = Transport( h=0.2,
                mixer=Mixer(0.1, 5, weight=100.0),
                pl_atoms=[pl_atoms1, pl_atoms2],
                pl_cells=[pl_cell1, pl_cell2],
-               pl_kpts=[4, 4, 15],
+               pl_kpts=[1, 1, 15],
                extra_density=True,
                analysis_data_list=['tc'],
                edge_atoms=[[ 0, 35],[0 , 107]],
-               mol_atoms=range(36, 72),
-               nleadlayers=[1,1])
+               mol_atoms=range(36, 72))
 system.set_calculator(t)
-t.calculate_iv(3., 16)
+t.calculate_iv(3., 2)
 
