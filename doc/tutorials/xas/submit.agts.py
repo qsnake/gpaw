42caa21df3670c7a7ba9afe1f8812cee8eaea694
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-04-20 09:16:43 +0000

    Fix XAS submit script.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6318 6419b465-d902-0410-997c-be2266baf723

--- doc/tutorials/xas/submit.agts.py
+++ doc/tutorials/xas/submit.agts.py
@@ -1,16 +1,16 @@
 def agts(queue):
     setups = queue.add('setups.py')
-    run = queue.add('run.py', args='--setups=.',
+    run = queue.add('run.py --setups=.',
                     ncpus=8, walltime=25, deps=[setups])
-    dks = queue.add('dks.py', args='--setups=.',
+    dks = queue.add('dks.py --setups=.',
                     ncpus=8, walltime=25, deps=[setups])
-    plot = queue.add('submit.agts.py', deps=[run, dks],
-                     creates=['xas_h2o_spectrum.png'])
+    queue.add('submit.agts.py --setups=.', deps=[run, dks],
+              creates=['xas_h2o_spectrum.png'])
 
 if __name__ == '__main__':
     from gpaw.test import wrap_pylab, equal
     wrap_pylab(['xas_h2o_spectrum.png'])
     execfile('plot.py')
 
-    e_dks = float(open('dks.py.output').readline().split()[2])
+    e_dks = float(open('dks.py_--setups=..output').readline().split()[2])
     equal(e_dks, 532.774, 0.001)
