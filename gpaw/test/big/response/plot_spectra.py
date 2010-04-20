7d5a445ce0a4350c7a3bac8da1adf3e55db81c3f
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2010-04-20 08:07:20 +0000

    modified plotting spectra
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6315 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/response/plot_spectra.py
+++ gpaw/test/big/response/plot_spectra.py
@@ -1,38 +1,42 @@
 import numpy as np
 import pylab as pl
 
-def plot_spectra(head):
+def plot_EELS(head):
 # plot EELS spectra
-    d = np.loadtxt(head + 'q_list')
+    pl.figure(figsize=(4,7))
+
+    d = np.loadtxt(head + '_q_list')
     q = d[:]
     ndata = q.shape[0] + 1
     w = np.zeros(ndata)
     w2 = np.zeros(ndata)
     
-    pl.subplot(1,2,1)
     for i in range(1,ndata):
-        filename = head + 'EELS_' + str(i) 
+        filename = head + '_EELS_' + str(i) 
     
         d = np.loadtxt(filename)
-        pl.plot(d[:,0], d[:,2],'-k')
+        pl.plot(d[:,0], d[:,2] + 0.4*(ndata-i-1),'-k', label=str(q[i-1])[:4])
         
     fsize = 14
     pl.xlabel('Energy (eV)', fontsize=fsize)
-    pl.ylabel('EELS', fontsize=fsize)
-    #pl.ylim([0, 6])
-    pl.title('EELS spectra of ' + head, fontsize=fsize)
+    pl.ylabel('Loss Function', fontsize=fsize)
+    pl.title('EELS spectra of ' + head +': $\Gamma-\mathrm{M}$', fontsize=fsize)
+    pl.ylim(0,)
+    pl.legend(loc=2)
     
-    
+def plot_ABS(head):    
     # plot optical absorption specctrum
-    d = np.loadtxt('Absorption')
+    pl.figure()
+    d = np.loadtxt(head+'_absorption')
     pl.plot(d[:,0], d[:,1], '-k', label='$\mathrm{Re}\epsilon(\omega)$')
     pl.plot(d[:,0], d[:,2], '-r', label='$\mathrm{Im}\epsilon(\omega)$')
     pl.title('Dielectric function of ' + head)
     pl.legend()
     pl.xlabel('Energy (eV)', fontsize=fsize)
     pl.ylabel('$\epsilon$', fontsize=fsize)
+    pl.ylim([-2,20])
     
-plot_spectra('graphite')
-plot_spectra('graphene')
+plot_EELS('graphite')
+#plot_EELS('graphene')
 
 pl.show()
