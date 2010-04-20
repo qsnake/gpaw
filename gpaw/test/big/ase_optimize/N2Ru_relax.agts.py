2a4914356d02441e1b2c1e7518256bfa915995f5
s052580 <s052580@6419b465-d902-0410-997c-be2266baf723>
2010-04-20 11:28:00 +0000

    Add a test and creates=[] in the rest
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6324 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/ase_optimize/N2Ru_relax.agts.py
+++ gpaw/test/big/ase_optimize/N2Ru_relax.agts.py
@@ -1,5 +1,6 @@
 def agts(queue):
-    queue.add('N2Ru_relax.agts.py')
+    queue.add('N2Ru_relax.agts.py',
+              creates=['N2Ru-N2.csv', 'N2Ru-surf.csv'])
 
 if __name__ == "__main__":
     from ase.optimize.test.N2Ru_relax import *
---
+++ gpaw/test/big/ase_optimize/neb.agts.py
@@ -0,0 +1,9 @@
+def agts(queue):
+    queue.add('neb.agts.py',
+              walltime=13*60,
+              ncpus=8,
+              creates=['neb-emt.csv', 'neb-gpaw.csv'])
+
+if __name__ == "__main__":
+    from ase.optimize.test.neb import *
+
