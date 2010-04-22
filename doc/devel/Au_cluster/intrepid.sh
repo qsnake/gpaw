b14f2840c30029c15a5ea12822079dc81f476986
s032082 <s032082@6419b465-d902-0410-997c-be2266baf723>
2010-04-22 00:32:52 +0000

    Corrected more sl_diagonalize=m,n,k,'d' -> sl_diagonalize=m,n,k
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6357 6419b465-d902-0410-997c-be2266baf723

--- doc/devel/Au_cluster/intrepid.sh
+++ doc/devel/Au_cluster/intrepid.sh
@@ -21,4 +21,4 @@ cp $input $scratch/$job
 cp $pos $scratch/$job
 cd $scratch/$job
 
-qsub -A $acct -n $nodes -t $time -q $queue --mode $mode --env BG_MAPPING=$mapping:MPIRUN_ENABLE_TTY_REPORTING=0:OMP_NUM_THREADS=1:GPAW_SETUP_PATH=$GPAW_SETUP_PATH:PYTHONPATH=${install}/gpaw-r6000:${install}/ase-r1438:$PYTHONPATH:LD_LIBRARY_PATH=$CN_LD_LIBRARY_PATH ${install}/gpaw-r6000/build/bin.linux-ppc64-2.6/gpaw-python ${type}.py --domain-decomposition=8,8,8 --state-parallelization=4 --sl_diagonalize=5,5,64,4 
+qsub -A $acct -n $nodes -t $time -q $queue --mode $mode --env BG_MAPPING=$mapping:MPIRUN_ENABLE_TTY_REPORTING=0:OMP_NUM_THREADS=1:GPAW_SETUP_PATH=$GPAW_SETUP_PATH:PYTHONPATH=${install}/gpaw-r6000:${install}/ase-r1438:$PYTHONPATH:LD_LIBRARY_PATH=$CN_LD_LIBRARY_PATH ${install}/gpaw-r6000/build/bin.linux-ppc64-2.6/gpaw-python ${type}.py --domain-decomposition=8,8,8 --state-parallelization=4 --sl_diagonalize=5,5,64
