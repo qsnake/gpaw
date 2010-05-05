4716648c1f8120761b61850e5c500c9b827c50c9
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-05-05 08:03:18 +0000

    NEB test with GPAW timed out.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6411 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/ase_optimize/neb.agts.py
+++ gpaw/test/big/ase_optimize/neb.agts.py
@@ -1,6 +1,6 @@
 def agts(queue):
     queue.add('neb.agts.py',
-              walltime=13*60,
+              walltime=24*60,
               ncpus=8,
               creates=['neb-emt.csv', 'neb-gpaw.csv'])
 
