d0f951434b0edb12c3079f4f6281350105c4b1f0
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2010-05-10 07:30:51 +0000

    no functional change - just more meaningful names
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6424 6419b465-d902-0410-997c-be2266baf723

--- doc/install/Linux/Niflheim/compile.sh
+++ doc/install/Linux/Niflheim/compile.sh
@@ -7,11 +7,11 @@ if test -z $GPAW_HOME;
 fi
 
 rm -rf $GPAW_HOME/build/
-#echo "cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-slid-ethernet.py build_ext 2>&1 | tee compile-slid-ethernet.log" | ssh slid bash
-#echo "cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-slid-infiniband.py build_ext 2>&1 | tee compile-slid-infiniband.log" | ssh slid bash
-echo "source /home/camp/modulefiles.sh&& module load NUMPY&& cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-fjorm.py build_ext 2>&1 | tee compile-fjorm.log" | ssh fjorm bash
-echo "source /home/camp/modulefiles.sh&& module load NUMPY&& cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-thul-acml.py build_ext 2>&1 | tee compile-thul-acml.log" | ssh thul bash
-echo "source /home/camp/modulefiles.sh&& module load NUMPY&& cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-fjorm-infiniband.py build_ext 2>&1 | tee compile-fjorm-infiniband.log" | ssh fjorm bash
+#echo "cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/slid-ethernet.py build_ext 2>&1 | tee compile-slid-ethernet.log" | ssh slid bash
+#echo "cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/slid-infiniband.py build_ext 2>&1 | tee compile-slid-infiniband.log" | ssh slid bash
+echo "source /home/camp/modulefiles.sh&& module load NUMPY&& cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/el5-xeon-gcc43-acml-4.3.0.py build_ext 2>&1 | tee compile-el5-xeon-gcc43-acml-4.3.0.log" | ssh thul bash
+echo "source /home/camp/modulefiles.sh&& module load NUMPY&& cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/el5-opteron-gcc43-goto-1.26-acml-4.3.0.py build_ext 2>&1 | tee compile-el5-opteron-gcc43-goto-1.26-acml-4.3.0.log" | ssh fjorm bash
+echo "source /home/camp/modulefiles.sh&& module load NUMPY&& cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/el5-opteron-infiniband-gcc43-goto-1.26-acml-4.3.0.py build_ext 2>&1 | tee compile-el5-opteron-infiniband-gcc43-goto-1.26-acml-4.3.0.log" | ssh fjorm bash
 # TAU
-echo "source /home/camp/modulefiles.sh&& module load NUMPY&& module load TAU&&cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-fjorm-TAU.py build_ext 2>&1 | tee compile-fjorm-TAU.log" | ssh fjorm bash
-echo "source /home/camp/modulefiles.sh&& module load NUMPY&& module load TAU&&cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/customize-thul-acml-TAU.py build_ext 2>&1 | tee compile-thul-acml-TAU.log" | ssh thul bash
+echo "source /home/camp/modulefiles.sh&& module load NUMPY&& module load TAU&&cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/el5-opteron-gcc43-goto-1.26-acml-4.3.0-TAU.py build_ext 2>&1 | tee compile-el5-opteron-gcc43-goto-1.26-acml-4.3.0-TAU.log" | ssh fjorm bash
+echo "source /home/camp/modulefiles.sh&& module load NUMPY&& module load TAU&&cd $GPAW_HOME&& python setup.py --remove-default-flags --customize=./doc/install/Linux/Niflheim/el5-xeon-gcc43-acml-4.3.0-TAU.py build_ext 2>&1 | tee compile-el5-xeon-gcc43-acml-4.3.0-TAU.log" | ssh thul bash
