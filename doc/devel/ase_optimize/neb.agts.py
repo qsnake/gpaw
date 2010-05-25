9cb52371dc3a1d9af5aa2cf099d90274af271024
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-05-25 11:24:04 +0000

    Use 3*4 cores for three NEB images.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6464 6419b465-d902-0410-997c-be2266baf723

--- doc/devel/ase_optimize/neb.agts.py
+++ doc/devel/ase_optimize/neb.agts.py
@@ -1,9 +1,9 @@
 def agts(queue):
     queue.add('neb.agts.py',
               walltime=36*60,
-              ncpus=8,
+              ncpus=12,
               creates=['neb-emt.csv', 'neb-gpaw.csv'])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from ase.optimize.test.neb import *
 
