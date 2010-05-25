588e4d0ffcd10368fe8cd6bb72609d2ae1bd236f
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-05-25 13:21:26 +0000

    Fix small buglet.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6470 6419b465-d902-0410-997c-be2266baf723

--- doc/devel/memory_bandwidth/run_numactl.py
+++ doc/devel/memory_bandwidth/run_numactl.py
@@ -5,5 +5,5 @@ machine = os.environ.get('MACHINE', 'TEST')
 ncores = os.environ.get('NCORES', 8)
 if rank == 0:
     os.chdir(machine)
-    os.system('STARTCORES='+ncores+'&&. ../run_numactl.sh')
+    os.system('STARTCORES=%d &&. ../run_numactl.sh' % ncores)
     #os.system('. ../run_numactl.sh') # full memory benchmark
