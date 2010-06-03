9fdc8cd6b5938805a3f494701b5ac0a24d631f90
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2010-06-03 15:45:30 +0000

    delete old tests
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6535 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/response/submit.agts.py
+++ gpaw/test/big/response/submit.agts.py
@@ -1,14 +1,20 @@
 def agts(queue):
-    calc = queue.add('graphite_EELS.py',
-                     ncpus=8,
-                     walltime=200)
+    pass
+
+    # New tests adding soon
     
-    queue.add('plot_spectra.py',
-              ncpus=1,
-              walltime=5,
-              deps=[calc])
-    
-    queue.add('check_spectra.py',
-              ncpus=1,
-              walltime=5,
-              deps=[calc])
+#    calc = queue.add('graphite_EELS.py',
+#                     ncpus=8,
+#                     walltime=200)
+#    
+#    queue.add('plot_spectra.py',
+#              ncpus=1,
+#              walltime=5,
+#              deps=[calc])
+#    
+#    queue.add('check_spectra.py',
+#              ncpus=1,
+#              walltime=5,
+#              deps=[calc])
+#
+#    calc = queue.add('silicon_ABS.py')
