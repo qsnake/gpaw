3028cad67f2e9e2f4d5547f6f9dc5b2477ad1e72
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2010-06-04 07:51:30 +0000

    New tests on aluminum (EELS) and diamond (Absorption)
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6540 6419b465-d902-0410-997c-be2266baf723

---
+++ gpaw/test/aluminum_EELS.py
@@ -0,0 +1,73 @@
+import numpy as np
+import sys
+import os
+import time
+from ase.units import Bohr
+from ase.structure import bulk
+from gpaw import GPAW
+from gpaw.atom.basis import BasisMaker
+from gpaw.response.df import DF
+from gpaw.mpi import serial_comm, rank, size
+from gpaw.utilities import devnull
+
+
+if rank != 0:
+  sys.stdout = devnull 
+
+assert size <= 4**3
+
+# Ground state calculation
+
+t1 = time.time()
+
+a = 4.043
+atoms = bulk('Al', 'fcc', a=a)
+atoms.center()
+calc = GPAW(h=0.2,
+            kpts=(4,4,4),
+            xc='LDA')
+
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
+calc.write('Al.gpw','all')
+
+t2 = time.time()
+
+
+
+# Excited state calculation
+calc = GPAW('Al.gpw',communicator=serial_comm)
+            
+dw = 0.1  
+wmax = 24.
+q = np.array([1./4.,0.,0.])
+
+df = DF(calc=calc, q=q, wmax=wmax, dw=dw, eta=0.2,Ecut=50)
+df1, df2 = df.get_dielectric_function()
+#df.write('Al.pckl')
+df.get_EELS_spectrum(df1, df2,filename='EELS_Al')
+df.check_sum_rule(df1, df2)
+
+t3 = time.time()
+
+
+print 'For ground  state calc, it took', (t2 - t1) / 60, 'minutes'
+print 'For excited state calc, it took', (t3 - t2) / 60, 'minutes'
+
+d = np.loadtxt('EELS_Al')
+wpeak = 15.9 # eV
+Nw = 159
+if d[Nw, 1] > d[Nw-1, 1] and d[Nw, 2] > d[Nw+1, 2]:
+    pass
+else:
+    raise ValueError('Plasmon peak not correct ! ')
+
+if (np.abs(d[Nw, 1] - 29.5855600163) > 1e-5
+    or np.abs(d[Nw, 2] -  28.3273860013) > 1e-5):
+    print d[Nw, 1], d[Nw, 2]
+    raise ValueError('Please check spectrum strength ! ')
+                                                              
+
+if rank == 0:
+  os.remove('Al.gpw')
+
---
+++ gpaw/test/diamond_absorption.py
@@ -0,0 +1,57 @@
+import numpy as np
+import sys
+import time
+
+from ase.units import Bohr
+from ase.structure import bulk
+from gpaw import GPAW, FermiDirac
+from gpaw.atom.basis import BasisMaker
+from gpaw.response.df import DF
+from gpaw.mpi import serial_comm, rank, size
+from gpaw.utilities import devnull
+
+
+if rank != 0:
+  sys.stdout = devnull 
+
+# GS Calculation One
+a = 6.75 * Bohr
+atoms = bulk('C', 'diamond', a=a)
+
+calc = GPAW(h=0.25,
+            kpts=(4,4,4),
+            occupations=FermiDirac(0.001))
+
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
+calc.write('C_gs.gpw','all')
+
+
+# Macroscopic dielectric constant calculation
+calc = GPAW('C_gs.gpw',communicator=serial_comm)
+            
+dw = 0.1  
+wmax = 24.
+q = np.array([0.0, 0.00001, 0.])
+
+df = DF(calc=calc, q=q, wlist=(0.,), eta=0.001,
+        Ecut=50, HilbertTrans=False, OpticalLimit=True)
+df1, df2 = df.get_dielectric_function()
+eM1, eM2 = df.get_macroscopic_dielectric_constant(df1, df2)
+
+eM1_ = 6.073033
+eM2_ = 5.981396
+
+if (np.abs(eM1 - eM1_) > 1e-5 or
+    np.abs(eM2 - eM2_) > 1e-5):
+    print eM1, eM2
+    raise ValueError('Macroscopic dielectric constant not correct ! ')
+
+
+# Absorption spectrum calculation
+#df = DF(calc=calc, q=q, wmax=wmax, dw=dw, eta=0.25,
+#        Ecut=50, OpticalLimit=True)
+#df1, df2 = df.get_dielectric_function()
+#df.get_absorption_spectrum(df1, df2)
+#df.check_sum_rule(df1, df2)
+
