c81aedc9d5112b62c481acab8c09d38393ddf392
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2010-06-05 03:41:33 +0000

    correct failed test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6547 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/diamond_absorption.py
+++ gpaw/test/diamond_absorption.py
@@ -18,7 +18,7 @@ if rank != 0:
 a = 6.75 * Bohr
 atoms = bulk('C', 'diamond', a=a)
 
-calc = GPAW(h=0.25,
+calc = GPAW(h=0.2,
             kpts=(4,4,4),
             occupations=FermiDirac(0.001))
 
@@ -39,8 +39,8 @@ df = DF(calc=calc, q=q, wlist=(0.,), eta=0.001,
 df1, df2 = df.get_dielectric_function()
 eM1, eM2 = df.get_macroscopic_dielectric_constant(df1, df2)
 
-eM1_ = 6.073033
-eM2_ = 5.981396
+eM1_ = 6.1518509552
+eM2_ = 6.0525646447
 
 if (np.abs(eM1 - eM1_) > 1e-5 or
     np.abs(eM2 - eM2_) > 1e-5):
