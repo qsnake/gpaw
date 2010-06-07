93cde7568b3494f956ba861301c81e2167928ff3
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-06-07 13:10:18 +0000

    Decrease timeout for NEB test.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6555 6419b465-d902-0410-997c-be2266baf723

--- doc/devel/ase_optimize/neb.agts.py
+++ doc/devel/ase_optimize/neb.agts.py
@@ -1,6 +1,6 @@
 def agts(queue):
     queue.add('neb.agts.py',
-              walltime=36*60,
+              walltime=5 * 60,
               ncpus=12,
               creates=['neb-emt.csv', 'neb-gpaw.csv'])
 
