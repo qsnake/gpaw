2724d9c9fb86fd2c32e24e2040b9d71ca8a0f0a8
jingzhe <jingzhe@6419b465-d902-0410-997c-be2266baf723>
2010-06-22 13:14:55 +0000

    remove has_key
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6719 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/lxc_xcatom.py
+++ gpaw/test/lxc_xcatom.py
@@ -69,11 +69,11 @@ for xc in libxc_set:
     print E2, E2s
     equal(E2, E2s, 1.0e-12)
 
-    if reference.has_key(xc): # compare with old gpaw
+    if xc in reference: # compare with old gpaw
         print 'A:', E2, reference[xc]
         equal(E2, reference[xc], tolerance)
 
-    if reference_libxc.has_key(xc): # compare with reference libxc
+    if xc in reference_libxc: # compare with reference libxc
         print 'B:', E2, reference_libxc[xc]
         equal(E2, reference_libxc[xc], tolerance)
 
