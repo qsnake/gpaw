5f5d1e8254886563e80880eab822560a164e1d38
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-06-25 08:35:56 +0000

    Use np.pi and remove tabs.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6777 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/vdw/benene-dimer-T-shaped.py
+++ gpaw/test/big/vdw/benene-dimer-T-shaped.py
@@ -10,7 +10,7 @@ from gpaw.test import equal
 # Initialization
 molname = 'benzene-mol'
 dimername = 'benzene-dimer'
-f = open('benzene-dimer-T-shape.dat','w')
+f = open('benzene-dimer-T-shape.dat', 'w')
 h = 0.18
 xc = 'vdW-DF'
 
@@ -32,10 +32,12 @@ benz.set_calculator(calc)
 
 # qn constraint
 for i in range(len(benz)):
-        plane = FixedPlane(i, (0, 0, 1))
-        benz.set_constraint(plane)
+    plane = FixedPlane(i, (0, 0, 1))
+    benz.set_constraint(plane)
 
-qn = QuasiNewton(benz,logfile=molname+'_relax.log',trajectory=molname+'_relax.traj')
+qn = QuasiNewton(benz,
+                 logfile=molname + '_relax.log',
+                 trajectory=molname + '_relax.traj')
 qn.run(fmax=0.01)
 
 e_mol = benz.get_potential_energy()
@@ -48,47 +50,47 @@ e_array = np.zeros(20)
 d_array = np.zeros(20)
 
 k = 0
-for i in np.linspace(-6,6,20):
-	k_str = str(k)
-	z = 6.0 + i*0.3
-	BENZ = benz.copy()
-	dimer = BENZ.copy()
-	tags = np.ones_like(dimer)
-	dimer.set_tags(tags)
-	BENZ.rotate('x', pi/2.,center='COM')
-	BENZ.translate([0.,0.,z])
-	dimer.extend(BENZ)
-	dimer.set_cell([cell[0,0],cell[1,1],cell[2,2]+8.])
-	dimer.center()
-	dimer.set_pbc(False)
-	pos = dimer.get_positions()
-	d = pos[21,2] - pos[0,2]
-
-	calc = GPAW(nbands=-2,
-        	    h=h,
-	            xc=xc,
-        	    occupations=FermiDirac(0.0),
-	            txt=dimername+'_'+k_str+'.txt')
-	dimer.set_calculator(calc)
-	e_dimer = dimer.get_potential_energy()
-	del calc
-
-	# interaction energy
-	e_int = e_dimer - 2*e_mol
-	e_array[k] = e_int
-	d_array[k] = d
-
-	print >> f, str(round(d,3)), e_int
-	f.flush()
-	k += 1
+for i in np.linspace(-6, 6, 20):
+    k_str = str(k)
+    z = 6.0 + i * 0.3
+    BENZ = benz.copy()
+    dimer = BENZ.copy()
+    tags = np.ones_like(dimer)
+    dimer.set_tags(tags)
+    BENZ.rotate('x', np.pi / 2, center='COM')
+    BENZ.translate([0, 0, z])
+    dimer.extend(BENZ)
+    dimer.set_cell([cell[0, 0], cell[1, 1], cell[2, 2] + 8])
+    dimer.center()
+    dimer.set_pbc(False)
+    pos = dimer.get_positions()
+    d = pos[21, 2] - pos[0, 2]
+    
+    calc = GPAW(nbands=-2,
+                h=h,
+                xc=xc,
+                occupations=FermiDirac(0.0),
+                txt=dimername + '_' + k_str + '.txt')
+    dimer.set_calculator(calc)
+    e_dimer = dimer.get_potential_energy()
+    del calc
+    
+    # interaction energy
+    e_int = e_dimer - 2 * e_mol
+    e_array[k] = e_int
+    d_array[k] = d
+    
+    print >> f, str(round(d, 3)), e_int
+    f.flush()
+    k += 1
 
 # E_int-curve minimum
 e_0 = 100.
 d_0 = 0.
 for i in range(len(e_array)):
-	if e_array[i] < e_0:
-		e_0 = e_array[i]
-		d_0 = d_array[i]
+    if e_array[i] < e_0:
+        e_0 = e_array[i]
+        d_0 = d_array[i]
 print >> f, '****************'
 print >> f, 'Minimum (E_int,d):', e_0, d_0 
 f.close()
