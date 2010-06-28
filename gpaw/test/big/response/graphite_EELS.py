edd41a0c7e22af55f71f211b0272cd0f5f0cce32
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2010-06-28 11:53:39 +0000

    correct test
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6785 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/response/graphite_EELS.py
+++ gpaw/test/big/response/graphite_EELS.py
@@ -62,11 +62,11 @@ if GS:
 
 if EELS:
                 
-    w = np.linspace(0, 40, 401)
     f = paropen('graphite_q_list', 'w')
 
     for i in range(1,8):
        
+        w = np.linspace(0, 40, 401)
         q = np.array([i/20., 0., 0.]) # Gamma-M excitation
         #q = np.array([i/20., -i/20., 0.]) # Gamma-K excitation
 
