0889509e009f6821c2414f83dca5c860e477c3ce
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-07-09 08:40:40 +0000

    Up walltime to 10 minutes.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6818 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/bader_water/submit.agts.py
+++ gpaw/test/big/bader_water/submit.agts.py
@@ -1,4 +1,4 @@
 def agts(queue):
-    calc = queue.add('bader_water.py', ncpus=8, walltime=4)
+    calc = queue.add('bader_water.py', ncpus=8, walltime=10)
     queue.add('bader_plot.py', walltime=5, deps=[calc])
 
