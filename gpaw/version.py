ddd383c3f8b91c8f515e7d7c512c5774a52d23fd
dulak <dulak@6419b465-d902-0410-997c-be2266baf723>
2010-08-13 15:13:43 +0000

    ase_required_version increased: i happened somewhere after 3.4.0 release as H_force.py test fails with import error with ase-3.4.0.1574
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@6982 6419b465-d902-0410-997c-be2266baf723

--- gpaw/version.py
+++ gpaw/version.py
@@ -3,8 +3,8 @@
 
 version_base = '0.8.0'
 
-ase_required_version = '3.2.0'
-ase_required_svnversion = '1158'
+ase_required_version = '3.4.1'
+ase_required_svnversion = '1765'
 
 try:
     from gpaw.svnversion import svnversion
