ccce25ab199447920450584ce32aed6f255bb998
kkaa <kkaa@6419b465-d902-0410-997c-be2266baf723>
2010-10-14 15:47:41 +0000

    Update in __init__.py
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@7165 6419b465-d902-0410-997c-be2266baf723

--- gpaw/dfpt/__init__.py
+++ gpaw/dfpt/__init__.py
@@ -72,15 +72,18 @@ from gpaw.dfpt.phononcalculator import *
 from gpaw.dfpt.responsecalculator import *
 from gpaw.dfpt.phononperturbation import *
 from gpaw.dfpt.dynamicalmatrix import *
+from gpaw.dfpt.electronphononcoupling import *
 
 # Set the behavior of from gpaw.dfpt import *
 import gpaw.dfpt.phononcalculator
 import gpaw.dfpt.responsecalculator
 import gpaw.dfpt.phononperturbation
 import gpaw.dfpt.dynamicalmatrix
+import gpaw.dfpt.electronphononcoupling
 
 __all__ = []
 __all__.extend(phononcalculator.__all__)
 __all__.extend(responsecalculator.__all__)
 __all__.extend(phononperturbation.__all__)
 __all__.extend(dynamicalmatrix.__all__)
+__all__.extend(electronphononcoupling.__all__)
