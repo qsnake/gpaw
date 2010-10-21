a650ad38cccc7093d27c4f573a95267d2e2eca8c
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2010-10-21 06:38:36 +0000

    5 cpu's is not allowed.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@7190 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/dfpt/submit.agts.py
+++ gpaw/test/big/dfpt/submit.agts.py
@@ -1,4 +1,4 @@
 def agts(queue):
    
     calc = queue.add('Si_AH.py', walltime=120, ncpus=1)
-    queue.add('Si_bandstructure.py', walltime=10 * 60, ncpus=5, deps=calc)
+    queue.add('Si_bandstructure.py', walltime=10 * 60, ncpus=4, deps=calc)
