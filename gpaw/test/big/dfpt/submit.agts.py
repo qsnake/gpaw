dd84d473e72849916551051296453ca60f94f65e
kkaa <kkaa@6419b465-d902-0410-997c-be2266baf723>
2010-12-16 11:32:23 +0000

    Walltime and number of cpus modified
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@7454 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/big/dfpt/submit.agts.py
+++ gpaw/test/big/dfpt/submit.agts.py
@@ -1,4 +1,4 @@
 def agts(queue):
    
     calc = queue.add('Si_AH.py', walltime=120, ncpus=1)
-    queue.add('Si_bandstructure.py', walltime=10 * 60, ncpus=4, deps=calc)
+    queue.add('Si_bandstructure.py', walltime=12 * 60, ncpus=8, deps=calc)
