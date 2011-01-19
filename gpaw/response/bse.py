c5fb9768cb105156498e3320cc06a2b83fb58a64
juya <juya@6419b465-d902-0410-997c-be2266baf723>
2011-01-19 15:43:14 +0000

    bse works for extended systems now
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@7570 6419b465-d902-0410-997c-be2266baf723

--- gpaw/response/bse.py
+++ gpaw/response/bse.py
@@ -7,11 +7,14 @@ from gpaw.utilities import pack
 from gpaw.lfc import LocalizedFunctionsCollection as LFC
 from gpaw.fd_operators import Gradient
 from gpaw.coulomb import CoulombNEW
+from gpaw.xc import XC
+from gpaw.mpi import world, size, rank
 from gpaw.response.cell import get_primitive_cell, set_Gvectors
 from gpaw.response.symmetrize import find_kq, find_ibzkpt, symmetrize_wavefunction
 from gpaw.response.math_func import two_phi_planewave_integrals
 from gpaw.response.chi import CHI
-from gpaw.xc import XC
+from gpaw.response.parallel import parallel_partition
+
 
 class BSE(CHI):
     """This class defines Belth-Selpether equations."""
@@ -23,7 +26,7 @@ class BSE(CHI):
                  q=None,
                  ecut=10.,
                  eta=0.2,
-                 ftol=1e-7,
+                 ftol=1e-5,
                  txt=None,
                  hilbert_trans=True,
                  optical_limit=False,
@@ -34,7 +37,17 @@ class BSE(CHI):
 
     def initialize(self):
 
+        # frequency points init
+        self.dw = self.w_w[1] - self.w_w[0]
+        assert ((self.w_w[1:] - self.w_w[:-1] - self.dw) < 1e-10).all() # make sure its linear w grid
+        assert self.w_w.max() == self.w_w[-1]
+
+        self.eta /= Hartree
         self.ecut /= Hartree
+        self.dw /= Hartree
+        self.w_w  /= Hartree
+        self.wmax = self.w_w[-1] 
+        self.Nw  = int(self.wmax / self.dw) + 1
 
         calc = self.calc
         
@@ -43,15 +56,6 @@ class BSE(CHI):
         self.ibzk_kc = calc.get_ibz_k_points()
         self.nkpt = self.bzk_kc.shape[0]
         self.ftol /= self.nkpt
-
-        # band init
-        if self.nbands is None:
-            self.nbands = calc.wfs.nbands
-        self.nvalence = calc.wfs.nvalence
-        self.nv = (self.nvalence + 1) // 2 # for semicondutors or molecules
-        self.nc = self.nbands - self.nv
-        self.nS = self.nc * self.nv * self.nkpt
-        print 'bands:',  self.nv, self.nc
 
         assert calc.wfs.nspins == 1
 
@@ -80,6 +84,11 @@ class BSE(CHI):
         self.f_kn = np.array([calc.get_occupation_numbers(kpt=k) / kweight_k[k]
                     for k in range(nibzkpt)]) / self.nkpt
         print self.f_kn
+
+        # band init
+        if self.nbands is None:
+            self.nbands = calc.wfs.nbands
+        self.nvalence = calc.wfs.nvalence
 
         # k + q init
         assert self.q_c is not None
@@ -120,6 +129,14 @@ class BSE(CHI):
 
         self.printtxt('Number of planewaves, %d' %(self.npw))
 
+        # parallel init
+        if self.nkpt > 1:
+            self.kcomm = world
+            self.nkpt, self.nkpt_local, self.kstart, self.kend = parallel_partition(
+                               self.nkpt, self.kcomm.rank, self.kcomm.size, reshape=False)
+            print self.kstart, self.kend, self.nkpt_local
+
+
         # For LCAO wfs
         if calc.input_parameters['mode'] == 'lcao':
             calc.initialize_positions()        
@@ -144,25 +161,16 @@ class BSE(CHI):
         self.printtxt('')
         self.printtxt('Finished phi_Gp !')
 
+        # Coulomb kernel init
         self.kc_G = np.zeros(self.npw)
-        for iG in range(1,self.npw):
+        for iG in range(self.npw):
             index = self.Gindex_G[iG]
             qG = np.dot(self.q_c + self.Gvec_Gc[iG], self.bcell_cv)
             self.kc_G[iG] = 1. / np.inner(qG, qG)
+        if self.optical_limit:
+            self.kc_G[0] = 0.
 
-        self.rhoG0_s = np.zeros((self.nv, self.nc, self.nkpt), dtype=complex)
-        self.flag = np.zeros((self.nv, self.nc, self.nkpt), dtype=bool)
-
-        self.dw = self.w_w[1] - self.w_w[0]
-        assert ((self.w_w[1:] - self.w_w[:-1] - self.dw) < 1e-10).all() # make sure its linear w grid
-        assert self.w_w.max() == self.w_w[-1]
-
-        self.eta /= Hartree
-        self.dw /= Hartree
-        self.w_w  /= Hartree
-        self.wmax = self.w_w[-1] 
-        self.Nw  = int(self.wmax / self.dw) + 1
-
+        
         return
 
     def calculate(self):
@@ -189,151 +197,85 @@ class BSE(CHI):
             ibzk2_k[ik], iop2_k[ik], timerev2_k[ik] = find_ibzkpt(self.op_scc,
                                                 ibzk_kc, bzk_kc[kq_k[ik]])
 
-        focc_s = np.zeros((self.nv, self.nc, self.nkpt))
-        e_s = np.zeros((self.nv, self.nc, self.nkpt))
+        e_S = {}
+        focc_s = {}
+        Sindex_S3 = {}
+        iS = 0
         for k1 in range(self.nkpt):
-            for n1 in range(self.nv):
-                for m1 in range(self.nc):
-                    e_s[n1, m1, k1] = e_kn[ibzk2_k[k1],m1+self.nv] - e_kn[ibzk1_k[k1],n1]
-                    focc_s[n1, m1, k1] = f_kn[ibzk1_k[k1],n1] - f_kn[ibzk2_k[k1],m1+self.nv]
+            for n1 in range(self.nbands):
+                for m1 in range(self.nbands):
+                    focc = f_kn[ibzk1_k[k1],n1] - f_kn[ibzk2_k[k1],m1]
+                    if np.abs(focc) > self.ftol:
+                        e_S[iS] = e_kn[ibzk2_k[k1],m1] - e_kn[ibzk1_k[k1],n1]
+                        focc_s[iS] = focc
+                        Sindex_S3[iS] = (k1, n1, m1)
+                        iS += 1
+        self.nS = iS
+        print self.nS, self.nkpt, self.nbands
+        focc_S = np.zeros(self.nS)
+        for iS in range(self.nS):
+            focc_S[iS] = focc_s[iS]
 
-        K_ss = np.zeros((self.nv, self.nc, self.nkpt,
-                         self.nv, self.nc, self.nkpt), dtype=complex)
-        for k1 in range(self.nkpt):
-            for n1 in range(self.nv):
-                for m1 in range(self.nc):
-                    focc = f_kn[ibzk1_k[k1],n1] - f_kn[ibzk2_k[k1],m1+self.nv] # m1 + self.nv
-                    if focc > self.ftol:
-                        for k2 in range(self.nkpt):
-                            for n2 in range(self.nv):
-                                for m2 in range(self.nc):
-                                    if 1: #n2 <= n1 and m2 <= m1:
-                                        K_ss[n1, m1, k1, n2, m2, k2] = \
-                                                 self.calculate_kernel(n1,m1+self.nv,k1,n2,m2+self.nv,k2)
-                                        print k1, n1, m1+self.nv, k2, n2, m2+self.nv
+        K_SS = np.zeros((self.nS, self.nS), dtype=complex)
+        rhoG0_S = np.zeros((self.nS), dtype=complex)
 
-        e_S = e_s.ravel()
-        focc_S = focc_s.ravel()
-        K_SS = K_ss.reshape(self.nv*self.nc*self.nkpt, self.nv*self.nc*self.nkpt)
+        nS, nS_local, nS_start, nS_end = parallel_partition(
+                               self.nS, world.rank, world.size, reshape=False)
+        print nS_local, nS_start, nS_end
+
+        for iS in range(nS_start, nS_end):
+            print iS
+            k1, n1, m1 = Sindex_S3[iS]
+            rho1_G = self.density_matrix_Gspace(n1,m1,k1)
+            rhoG0_S[iS] = rho1_G[0]
+            for jS in range(self.nS):
+                k2, n2, m2 = Sindex_S3[jS]
+                rho2_G = self.density_matrix_Gspace(n2,m2,k2)
+                K_SS[iS, jS] = np.sum(rho1_G.conj() * rho2_G * self.kc_G)
+        K_SS *= 4 * pi / self.vol
+        world.sum(K_SS)
+        world.sum(rhoG0_S)
+        
         H_SS = np.zeros_like(K_SS)
         for iS in range(self.nS):
             H_SS[iS,iS] = e_S[iS]
             for jS in range(self.nS):
                 H_SS[iS,jS] += focc_S[iS] * K_SS[iS,jS]
 
-        # Including the resonant and anti-resonant term
-        H2_SS = np.zeros((self.nS*2, self.nS*2),dtype=complex)
-        for iS in range(self.nS):
-            for jS in range(self.nS):
-                H2_SS[iS, jS] = H_SS[iS, jS]
-                H2_SS[iS+self.nS, jS+self.nS] = -H_SS[iS, jS]
-                H2_SS[iS, jS+self.nS] = focc_S[iS] * K_SS[iS, jS]
-                H2_SS[iS+self.nS, jS] = -focc_S[iS] * K_SS[iS, jS]
+        w_S, v_SS = np.linalg.eig(H_SS)
 
-        w, v = np.linalg.eig(H2_SS)
-        print 'Solve BSE (without Tamm-Dancoff appx.):'
-        print w * Hartree
-
-        # Using Tamm-Dancoff approximation
-        w_S = np.zeros(self.nS)
-        diagonalize(H_SS, w_S) # H_SS is replaced by the eigenvectors after diagonalization
-        print 'Solve BSE (with Tamm-Dancoff appx.):'        
-        print w_S * Hartree
-
-        # solve casida's equation
-        Omega_SS = np.zeros_like(K_SS)
-        for iS in range(self.nS):
-            Omega_SS[iS,iS] = e_S[iS]**2
-            for jS in range(self.nS):
-                Omega_SS[iS,jS] += 2 * np.sqrt(focc_S[iS] * e_S[iS]
-                                               * focc_S[jS] * e_S[jS]) * K_SS[iS,jS]
-        w2_S = np.zeros(self.nS)
-        diagonalize(Omega_SS, w2_S)
-        print 'Solve Casida equation:'
-        print np.sqrt(w2_S) * Hartree           
-
-
-        rhoG0_S = self.rhoG0_s.ravel()
         epsilon_w = np.zeros(self.Nw, dtype=complex)
-        
-        for iS in range(len(w_S)):
-            tmp_S = H_SS[iS] * rhoG0_S
-            tmp = np.outer(tmp_S*focc_S, tmp_S.conj()).sum()
+
+        tmp = np.zeros((self.nS, self.nS), dtype=complex)
+        for iS in range(self.nS):
+            for jS in range(self.nS):
+                tmp[iS, jS] = (v_SS[:, iS].conj() * v_SS[:, jS]).sum()
+        overlap_SS = np.linalg.inv(tmp)
+
+        tmp_w = np.zeros(self.Nw, dtype=complex)
+        for iS in range(nS_start, nS_end):
+            tmp_iS = v_SS[:,iS] * rhoG0_S 
             for iw in range(self.Nw):
-                epsilon_w[iw] += tmp / (iw*self.dw - w_S[iS] + 1j * self.eta)
-        epsilon_w /=  self.nkpt * self.vol
-        epsilon_w *= 1-4 * pi / np.inner(self.qq_v, self.qq_v)
+                tmp_w[iw] = 1. / (iw*self.dw - w_S[iS] + 1j * self.eta)
+            print iS
+            for jS in range(self.nS):
+                tmp_jS = v_SS[:,jS] * rhoG0_S * focc_S
+                tmp = np.outer(tmp_iS, tmp_jS.conj()).sum() * overlap_SS[iS, jS]
+                epsilon_w += tmp * tmp_w
+        world.sum(epsilon_w)
+
+        epsilon_w *=  - 4 * pi / np.inner(self.qq_v, self.qq_v) / self.vol
+        epsilon_w += 1        
 
         return w_S, epsilon_w
 
-
-    def calculate_kernel(self, n1,m1,k1,n2,m2,k2):
-
-        gd = self.calc.wfs.gd
-        
-        psit1_g, P1_ap, rho1_G0 = self.density_matrix_Rspace(n1,m1,k1)
-        psit2_g, P2_ap, rho2_G0 = self.density_matrix_Rspace(n2,m2,k2)
-
-        if 0: #FFT:
-            rho1_G = self.density_matrix_Gspace(psit1_g, P1_ap, rho1_G0, n1, m1, k1)
-            rho2_G = self.density_matrix_Gspace(psit2_g, P2_ap, rho2_G0, n2, m2, k2)
-
-            kernel= 4 * pi / self.vol * np.sum(rho1_G.conj() * rho2_G * self.kc_G)
-
-        if 1: # Poisson:
-            spos_ac = self.calc.atoms.get_scaled_positions()
-            coulomb = CoulombNEW(gd, self.calc.wfs.setups, spos_ac, fft=False)
-            P1_aP = {}
-            P2_aP = {}
-            for a, id in enumerate(self.calc.wfs.setups.id_a):
-                ni = self.calc.wfs.setups[a].ni
-                P1_aP[a] = pack(P1_ap[a].conj().reshape(ni,ni), tolerance=1e30)
-                P2_aP[a] = pack(P2_ap[a].reshape(ni,ni), tolerance=1e30)
-                            
-            kernel = coulomb.calculate(psit1_g.conj(), psit2_g, P1_aP, P2_aP) / Hartree
-
-        # xc kernel, PAW term not included yet.
-        xc = XC('LDA')
-        nt_sG = self.calc.density.nt_sG
-        fxc_sg = np.zeros_like(nt_sG)
-        xc.calculate_fxc(gd, nt_sG, fxc_sg)
-
-#        kernel += gd.integrate(psit1_g.conj() * psit2_g * fxc_sg[0])
-
-        return kernel
-
-    def density_matrix_Gspace(self, psit_g, P_ap, rho_G0, n, m, k):
-
-
-        rho_g = np.fft.fftn(psit_g*self.expqr_g) * self.vol / self.nG0
-
-        # Here, planewave cutoff is applied
-        rho_G = np.zeros(self.npw, dtype=complex)
-        for iG in range(self.npw):
-            index = self.Gindex_G[iG]
-            rho_G[iG] = rho_g[index[0], index[1], index[2]]
-
-        if self.optical_limit:
-            rho_G[0] = rho_G0
-
-        # PAW correction
-        for a, id in enumerate(self.calc.wfs.setups.id_a):
-            gemv(1.0, self.phi_aGp[a], P_ap[a], 1.0, rho_G)
-
-        if self.optical_limit: 
-            rho_G[0] /= self.e_kn[self.ibzk2_k[k], m] - self.e_kn[self.ibzk1_k[k], n]
-        if self.flag[n, m-self.nv, k] == False:
-            self.rhoG0_s[n, m-self.nv, k] = rho_G[0]
-            self.flag[n, m-self.nv, k] = True
-
-        return rho_G
-
     
-    def density_matrix_Rspace(self,n,m,k):
+    def density_matrix_Gspace(self,n,m,k):
 
         ibzk_kc = self.ibzk_kc
         bzk_kc = self.bzk_kc
         kq_k = self.kq_k
+        gd = self.gd
 
         ibzkpt1, iop1, timerev1 = find_ibzkpt(self.op_scc, ibzk_kc, bzk_kc[k])
         ibzkpt2, iop2, timerev2 = find_ibzkpt(self.op_scc, ibzk_kc, bzk_kc[kq_k[k]])
@@ -346,21 +288,16 @@ class BSE(CHI):
         psit2_g = symmetrize_wavefunction(psitold_g, self.op_scc[iop2], ibzk_kc[ibzkpt2],
                                           bzk_kc[kq_k[k]], timerev2)
 
-        psit1_g = np.complex128(self.calc.wfs.kpt_u[0].psit_nG[n])
-        psit2_g = np.complex128(self.calc.wfs.kpt_u[0].psit_nG[m])
-        
-        pt = self.pt
-        P1_ai = pt.dict()
-        pt.integrate(psit1_g, P1_ai, k)
-        P2_ai = pt.dict()
-        pt.integrate(psit2_g, P2_ai, kq_k[k])
-        P_ap = {}
-        for a, id in enumerate(self.calc.wfs.setups.id_a):        
-            P_ap[a] = np.outer(P1_ai[a].conj(), P2_ai[a]).ravel()
-            P_ni = self.calc.wfs.kpt_u[0].P_ani[a]
+        # FFT
+        tmp_g = psit1_g.conj()* psit2_g * self.expqr_g
+        rho_g = np.fft.fftn(tmp_g) * self.vol / self.nG0
 
-        gd = self.gd
-        rhoG0 = None
+        # Here, planewave cutoff is applied
+        rho_G = np.zeros(self.npw, dtype=complex)
+        for iG in range(self.npw):
+            index = self.Gindex_G[iG]
+            rho_G[iG] = rho_g[index[0], index[1], index[2]]
+
         if self.optical_limit:
             d_c = [Gradient(gd, i, n=4, dtype=complex).apply for i in range(3)]
             dpsit_g = gd.empty(dtype=complex)
@@ -370,6 +307,20 @@ class BSE(CHI):
             for ix in range(3):
                 d_c[ix](psit2_g, dpsit_g, phase_cd)
                 tmp[ix] = gd.integrate(psit1_g.conj() * dpsit_g)
-            rhoG0 = -1j * np.dot(self.qq_v, tmp)
+            rho_G[0] = -1j * np.dot(self.qq_v, tmp)
 
-        return psit1_g.conj()* psit2_g, P_ap, rhoG0
+        # PAW correction
+        pt = self.pt
+        P1_ai = pt.dict()
+        pt.integrate(psit1_g, P1_ai, k)
+        P2_ai = pt.dict()
+        pt.integrate(psit2_g, P2_ai, kq_k[k])
+                        
+        for a, id in enumerate(self.calc.wfs.setups.id_a):
+            P_p = np.outer(P1_ai[a].conj(), P2_ai[a]).ravel()
+            gemv(1.0, self.phi_aGp[a], P_p, 1.0, rho_G)
+
+        if self.optical_limit:
+            rho_G[0] /= self.e_kn[ibzkpt2, m] - self.e_kn[ibzkpt1, n]
+
+        return rho_G
