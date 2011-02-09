0bd3315a058ecc3763389ac81b41895c95879e79
tolsen <tolsen@6419b465-d902-0410-997c-be2266baf723>
2011-02-09 15:10:25 +0000

    Changed test to even number of kpoints
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@7725 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/rpa_energy_Kr.py
+++ gpaw/test/rpa_energy_Kr.py
@@ -6,16 +6,14 @@ from gpaw.mpi import serial_comm
 from gpaw.test import equal
 from gpaw.xc.rpa_correlation_energy import RPACorrelation
 
-calc = GPAW(h=0.18, xc='LDA', kpts=(3,3,3),
+calc = GPAW(h=0.18, xc='LDA', kpts=(4,4,4), #usesymm=None,
             nbands=15, eigensolver='cg', 
             convergence={'bands': -5},
             communicator=serial_comm)
 
 V = 30.
 a0 = (4.*V)**(1/3.)
-cell = bulk('Kr', 'fcc', a=a0).get_cell()
-Kr = Atoms('Kr', cell=cell, pbc=True,
-           scaled_positions=[[0,0,0]])
+Kr = bulk('Kr', 'fcc', a=a0)
 
 Kr.set_calculator(calc)
 Kr.get_potential_energy()
@@ -25,4 +23,4 @@ w = np.linspace(0.0, 50., 8)
 rpa = RPACorrelation(calc)
 E_rpa = rpa.get_rpa_correlation_energy(ecut=ecut, w=w)
 
-equal(E_rpa, -1.85, 0.01)
+equal(E_rpa, -3.2, 0.1)
