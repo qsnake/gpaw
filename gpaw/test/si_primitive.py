64d9083a0d3cb94e845d560f705f82618eb84fac
jensj <jensj@6419b465-d902-0410-997c-be2266baf723>
2011-02-21 09:49:52 +0000

    Back to real-valued step lengths for RMM-DIIS.
    git-svn-id: https://svn.fysik.dtu.dk/projects/gpaw/trunk@7757 6419b465-d902-0410-997c-be2266baf723

--- gpaw/test/si_primitive.py
+++ gpaw/test/si_primitive.py
@@ -11,10 +11,10 @@ calc = GPAW(h=0.24,
 atoms = bulk('Si', 'diamond', a=a)
 atoms.set_calculator(calc)
 E = atoms.get_potential_energy()
-equal(E, -11.8094734, 0.0001)
+equal(E, -11.8092858, 0.0001)
 niter = calc.get_number_of_iterations()
-assert 23 <= niter <= 25, niter
+assert 25 <= niter <= 27, niter
 
-equal(atoms.calc.get_fermi_level(), 5.17745781, 0.005)
+equal(atoms.calc.get_fermi_level(), 5.17751284, 0.005)
 homo, lumo = calc.get_homo_lumo()
-equal(lumo - homo, 1.11452458, 0.001)
+equal(lumo - homo, 1.11445025, 0.001)
